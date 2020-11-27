1edbb84dbcf4646b201fd8798ed9b923b522303b
Rellu <github@relluem94.de>
2020-11-27 16:02:01 +0100

    fixed wrong command name

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/commands/Repair.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/commands/Repair.java
@@ -16,7 +16,7 @@ import org.bukkit.inventory.meta.ItemMeta;
 public class Repair implements CommandExecutor {
 
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (command.getName().equalsIgnoreCase("more")) {
+        if (command.getName().equalsIgnoreCase("repair")) {
             if (args.length == 0) {
                 if (sender instanceof Player) {
                     Player p = (Player) sender;
