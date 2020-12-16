fe056d6eefb6e1df6e4eac254a5453142fa07e97
Rellu <github@relluem94.de>
2020-12-16 04:45:10 +0100

    added save join for vips and higher. no longer dead on join lol.

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/events/BetterPlayerJoin.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/events/BetterPlayerJoin.java
@@ -11,6 +11,7 @@ import org.bukkit.event.player.PlayerJoinEvent;
 
 import main.java.de.relluem94.minecraft.server.spigot.essentials.permissions.User;
 import main.java.de.relluem94.minecraft.server.spigot.essentials.permissions.groups.UserGroup;
+import org.bukkit.Material;
 
 public class BetterPlayerJoin implements Listener{
 	
@@ -21,7 +22,12 @@ public class BetterPlayerJoin implements Listener{
 		e.setJoinMessage(null);
 		Player p = e.getPlayer();
 		addPlayer(p);
-		
+		if(User.getGroup(p).getId() >= 2){
+                    p.setAllowFlight(true);
+                    if(p.getLocation().getBlock().getRelative(0, -1, 0).getType().equals(Material.AIR)){
+                        p.setFlying(true);
+                    }
+                }
 		Bukkit.broadcastMessage(String.format(PLUGIN_EVENT_JOIN_MESSAGE, p.getCustomName()));
 	}
 	
