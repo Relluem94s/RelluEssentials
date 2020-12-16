19c1fb44ad6f0f03c9a3c07184091abe7527f0fe
Rellu <github@relluem94.de>
2020-12-16 15:43:13 +0100

    consolecommandsender for setgroup. possible to set owner on a fresh server hahaha

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/commands/PermissionsGroup.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/commands/PermissionsGroup.java
@@ -11,36 +11,45 @@ import org.bukkit.entity.Player;
 import main.java.de.relluem94.minecraft.server.spigot.essentials.permissions.Group;
 import main.java.de.relluem94.minecraft.server.spigot.essentials.permissions.Permission;
 import main.java.de.relluem94.minecraft.server.spigot.essentials.permissions.User;
+import org.bukkit.command.ConsoleCommandSender;
 
 public class PermissionsGroup implements CommandExecutor {
-	
-	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		if (command.getName().equalsIgnoreCase("setGroup")) {
-			if (args.length == 2) {
-				Player target = Bukkit.getPlayer(args[0]);
-				 if(target != null) {
-					 if (sender instanceof Player) {
-						Player p = (Player) sender;
-						if(Permission.isAuthorized(p, 8)) {
-							Group g = Group.getGroupFromName(args[1]);
-							User u = User.getUserByPlayerName(target.getName());
-							 
-							if(u != null) {
-								u.setGroup(g);
-							}
-							else {
-								u = new User(target, User.getGroup(target));
-							} 
-							return true;
-						}
-						else {
-							p.sendMessage(PLUGIN_COMMAND_PERMISSION_MISSING);
-							return true;
-						}
-					 }
-				 }
-			 }
-		}
-		return false;
-	}
+
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("setGroup")) {
+            if (args.length == 2) {
+                Player target = Bukkit.getPlayer(args[0]);
+                if (target != null) {
+                    if (sender instanceof Player) {
+                        Player p = (Player) sender;
+                        if (Permission.isAuthorized(p, 8)) {
+                            Group g = Group.getGroupFromName(args[1]);
+                            User u = User.getUserByPlayerName(target.getName());
+
+                            if (u != null) {
+                                u.setGroup(g);
+                            } else {
+                                u = new User(target, User.getGroup(target));
+                            }
+                            return true;
+                        } else {
+                            p.sendMessage(PLUGIN_COMMAND_PERMISSION_MISSING);
+                            return true;
+                        }
+                    } else if (sender instanceof ConsoleCommandSender) {
+                        Group g = Group.getGroupFromName(args[1]);
+                        User u = User.getUserByPlayerName(target.getName());
+
+                        if (u != null) {
+                            u.setGroup(g);
+                        } else {
+                            u = new User(target, User.getGroup(target));
+                        }
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
 }
