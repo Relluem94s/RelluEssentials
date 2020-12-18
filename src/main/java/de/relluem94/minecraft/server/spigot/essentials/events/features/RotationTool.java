b24cfd35ab4bd635eda38000f69822a58aff4126
Rellu <github@relluem94.de>
2020-12-18 02:50:52 +0100

    java format

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/events/features/RotationTool.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/events/features/RotationTool.java
@@ -3,5 +3,5 @@ package main.java.de.relluem94.minecraft.server.spigot.essentials.events.feature
 import org.bukkit.event.Listener;
 
 public class RotationTool implements Listener {
-
+    // To rotate rotatable Blocks like stairs etc.
 }
--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/events/features/SelectionTool.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/events/features/SelectionTool.java
@@ -5,11 +5,12 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 public class SelectionTool implements Listener {
-	@EventHandler
-	public void onClick(PlayerInteractEvent e) {
-		//TODO click left = pos 1
-		//TODO click right = pos 2
-		//TODO selection into main selections
-		//TODO Netherstar as ITEM
-	}
+
+    @EventHandler
+    public void onClick(PlayerInteractEvent e) {
+        //TODO click left = pos 1
+        //TODO click right = pos 2
+        //TODO selection into main selections
+        //TODO Netherstar as ITEM
+    }
 }
