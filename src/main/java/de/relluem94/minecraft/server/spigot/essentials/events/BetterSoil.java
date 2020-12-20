447ee4ede582347b10a1fc4a353ae02ba5904f8e
Rellu <github@relluem94.de>
2020-12-20 18:43:22 +0100

    fixed soil problem

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/events/BetterSoil.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/events/BetterSoil.java
@@ -1,18 +1,26 @@
 package main.java.de.relluem94.minecraft.server.spigot.essentials.events;
 
 import org.bukkit.Material;
-import org.bukkit.entity.EntityType;
+import org.bukkit.block.Block;
+import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityInteractEvent;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
 
 /* Better Call Soil */
 public class BetterSoil implements Listener {
 
     @EventHandler
-    public void onChange(EntityInteractEvent e) {
-        if ((e.getEntityType() != EntityType.PLAYER) && (e.getBlock().getType() == Material.FARMLAND)) {
-            e.setCancelled(true);
+    public void onChange(PlayerInteractEvent e) {
+        if(e.getAction() == Action.PHYSICAL){
+            Block b = e.getClickedBlock();
+            if(b.getType().equals(Material.FARMLAND)){
+                if(!e.getPlayer().isSneaking()){
+                    e.setUseInteractedBlock(Event.Result.DENY);
+                    e.setCancelled(true);
+                }
+            }
         }
     }
 }
