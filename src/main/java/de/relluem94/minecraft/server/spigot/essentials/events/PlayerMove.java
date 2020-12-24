6a49b35cda726ba646057a63e104af79a32235b5
Relluem94 <github@relluem94.de>
2020-12-24 23:01:18 +0100

    added event for afk

---
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/events/PlayerMove.java
@@ -0,0 +1,17 @@
+package main.java.de.relluem94.minecraft.server.spigot.essentials.events;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+
+/**
+ *
+ * @author rellu
+ */
+public class PlayerMove implements Listener {
+    
+     @EventHandler
+    public void onSpawn(PlayerMoveEvent e) {
+        
+    }
+}
