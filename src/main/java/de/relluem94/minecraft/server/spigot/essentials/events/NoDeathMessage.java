c0e9a5053c49a605be314f5d9e3bb05dd7f2023f
Relluem94 <github@relluem94.de>
2020-12-25 22:03:03 +0100

    show location on death

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/events/NoDeathMessage.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/events/NoDeathMessage.java
@@ -1,5 +1,8 @@
 package main.java.de.relluem94.minecraft.server.spigot.essentials.events;
 
+import static main.java.de.relluem94.minecraft.server.spigot.essentials.Strings.PLUGIN_EVENT_DEATH;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
@@ -8,6 +11,12 @@ public class NoDeathMessage implements Listener {
 
     @EventHandler
     public void onSpawn(PlayerDeathEvent e) {
+        Player p = e.getEntity().getPlayer();
+        String loc = "";
+        Location l = p.getLocation();
+        loc += l.getX() + " " + l.getY() + " " + l.getZ();
+        
+        p.sendMessage(String.format(PLUGIN_EVENT_DEATH, loc));
         e.setKeepLevel(true);
         e.setDroppedExp(0);
         e.setDeathMessage(null);
