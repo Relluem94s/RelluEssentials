6968dd89f7ccd41dd2740ae84f29379aaa445d92
Relluem94 <github@relluem94.de>
2020-12-25 22:14:42 +0100

    added save home on death

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/events/NoDeathMessage.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/events/NoDeathMessage.java
@@ -1,5 +1,6 @@
 package main.java.de.relluem94.minecraft.server.spigot.essentials.events;
 
+import static main.java.de.relluem94.minecraft.server.spigot.essentials.RelluEssentials.players;
 import static main.java.de.relluem94.minecraft.server.spigot.essentials.Strings.PLUGIN_EVENT_DEATH;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
@@ -15,6 +16,13 @@ public class NoDeathMessage implements Listener {
         String loc = "";
         Location l = p.getLocation();
         loc += l.getX() + " " + l.getY() + " " + l.getZ();
+        
+        players.getConfig().set("player." + p.getUniqueId() + ".home.death.x", l.getX());
+        players.getConfig().set("player." + p.getUniqueId() + ".home.death.y", l.getBlockY());
+        players.getConfig().set("player." + p.getUniqueId() + ".home.death.z", l.getBlockZ());
+        players.getConfig().set("player." + p.getUniqueId() + ".home.death.yaw", l.getYaw());
+        players.getConfig().set("player." + p.getUniqueId() + ".home.death.pitch", l.getPitch());
+        players.getConfig().set("player." + p.getUniqueId() + ".home.death.world", l.getWorld().getName());
         
         p.sendMessage(String.format(PLUGIN_EVENT_DEATH, loc));
         e.setKeepLevel(true);
