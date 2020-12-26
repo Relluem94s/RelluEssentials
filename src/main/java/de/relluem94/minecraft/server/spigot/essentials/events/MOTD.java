7d740dc095be4d200115edfa8072ffa568a033a2
Relluem94 <github@relluem94.de>
2020-12-26 01:39:20 +0100

    added motd from config.yml

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/events/MOTD.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/events/MOTD.java
@@ -1,15 +1,18 @@
 package main.java.de.relluem94.minecraft.server.spigot.essentials.events;
 
+import main.java.de.relluem94.minecraft.server.spigot.essentials.RelluEssentials;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.server.ServerListPingEvent;
-import static main.java.de.relluem94.minecraft.server.spigot.essentials.Strings.*;
 
 public class MOTD implements Listener {
 
     @EventHandler
     public void onSpawn(ServerListPingEvent e) {
-        e.setMotd(PLUGIN_EVENT_MOTD);
-        e.setMaxPlayers(PLUGIN_EVENT_MOTD_MAXPLAYERS);
+        String message = RelluEssentials.getPlugin(RelluEssentials.class).getConfig().getString("motd.message") + "";
+        int players = RelluEssentials.getPlugin(RelluEssentials.class).getConfig().getInt("motd.players");
+        
+        e.setMotd(message);
+        e.setMaxPlayers(players);
     }
 }
