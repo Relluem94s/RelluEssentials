f16ada768c0dd0be5277efabbcc6d38a713687b3
Relluem94 <github@relluem94.de>
2020-12-26 01:39:43 +0100

    removed motd

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/Strings.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/Strings.java
@@ -92,8 +92,4 @@ public class Strings {
     public static final String PLUGIN_EVENT_SKILL_REPAIR_DONE = PLUGIN_PREFIX + PLUGIN_SPACER + PLUGIN_COMMAND_COLOR + "Du hast den Gegenstand repariert!";
     public static final String PLUGIN_EVENT_SKILL_REPAIR_WARNING = PLUGIN_PREFIX + PLUGIN_SPACER + PLUGIN_COMMAND_COLOR + "Du benötigst mehr" + PLUGIN_COMMAND_ARG_COLOR + " %s " + PLUGIN_COMMAND_COLOR + "um diesen Gegenstand zu reparieren!";
     public static final String PLUGIN_EVENT_SKILL_SALVAGE_DONE = PLUGIN_PREFIX + PLUGIN_SPACER + PLUGIN_COMMAND_COLOR + "Du hast" + PLUGIN_COMMAND_ARG_COLOR + " %s " + PLUGIN_COMMAND_COLOR + "erhalten!";
-
-    public static final String PLUGIN_EVENT_MOTD = "§6powered by " + PLUGIN_PREFIX;
-    public static final int PLUGIN_EVENT_MOTD_MAXPLAYERS = 94;
-
 }
