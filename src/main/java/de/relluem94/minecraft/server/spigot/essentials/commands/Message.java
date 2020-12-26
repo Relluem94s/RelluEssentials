dfa35745002a6291d3150e66403da0026752235f
Relluem94 <github@relluem94.de>
2020-12-26 20:55:17 +0100

    added reply

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/commands/Message.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/commands/Message.java
@@ -1,5 +1,7 @@
 package main.java.de.relluem94.minecraft.server.spigot.essentials.commands;
 
+import java.util.HashMap;
+import java.util.Map;
 import static main.java.de.relluem94.minecraft.server.spigot.essentials.Strings.*;
 
 import org.bukkit.Bukkit;
@@ -13,34 +15,62 @@ import main.java.de.relluem94.minecraft.server.spigot.essentials.permissions.enu
 
 public class Message implements CommandExecutor {
 
+    Map<Player, Player> reply = new HashMap<>();
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (command.getName().equalsIgnoreCase("msg")) {
             if (args.length > 1) {
                 Player target = Bukkit.getPlayer(args[0]);
                 if (target != null) {
-                    if (sender instanceof Player) {
-                        Player p = (Player) sender;
-                        if (Permission.isAuthorized(p, Groups.USER.getId())) {
-                            String message = "";
-                            for (int i = 1; args.length > i; i++) {
-                                if(args[i] == null){
-                                    break;
-                                }
-                                message += args[i] + " ";
-                            }
-                            target.sendMessage(p.getCustomName() + PLUGIN_COMMAND_MSG_SPACER_IN  + message);
-                            p.sendMessage(target.getCustomName() + PLUGIN_COMMAND_MSG_SPACER_OUT + message);
-                            return true;
-                        }
+                    msg(sender, target, args, 1);
+                }
+            } else {
+                if (sender instanceof Player) {
+                    Player p = (Player) sender;
+                    p.sendMessage(PLUGIN_COMMAND_MSG_INFO);
+                }
+            }
+        } else if (command.getName().equalsIgnoreCase("r")) {
+            if (sender instanceof Player) {
+                Player p = (Player) sender;
+                if (reply.containsKey(p)) {
+                    Player target = reply.get(p);
+                    if (args.length > 1) {
+                        msg(sender, target, args, 0);
                     }
                 }
             }
-            else {
-                if (sender instanceof Player) {
-                        Player p = (Player) sender;
-                        p.sendMessage(PLUGIN_COMMAND_MSG_INFO);
+
+        }
+        return false;
+    }
+
+    private boolean msg(CommandSender sender, Player target, String[] args, int start) {
+        if (sender instanceof Player) {
+            Player p = (Player) sender;
+            if (Permission.isAuthorized(p, Groups.USER.getId())) {
+                String message = "";
+                for (int i = start; args.length > i; i++) {
+                    if (args[i] == null) {
+                        break;
+                    }
+                    message += args[i] + " ";
                 }
+                
+                if(reply.containsKey(p)){
+                    reply.remove(p);
+                }
+                if(reply.containsKey(target)){
+                    reply.remove(target);
+                }
+                
+                reply.put(p, target);
+                reply.put(target, p);
+                
+                target.sendMessage(p.getCustomName() + PLUGIN_COMMAND_MSG_SPACER_IN + message);
+                p.sendMessage(target.getCustomName() + PLUGIN_COMMAND_MSG_SPACER_OUT + message);
+                return true;
             }
         }
         return false;
