b5246a129ccf15242eb6cdc085fbda063bf9d1a6
Relluem94 <github@relluem94.de>
2020-12-27 02:19:53 +0100

    added where command

---
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/commands/Where.java
@@ -0,0 +1,49 @@
+package main.java.de.relluem94.minecraft.server.spigot.essentials.commands;
+
+import static main.java.de.relluem94.minecraft.server.spigot.essentials.Strings.*;
+import static main.java.de.relluem94.minecraft.server.spigot.essentials.helpers.StringHelper.locationToString;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import main.java.de.relluem94.minecraft.server.spigot.essentials.permissions.Permission;
+import main.java.de.relluem94.minecraft.server.spigot.essentials.permissions.enums.Groups;
+
+public class Where implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("where")) {
+            if (args.length == 0) {
+                if (sender instanceof Player) {
+                    Player p = (Player) sender;
+                    if (Permission.isAuthorized(p, Groups.USER.getId())) {
+                        p.sendMessage(PLUGIN_PREFIX + PLUGIN_SPACER + locationToString(p.getLocation()));
+                        return true;
+                    } else {
+                        p.sendMessage(PLUGIN_COMMAND_PERMISSION_MISSING);
+                        return true;
+                    }
+                }
+            } else {
+                Player target = Bukkit.getPlayer(args[0]);
+                if (target != null) {
+                    if (sender instanceof Player) {
+                        Player p = (Player) sender;
+                        if (Permission.isAuthorized(p, Groups.MOD.getId())) {
+                            p.sendMessage(PLUGIN_PREFIX + PLUGIN_SPACER + locationToString(target.getLocation()));
+                            return true;
+                        } else {
+                            p.sendMessage(PLUGIN_COMMAND_PERMISSION_MISSING);
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
