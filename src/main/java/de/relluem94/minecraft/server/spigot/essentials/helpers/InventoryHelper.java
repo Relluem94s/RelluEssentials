d4371ef68bcf1bea2a361d812b081304decc3aa6
Relluem94 <github@relluem94.de>
2020-12-30 21:08:59 +0100

    added InventoryHelper

---
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/helpers/InventoryHelper.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main.java.de.relluem94.minecraft.server.spigot.essentials.helpers;
+
+/**
+ *
+ * @author rellu
+ */
+public class InventoryHelper {
+    
+    public static int inventorySize(int size){
+        
+        int[] sizes = new int[7];
+        sizes[0] = 9;
+        sizes[1] = 18;
+        sizes[2] = 27;
+        sizes[3] = 36;
+        sizes[4] = 45;
+        sizes[5] = 54;
+        
+        int actualSize = 0;
+        for(int i = 5; sizes[i] >= size; i --){
+            actualSize = sizes[i];
+        }
+        
+        return actualSize;
+    }
+    
+}
