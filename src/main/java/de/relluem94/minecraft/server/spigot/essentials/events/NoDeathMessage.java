922136b3cfcda9ff77099c1d6ef8f89088c28d34
Relluem94 <github@relluem94.de>
2020-12-30 20:35:21 +0100

    implemented set and getLocation

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/events/NoDeathMessage.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/events/NoDeathMessage.java
@@ -13,17 +13,11 @@ public class NoDeathMessage implements Listener {
     @EventHandler
     public void onSpawn(PlayerDeathEvent e) {
         Player p = e.getEntity().getPlayer();
-        String loc = "";
+        
         Location l = p.getLocation();
-        loc += l.getX() + " " + l.getY() + " " + l.getZ();
+        players.setLocation("player." + p.getUniqueId() + ".home.death", l);
         
-        players.getConfig().set("player." + p.getUniqueId() + ".home.death.x", l.getX());
-        players.getConfig().set("player." + p.getUniqueId() + ".home.death.y", l.getBlockY());
-        players.getConfig().set("player." + p.getUniqueId() + ".home.death.z", l.getBlockZ());
-        players.getConfig().set("player." + p.getUniqueId() + ".home.death.yaw", l.getYaw());
-        players.getConfig().set("player." + p.getUniqueId() + ".home.death.pitch", l.getPitch());
-        players.getConfig().set("player." + p.getUniqueId() + ".home.death.world", l.getWorld().getName());
-        
+        String loc = l.getX() + " " + l.getY() + " " + l.getZ();
         p.sendMessage(String.format(PLUGIN_EVENT_DEATH, loc));
         e.setKeepLevel(true);
         e.setDroppedExp(0);
