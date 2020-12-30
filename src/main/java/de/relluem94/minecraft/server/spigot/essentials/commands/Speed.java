2b187597b00ca1698764bc8357ca870b60c5d76f
Relluem94 <github@relluem94.de>
2020-12-30 20:00:24 +0100

    added speed command

---
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/commands/Speed.java
@@ -0,0 +1,48 @@
+package main.java.de.relluem94.minecraft.server.spigot.essentials.commands;
+
+import static main.java.de.relluem94.minecraft.server.spigot.essentials.Strings.*;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import main.java.de.relluem94.minecraft.server.spigot.essentials.permissions.Permission;
+import main.java.de.relluem94.minecraft.server.spigot.essentials.permissions.enums.Groups;
+
+public class Speed implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("speed")) {
+            if (args.length == 1) {
+                if (sender instanceof Player) {
+                    Player p = (Player) sender;
+                    if (Permission.isAuthorized(p, Groups.MOD.getId())) {
+                        return true;
+                    } else {
+                        p.sendMessage(PLUGIN_COMMAND_PERMISSION_MISSING);
+                        return true;
+                    }
+                }
+            } else {
+                Player target = Bukkit.getPlayer(args[0]);
+                if (target != null) {
+                    if (sender instanceof Player) {
+                        Player p = (Player) sender;
+                        if (Permission.isAuthorized(p, Groups.MOD.getId())) {
+                            return true;
+                        } else {
+                            p.sendMessage(PLUGIN_COMMAND_PERMISSION_MISSING);
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+}
