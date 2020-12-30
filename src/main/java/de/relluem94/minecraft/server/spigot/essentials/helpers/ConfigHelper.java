dcc8dee1ad6d03d9ad1387e6dfb0bc105c960743
Relluem94 <github@relluem94.de>
2020-12-30 20:32:56 +0100

    added set and getLocation for ConfigHelper

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/helpers/ConfigHelper.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/helpers/ConfigHelper.java
@@ -9,6 +9,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import main.java.de.relluem94.minecraft.server.spigot.essentials.RelluEssentials;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.YamlConfiguration;
 
@@ -55,4 +57,28 @@ public class ConfigHelper {
     public void reload() throws IOException, FileNotFoundException, InvalidConfigurationException {
         config.load(file);
     }
+
+    public void setLocation(String path, Location l) {
+        config.set(path + ".x", l.getX());
+        config.set(path + ".y", l.getBlockY());
+        config.set(path + ".z", l.getBlockZ());
+        config.set(path + ".yaw", l.getYaw());
+        config.set(path + ".pitch", l.getPitch());
+        config.set(path + ".world", l.getWorld().getName());
+    }
+
+    public Location getLocation(String path) {
+        double x, y, z;
+        float yaw, pitch;
+        String world;
+        x = config.getDouble(path + ".x");
+        y = config.getDouble(path + ".y");
+        z = config.getDouble(path + ".z");
+        yaw = (float) config.getDouble(path + ".yaw");
+        pitch = (float) config.getDouble(path + ".pitch");
+        world = config.getString(path + ".world");
+
+        return new Location(Bukkit.getWorld(world), x, y, z, yaw, pitch);
+    }
+
 }
