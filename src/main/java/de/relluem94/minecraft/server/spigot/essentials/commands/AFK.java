9d042fedacf524bbe72b8789be8f1e8f8e68ad16
Relluem94 <github@relluem94.de>
2021-01-03 12:10:36 +0100

    added afk method

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/commands/AFK.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/commands/AFK.java
@@ -1,7 +1,7 @@
 package de.relluem94.minecraft.server.spigot.essentials.commands;
 
-import static de.relluem94.minecraft.server.spigot.essentials.RelluEssentials.players;
 import static de.relluem94.minecraft.server.spigot.essentials.Strings.*;
+import de.relluem94.minecraft.server.spigot.essentials.helpers.PlayerHelper;
 
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
@@ -16,48 +16,32 @@ public class AFK implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (args.length == 0) {
-            if (sender instanceof Player) {
-                Player p = (Player) sender;
-                if (Permission.isAuthorized(p, Groups.USER.getId())) {
-                    return setAFK(command, p);
-                } else {
-                    p.sendMessage(PLUGIN_COMMAND_PERMISSION_MISSING);
-                    return true;
-                }
-            }
-        } else {
-            Player target = Bukkit.getPlayer(args[0]);
-            if (target != null) {
+        if (command.getName().equalsIgnoreCase("afk")) {
+            if (args.length == 0) {
                 if (sender instanceof Player) {
                     Player p = (Player) sender;
-                    if (Permission.isAuthorized(p, Groups.MOD.getId())) {
-                        return setAFK(command, target);
+                    if (Permission.isAuthorized(p, Groups.USER.getId())) {
+                        return PlayerHelper.setAFK(p, false);
                     } else {
                         p.sendMessage(PLUGIN_COMMAND_PERMISSION_MISSING);
                         return true;
+                    }
+                }
+            } else {
+                Player target = Bukkit.getPlayer(args[0]);
+                if (target != null) {
+                    if (sender instanceof Player) {
+                        Player p = (Player) sender;
+                        if (Permission.isAuthorized(p, Groups.MOD.getId())) {
+                            return PlayerHelper.setAFK(target, false);
+                        } else {
+                            p.sendMessage(PLUGIN_COMMAND_PERMISSION_MISSING);
+                            return true;
+                        }
                     }
                 }
             }
         }
         return false;
     }
-
-    private boolean setAFK(Command command, Player p) {
-        if (command.getName().equalsIgnoreCase("afk")) {
-            boolean isAFK = true;
-            
-            if(players.getConfig().get("player." + p.getUniqueId() + ".afk") != null){
-                isAFK = players.getConfig().getBoolean("player." + p.getUniqueId() + ".afk");
-            }
-            players.getConfig().set("player." + p.getUniqueId() + ".afk", !isAFK);
-            p.setInvulnerable(!isAFK);
-            p.setPlayerListName((!isAFK ? "§c[AFK] " : "") + p.getCustomName());
-            Bukkit.broadcastMessage(String.format(PLUGIN_COMMAND_AFK, p.getCustomName(), !isAFK ? PLUGIN_COMMAND_AFK_ACTIVATED : PLUGIN_COMMAND_AFK_DEACTIVATED));
-            return true;
-        } else {
-            return false;
-        }
-    }
-
 }
