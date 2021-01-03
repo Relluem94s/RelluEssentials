70d1965f78e089afe68b8e6175ba94430eba8c0a
Relluem94 <github@relluem94.de>
2021-01-03 12:10:28 +0100

    added afk method

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/helpers/PlayerHelper.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/helpers/PlayerHelper.java
@@ -1,9 +1,12 @@
 package de.relluem94.minecraft.server.spigot.essentials.helpers;
 
+import static de.relluem94.minecraft.server.spigot.essentials.RelluEssentials.players;
+import static de.relluem94.minecraft.server.spigot.essentials.Strings.PLUGIN_COMMAND_AFK;
+import static de.relluem94.minecraft.server.spigot.essentials.Strings.PLUGIN_COMMAND_AFK_ACTIVATED;
+import static de.relluem94.minecraft.server.spigot.essentials.Strings.PLUGIN_COMMAND_AFK_DEACTIVATED;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufAllocator;
 import java.io.IOException;
-import static de.relluem94.minecraft.server.spigot.essentials.RelluEssentials.players;
 import de.relluem94.minecraft.server.spigot.essentials.permissions.User;
 import de.relluem94.minecraft.server.spigot.essentials.permissions.enums.Groups;
 import net.minecraft.server.v1_16_R3.EntityPlayer;
@@ -11,7 +14,9 @@ import net.minecraft.server.v1_16_R3.IChatBaseComponent;
 import net.minecraft.server.v1_16_R3.IChatBaseComponent.ChatSerializer;
 import net.minecraft.server.v1_16_R3.PacketDataSerializer;
 import net.minecraft.server.v1_16_R3.PacketPlayOutPlayerListHeaderFooter;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.command.Command;
 import org.bukkit.craftbukkit.v1_16_R3.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
@@ -22,7 +27,7 @@ import org.bukkit.entity.Player;
 public class PlayerHelper {
 
     /**
-     * 
+     *
      * @param p Player to set Flying
      */
     public static void setFlying(Player p) {
@@ -36,8 +41,28 @@ public class PlayerHelper {
         }
     }
 
+    public static boolean setAFK(Player p, boolean join) {
+
+        boolean isAFK = true;
+
+        if (players.getConfig().get("player." + p.getUniqueId() + ".afk") != null) {
+            isAFK = players.getConfig().getBoolean("player." + p.getUniqueId() + ".afk");
+        }
+
+        if (!join) {
+            players.getConfig().set("player." + p.getUniqueId() + ".afk", !isAFK);
+            Bukkit.broadcastMessage(String.format(PLUGIN_COMMAND_AFK, p.getCustomName(), !isAFK ? PLUGIN_COMMAND_AFK_ACTIVATED : PLUGIN_COMMAND_AFK_DEACTIVATED));
+        } else {
+            isAFK = !isAFK; // Invert for double invertion ^_^
+        }
+
+        p.setInvulnerable(!isAFK);
+        p.setPlayerListName((!isAFK ? "§c[AFK] " : "") + p.getCustomName());
+        return true;
+    }
+
     /**
-     * 
+     *
      * @param p Player to send TabList
      * @param header String Header Text
      * @param footer String Footer Text
