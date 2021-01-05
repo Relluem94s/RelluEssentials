92cd701afd37cbfc2aba9a53cfff4d7404d8f150
Relluem94 <github@relluem94.de>
2021-01-05 23:00:42 +0100

    Update createBlockHistory.sql

--- src/main/resources/sqls/patches/v1.0/createBlockHistory.sql
+++ src/main/resources/sqls/patches/v1.0/createBlockHistory.sql
@@ -19,8 +19,8 @@ CREATE TABLE IF NOT EXISTS `rellu_essentials`.`block_history` (
   `player_fk` INT NOT NULL,
   `material` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`ID`, `location_fk`, `player_fk`, `material`),
-  INDEX `fk_block_history_1_idx` (`location_fk` ASC) VISIBLE,
-  INDEX `fk_block_history_2_idx` (`player_fk` ASC) VISIBLE,
+  INDEX `fk_block_history_1_idx` (`location_fk` ASC),
+  INDEX `fk_block_history_2_idx` (`player_fk` ASC),
   CONSTRAINT `fk_block_history_1`
     FOREIGN KEY (`location_fk`)
     REFERENCES `rellu_essentials`.`location` (`id`),
