01b8e8d600097ce747b46d1fbdb9fb77b25c3e48
Relluem94 <github@relluem94.de>
2021-01-05 22:18:24 +0100

    removed customname from insertPlayer

--- src/main/resources/sqls/insertPlayer.sql
+++ src/main/resources/sqls/insertPlayer.sql
@@ -3,4 +3,4 @@
  * Created: 05.01.2021
  */
 
-INSERT INTO `rellu_essentials`.`player` (`created`, `createdby`, `uuid`, `group_fk`, `customname`) VALUES (now(), ?, ?, ?, ?);
+INSERT INTO `rellu_essentials`.`player` (`created`, `createdby`, `uuid`, `group_fk`) VALUES (now(), ?, ?, ?);
