316c20599d1be98ac09d2b09db57693000c2a92a
Relluem94 <github@relluem94.de>
2021-01-07 21:11:18 +0100

    fixed spacing

--- src/main/resources/sqls/patches/v1.0/dump/full_dump.sql
+++ src/main/resources/sqls/patches/v1.0/dump/full_dump.sql
@@ -180,4 +180,4 @@ COMMIT;
 -- -----------------------------------------------------
 -- Data for table `rellu_essentials`.`plugin_informations`
 -- -----------------------------------------------------
-INSERT INTO `rellu_essentials`.`plugin_informations` (`id`, `created`, `createdby`, `tab_header`, `tab_footer`, `motd_message`, `motd_players`, `db_version`) VALUES (1, '2021-01-05 20:57:05', 1, "§7<<------------------>>          §6Relluem94.de          §7<<------------------>> ", , "§7<<------------------>> §6powered by §8Rellu§cEssentials §7<<------------------>>", "§6powered by §8Rellu§cEssentials", 94, 1);
+INSERT INTO `rellu_essentials`.`plugin_informations` (`id`, `created`, `createdby`, `tab_header`, `tab_footer`, `motd_message`, `motd_players`, `db_version`) VALUES (1, '2021-01-05 20:57:05', 1, "§7<<------------------>>          §6Relluem94.de           §7<<------------------>>", , "§7<<------------------>> §6powered by §8Rellu§cEssentials §7<<------------------>>", "§6powered by §8Rellu§cEssentials", 94, 1);
