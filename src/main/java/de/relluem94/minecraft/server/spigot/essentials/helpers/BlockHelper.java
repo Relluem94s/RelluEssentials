7196a2355081b1d2de9e091525587b97759fca36
Relluem94 <github@relluem94.de>
2021-01-11 21:28:02 +0100

    added BlockHelper to set Blocks

---
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/helpers/BlockHelper.java
@@ -0,0 +1,20 @@
+package de.relluem94.minecraft.server.spigot.essentials.helpers;
+
+import de.relluem94.minecraft.server.spigot.essentials.helpers.pojo.BlockHistoryEntry;
+import org.bukkit.Material;
+
+/**
+ *
+ * @author rellu
+ */
+public class BlockHelper {
+    
+    public static void setBlock(BlockHistoryEntry bh){
+        Material m = Material.getMaterial(bh.getMaterial());
+        if(m == null){
+            m = Material.AIR;
+        }
+        bh.getLocation().getLocation().getBlock().setType(m);
+    }
+    
+}
