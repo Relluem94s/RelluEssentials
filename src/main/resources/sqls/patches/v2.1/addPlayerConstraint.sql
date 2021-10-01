fa9470894dfffbddc40028fa8fdd6bbc473535cb
Relluem94 <github@relluem94.de>
2021-10-02 01:59:50 +0200

    changes

--- src/main/resources/sqls/patches/v2.1/addPlayerConstraint.sql
+++ src/main/resources/sqls/patches/v2.1/addPlayerConstraint.sql
@@ -1,2 +1,7 @@
+/**
+ * Author:  rellu
+ * Created: 02.10.2021
+ */
+
 ALTER TABLE `rellu_essentials`.`player`
 ADD CONSTRAINT `fk_player_group_1` FOREIGN KEY (group_fk) REFERENCES `rellu_essentials`.`group` (`id`);
