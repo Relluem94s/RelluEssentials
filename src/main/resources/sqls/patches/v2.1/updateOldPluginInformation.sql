34c7c6539761554f149f1cdc4fbe68b3db146496
Relluem94 <github@relluem94.de>
2021-10-02 01:54:59 +0200

    added 2.1 migration scripts

--- src/main/resources/sqls/patches/v2.1/updateOldPluginInformation.sql
+++ src/main/resources/sqls/patches/v2.1/updateOldPluginInformation.sql
@@ -3,4 +3,4 @@
  * Created: 10.01.2021
  */
 
-UPDATE `plugin_informations` SET `deleted` = now(), `deletedby` = 1 WHERE `plugin_informations`.`DELETEDBY` IS NULL and `db_version` <> 2 ;
+UPDATE `plugin_informations` SET `deleted` = now(), `deletedby` = 1 WHERE `plugin_informations`.`DELETEDBY` IS NULL and `db_version` <> 3 ;
