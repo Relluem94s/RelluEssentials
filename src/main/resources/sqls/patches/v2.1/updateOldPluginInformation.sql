07e2e577f6748dd201b6a16d8668d0df3f565967
Relluem94 <github@relluem94.de>
2021-10-02 01:59:57 +0200

    changes

--- src/main/resources/sqls/patches/v2.1/updateOldPluginInformation.sql
+++ src/main/resources/sqls/patches/v2.1/updateOldPluginInformation.sql
@@ -1,6 +1,6 @@
 /**
  * Author:  rellu
- * Created: 10.01.2021
+ * Created: 02.10.2021
  */
 
 UPDATE `plugin_informations` SET `deleted` = now(), `deletedby` = 1 WHERE `plugin_informations`.`DELETEDBY` IS NULL and `db_version` <> 3 ;
