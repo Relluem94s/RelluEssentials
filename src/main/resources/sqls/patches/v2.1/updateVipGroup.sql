c62f91830de6fb859d581b633c31c094784de7d4
Relluem94 <github@relluem94.de>
2021-10-02 03:32:16 +0200

    added Update Script for VIP

---
+++ src/main/resources/sqls/patches/v2.1/updateVipGroup.sql
@@ -0,0 +1,8 @@
+/**
+ * Author:  rellu
+ * Created: 02.10.2021
+ */
+
+UPDATE `rellu_essentials`.`group` 
+SET id = 32
+WHERE id = 2;
---
+++ src/main/resources/sqls/patches/v2.1/updateVipGroupPlayer.sql
@@ -0,0 +1,8 @@
+/**
+ * Author:  rellu
+ * Created: 02.10.2021
+ */
+
+UPDATE `rellu_essentials`.`player` 
+SET group_fk = 32
+WHERE group_fk = 2;
