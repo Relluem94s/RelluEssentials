5a4f07fe1f3cbaede1caee3446184065a2360d59
Relluem94 <github@relluem94.de>
2021-10-11 19:31:03 +0200

    added comments and refactor

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/helpers/ItemHelper.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/helpers/ItemHelper.java
@@ -10,64 +10,103 @@ import org.bukkit.inventory.meta.ItemMeta;
  *
  * @author rellu
  */
-public class ItemHelper{
-       
+public class ItemHelper {
+
     private final ItemStack is;
     private final Material material;
     private final int amount;
     private final String displayName;
-    private List<String> lore;
-    private ItemType it;
+    private final ItemType it;
     
-    public ItemHelper(Material material, int amount, String displayName, ItemType itemType){
+    private List<String> lore;
+
+    
+    /**
+     * 
+     * @param material Bukkit Material
+     * @param amount Integer
+     * @param displayName String
+     * @param itemType ItemType
+     */
+    public ItemHelper(Material material, int amount, String displayName, ItemType itemType) {
         this.amount = amount;
         this.material = material;
         this.displayName = displayName;
         this.it = itemType;
-        
+
         is = new ItemStack(this.material, this.amount);
         ItemMeta im = is.getItemMeta();
         im.setDisplayName(this.displayName);
         is.setItemMeta(im);
     }
-    
-    public ItemHelper(Material material, int amount, String displayName, ItemType itemType, List<String> lore){
+
+    /**
+     * 
+     * @param material Bukkit Material
+     * @param amount Integer
+     * @param displayName String
+     * @param itemType ItemType
+     * @param lore List String
+     */
+    public ItemHelper(Material material, int amount, String displayName, ItemType itemType, List<String> lore) {
         this.amount = amount;
         this.material = material;
         this.displayName = displayName;
         this.lore = lore;
         this.it = itemType;
-        
+
         is = new ItemStack(this.material, this.amount);
         ItemMeta im = is.getItemMeta();
         im.setDisplayName(this.displayName);
         im.setLore(this.lore);
         is.setItemMeta(im);
     }
-    
-    public ItemStack getCustomItem(){
+
+    /**
+     * 
+     * @return Bukkit ItemStack
+     */
+    public ItemStack getCustomItem() {
         return is;
     }
 
+    /**
+     * 
+     * @return Amount of ItemHelper
+     */
     public int getAmount() {
         return amount;
     }
 
+    /**
+     * 
+     * @return DisplayName of ItemHelper
+     */
     public String getDisplayName() {
         return displayName;
     }
 
+    /**
+     * 
+     * @return Lore (String List) of ItemHelper
+     */
     public List<String> getLore() {
         return lore;
     }
 
+    /**
+     * 
+     * @return Bukkit Material of ItemHelper
+     */
     public Material getMaterial() {
         return material;
     }
 
+    /**
+     *
+     * @return ItemType of ItemHelper
+     */
     public ItemType getItemType() {
         return it;
     }
-    
-    
 }
