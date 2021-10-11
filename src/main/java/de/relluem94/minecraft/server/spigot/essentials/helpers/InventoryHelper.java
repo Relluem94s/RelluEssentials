c8bec33c7f5c019110e3c8c50c6b6eb7bdeb0150
Relluem94 <github@relluem94.de>
2021-10-11 19:19:28 +0200

    added more InventoryHelper methods

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/helpers/InventoryHelper.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/helpers/InventoryHelper.java
@@ -1,6 +1,8 @@
 package de.relluem94.minecraft.server.spigot.essentials.helpers;
 
 import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 
 /**
@@ -41,5 +43,38 @@ public class InventoryHelper {
     public static Inventory createInventory(int size, String name){
         return Bukkit.createInventory(null, size, name);
     }
+
+    /**
+     * 
+     * @param sender Updates Inventory for CommandSender / Player 
+     */
+    public static void updateInventory(CommandSender sender) {
+        if (sender instanceof Player) {
+            Player p = (Player) sender;
+            p.updateInventory();
+        }
+    }
+
+    /**
+     * 
+     * @param sender Closes Inventory for CommandSender / Player
+     */
+    public static void closeInventory(CommandSender sender) {
+        if (sender instanceof Player) {
+            Player p = (Player) sender;
+            p.closeInventory();
+        } 
+    }
     
+    /**
+     * 
+     * @param sender Opens Inventory for CommandSender / Player
+     * @param inv Inventory to Open
+     */
+    public static void openInventory(CommandSender sender, Inventory inv) {
+        if (TypeHelper.isPlayer(sender)) {
+            Player p = (Player) sender;
+            p.openInventory(inv);
+        }
+    }
 }
