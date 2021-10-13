36a2a722bfd9e834607f359f2d3c6d9118ec636e
Relluem94 <github@relluem94.de>
2021-10-13 21:35:22 +0200

    added sendMessage

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/commands/AFK.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/commands/AFK.java
@@ -1,7 +1,9 @@
 package de.relluem94.minecraft.server.spigot.essentials.commands;
 
 import static de.relluem94.minecraft.server.spigot.essentials.Strings.*;
+import static de.relluem94.minecraft.server.spigot.essentials.helpers.ChatHelper.sendMessage;
 import de.relluem94.minecraft.server.spigot.essentials.helpers.PlayerHelper;
+import static de.relluem94.minecraft.server.spigot.essentials.helpers.TypeHelper.isPlayer;
 
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
@@ -18,24 +20,24 @@ public class AFK implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (command.getName().equalsIgnoreCase("afk")) {
             if (args.length == 0) {
-                if (sender instanceof Player) {
+                if (isPlayer(sender)) {
                     Player p = (Player) sender;
                     if (Permission.isAuthorized(p, Groups.getGroup("user").getId())) {
                         return PlayerHelper.setAFK(p, false);
                     } else {
-                        p.sendMessage(PLUGIN_COMMAND_PERMISSION_MISSING);
+                        sendMessage(p, PLUGIN_COMMAND_PERMISSION_MISSING);
                         return true;
                     }
                 }
             } else {
                 Player target = Bukkit.getPlayer(args[0]);
                 if (target != null) {
-                    if (sender instanceof Player) {
+                    if (isPlayer(sender)) {
                         Player p = (Player) sender;
                         if (Permission.isAuthorized(p, Groups.getGroup("mod").getId())) {
                             return PlayerHelper.setAFK(target, false);
                         } else {
-                            p.sendMessage(PLUGIN_COMMAND_PERMISSION_MISSING);
+                            sendMessage(p, PLUGIN_COMMAND_PERMISSION_MISSING);
                             return true;
                         }
                     }
