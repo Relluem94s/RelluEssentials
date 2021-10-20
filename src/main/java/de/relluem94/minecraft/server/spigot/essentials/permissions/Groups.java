c3b9212303dbeea2cd2794b2d069b6deb4f3f11b
Relluem94 <github@relluem94.de>
2021-10-20 22:31:49 +0200

    added java docs

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/permissions/Groups.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/permissions/Groups.java
@@ -11,6 +11,13 @@ import de.relluem94.minecraft.server.spigot.essentials.helpers.pojo.GroupEntry;
  */
 public class Groups {
 
+    /**
+     * Returns a GroupEntry if a Group is found by name<br>
+     * Returns null if no Group is found
+     * 
+     * @param name String
+     * @return GroupEntry
+     */
     public static GroupEntry getGroup(String name) {
         for (GroupEntry ge : RelluEssentials.groupEntryList) {
             if (ge.getName().equalsIgnoreCase(name)) {
@@ -20,6 +27,13 @@ public class Groups {
         return null;
     }
 
+    /**
+     * Returns a GroupEntry if a Group is found by id<br>
+     * Returns null if no Group is found
+     * 
+     * @param id int
+     * @return GroupEntry
+     */
     public static GroupEntry getGroup(int id) {
         for (GroupEntry ge : RelluEssentials.groupEntryList) {
             if (ge.getId() == id) {
@@ -29,9 +43,15 @@ public class Groups {
         return null;
     }
 
-    public static boolean addGroup(GroupEntry g) {
-        if (getGroup(g.getName()) == null) {
-            dBH.insertGroup(g);
+    /**
+     * Returns true if GroupEntry is in Database else false
+     * 
+     * @param groupEntry GroupEntry
+     * @return boolean
+     */
+    public static boolean addGroup(GroupEntry groupEntry) {
+        if (getGroup(groupEntry.getName()) == null) {
+            dBH.insertGroup(groupEntry);
             groupEntryList.addAll(dBH.getGroups());
             return true;
         } else {
