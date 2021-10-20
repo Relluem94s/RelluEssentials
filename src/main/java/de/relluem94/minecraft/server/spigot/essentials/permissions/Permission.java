454fe0622e3b515110a9d1e7275ade05b27c5cbb
Relluem94 <github@relluem94.de>
2021-10-20 22:39:54 +0200

    added java docs

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/permissions/Permission.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/permissions/Permission.java
@@ -1,27 +1,44 @@
 package de.relluem94.minecraft.server.spigot.essentials.permissions;
 
-import org.bukkit.command.BlockCommandSender;
 import org.bukkit.command.CommandSender;
-import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
+
+
+import static de.relluem94.minecraft.server.spigot.essentials.helpers.TypeHelper.isCMDBlock;
+import static de.relluem94.minecraft.server.spigot.essentials.helpers.TypeHelper.isConsole;
+import static de.relluem94.minecraft.server.spigot.essentials.helpers.TypeHelper.isPlayer;
 
 public class Permission {
 
+    /**
+     * Checks if Player is Authorized
+     * Player has to be in a Group with id >= group
+     * 
+     * @param p Player
+     * @param group long
+     * @return 
+     */
     public static boolean isAuthorized(Player p, long group) {
         long id = User.getGroup(p).getId();
         return id >= group;
     }
 
+    /**
+     * Checks if CommandSender is Authorized
+     * 
+     * @param sender CommandSender
+     * @param group long
+     * @return boolean
+     */
     public static boolean isAuthorized(CommandSender sender, long group) {
-        if (sender instanceof Player) {
-            Player p = (Player) sender;
-            return isAuthorized(p, group);
-        } else if (sender instanceof BlockCommandSender) {
+        if (isPlayer(sender)) {
+            return isAuthorized((Player) sender, group);
+        } else if (isCMDBlock(sender)) {
             return true;
-        } else if (sender instanceof ConsoleCommandSender) {
+        } else if (isConsole(sender)) {
             return true;
-        } else {
-            return false;
-        }
+        } 
+        
+        return false;
     }
 }
