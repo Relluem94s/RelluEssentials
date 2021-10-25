f2d5b7582294a45cca2d838ce05de1be3afe8b63
Relluem94 <github@relluem94.de>
2021-10-25 22:56:55 +0200

    changes

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/CustomItems.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/CustomItems.java
@@ -5,7 +5,13 @@ import de.relluem94.minecraft.server.spigot.essentials.helpers.ItemHelper;
 import de.relluem94.minecraft.server.spigot.essentials.helpers.enums.ItemType;
 import java.util.Arrays;
 import org.bukkit.Color;
+import org.bukkit.DyeColor;
 import org.bukkit.Material;
+import org.bukkit.block.Banner;
+import org.bukkit.block.banner.Pattern;
+import org.bukkit.block.banner.PatternType;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.meta.BlockStateMeta;
 import org.bukkit.inventory.meta.LeatherArmorMeta;
 
 /**
@@ -13,15 +19,93 @@ import org.bukkit.inventory.meta.LeatherArmorMeta;
  * @author rellu
  */
 public class CustomItems {
-    public static ItemHelper cloudSailor = new ItemHelper(Material.FEATHER, 1, PLUGIN_ITEM_CLOUDSAILOR, ItemType.HUB, Arrays.asList(new String[] {PLUGIN_ITEM_CLOUDSAILOR_LORE1, PLUGIN_ITEM_CLOUDSAILOR_LORE2}));
-    public static ItemHelper cloudBoots = new ItemHelper(Material.LEATHER_BOOTS, 1, PLUGIN_ITEM_CLOUDBOOTS, ItemType.HUB, Arrays.asList(new String[] {PLUGIN_ITEM_CLOUDBOOTS_LORE1, PLUGIN_ITEM_CLOUDBOOTS_LORE2})){
+    public static ItemHelper cloudSailor = new ItemHelper(Material.FEATHER, 1, PLUGIN_ITEM_CLOUDSAILOR, ItemType.RPG, Arrays.asList(new String[] {PLUGIN_ITEM_CLOUDSAILOR_LORE1, PLUGIN_ITEM_CLOUDSAILOR_LORE2}));
+    public static ItemHelper cloudBoots = new ItemHelper(Material.LEATHER_BOOTS, 1, PLUGIN_ITEM_CLOUDBOOTS, ItemType.RPG, Arrays.asList(new String[] {PLUGIN_ITEM_CLOUDBOOTS_LORE1, PLUGIN_ITEM_CLOUDBOOTS_LORE2})){
         @Override
         public void init(){
             LeatherArmorMeta cloudBootsMeta = (LeatherArmorMeta) getItemMeta();
-            cloudBootsMeta.setDisplayName(PLUGIN_ITEM_CLOUDBOOTS);
             cloudBootsMeta.setColor(Color.SILVER);
             cloudBootsMeta.setUnbreakable(true);
             setItemMeta(cloudBootsMeta);
+        }
+    };
+    
+    
+    
+    public static ItemHelper relluHelmet = new ItemHelper(Material.LEATHER_HELMET, 1, PLUGIN_ITEM_RELLU_HELMET, ItemType.RPG){
+        @Override
+        public void init(){
+            LeatherArmorMeta lam = (LeatherArmorMeta) getItemMeta();
+            lam.setColor(Color.fromRGB(243, 125, 0));
+            lam.addEnchant(Enchantment.DAMAGE_ALL, 94, true);
+            lam.addEnchant(Enchantment.THORNS, 94, true);
+            lam.addEnchant(Enchantment.LOOT_BONUS_MOBS, 94, true);
+            lam.addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, 94, true);
+            lam.setUnbreakable(true);
+            setItemMeta(lam);
+        }
+    };
+    
+    public static ItemHelper relluChestPlate = new ItemHelper(Material.LEATHER_CHESTPLATE, 1, PLUGIN_ITEM_RELLU_CHESTPLATE, ItemType.RPG){
+        @Override
+        public void init(){
+            LeatherArmorMeta lam = (LeatherArmorMeta) getItemMeta();
+            lam.setColor(Color.fromRGB(72, 179, 177));
+            lam.addEnchant(Enchantment.DAMAGE_ALL, 94, true);
+            lam.addEnchant(Enchantment.THORNS, 94, true);
+            lam.addEnchant(Enchantment.LOOT_BONUS_MOBS, 94, true);
+            lam.addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, 94, true);
+            lam.setUnbreakable(true);
+            setItemMeta(lam);
+        }
+    };
+    
+    public static ItemHelper relluLeggings = new ItemHelper(Material.LEATHER_LEGGINGS, 1, PLUGIN_ITEM_RELLU_LEGGINGS, ItemType.RPG){
+        @Override
+        public void init(){
+            LeatherArmorMeta lam = (LeatherArmorMeta) getItemMeta();
+            lam.setColor(Color.fromRGB(152, 216, 1));
+            lam.addEnchant(Enchantment.DAMAGE_ALL, 94, true);
+            lam.addEnchant(Enchantment.THORNS, 94, true);
+            lam.addEnchant(Enchantment.LOOT_BONUS_MOBS, 94, true);
+            lam.addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, 94, true);
+            lam.setUnbreakable(true);
+            setItemMeta(lam);
+        }
+    };
+    
+    public static ItemHelper relluBoots = new ItemHelper(Material.LEATHER_BOOTS, 1, PLUGIN_ITEM_RELLU_BOOTS, ItemType.RPG){
+        @Override
+        public void init(){
+            LeatherArmorMeta lam = (LeatherArmorMeta) getItemMeta();
+            lam.setColor(Color.fromRGB(227, 59, 46));
+            lam.addEnchant(Enchantment.DAMAGE_ALL, 94, true);
+            lam.addEnchant(Enchantment.THORNS, 94, true);
+            lam.addEnchant(Enchantment.LOOT_BONUS_MOBS, 94, true);
+            lam.addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, 94, true);
+            lam.setUnbreakable(true);
+            setItemMeta(lam);
+        }
+    };
+    
+    public static ItemHelper relluShield = new ItemHelper(Material.SHIELD, 1, PLUGIN_ITEM_RELLU_SHIELD, ItemType.RPG){
+        @Override
+        public void init(){
+            BlockStateMeta bmeta = (BlockStateMeta) getItemMeta();
+            
+            Banner banner = (Banner) bmeta.getBlockState();
+            banner.setBaseColor(DyeColor.PURPLE);
+            
+            banner.update();
+
+            bmeta.setBlockState(banner);
+            
+            bmeta.addEnchant(Enchantment.DAMAGE_ALL, 94, true);
+            bmeta.addEnchant(Enchantment.THORNS, 94, true);
+            bmeta.addEnchant(Enchantment.LOOT_BONUS_MOBS, 94, true);
+            bmeta.addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, 94, true);
+            bmeta.setUnbreakable(true);
+            setItemMeta(bmeta);
         }
     };
 }
