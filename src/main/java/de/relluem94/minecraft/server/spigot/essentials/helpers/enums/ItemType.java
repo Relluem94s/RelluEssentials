63a7bde051cf432fd285a5673f14a8b1ef42cfc5
Relluem94 <github@relluem94.de>
2021-10-28 22:29:17 +0200

    removed IItemType as interfacing doesn't work here.

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/helpers/enums/ItemType.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/helpers/enums/ItemType.java
@@ -1,16 +1,16 @@
 package de.relluem94.minecraft.server.spigot.essentials.helpers.enums;
 
-import de.relluem94.minecraft.server.spigot.essentials.helpers.interfaces.IItemType;
-
 /**
  *
  * @author rellu
  */
-public enum ItemType implements IItemType {
+public enum ItemType {
     TOOL,
     GADGET,
     ARMOR,
     WEAPON,
     HUB,
+    DECORATION,
+    BUILDING,
     NONE;
 }
--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/helpers/interfaces/IItemType.java
+++
@@ -1,7 +0,0 @@
-package de.relluem94.minecraft.server.spigot.essentials.helpers.interfaces;
-
-/**
- *
- * @author rellu
- */
-public interface IItemType {}
