555591d792bd899eac5371f663764b4503807adc
Relluem94 <github@relluem94.de>
2021-10-28 21:57:36 +0200

    changed ItemTypes

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/CustomItems.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/CustomItems.java
@@ -18,8 +18,8 @@ import org.bukkit.inventory.meta.LeatherArmorMeta;
  * @author rellu
  */
 public class CustomItems {
-    public static ItemHelper cloudSailor = new ItemHelper(Material.FEATHER, 1, PLUGIN_ITEM_CLOUDSAILOR, ItemType.RPG, ItemRarity.EPIC, Arrays.asList(new String[] {PLUGIN_ITEM_CLOUDSAILOR_LORE1, PLUGIN_ITEM_CLOUDSAILOR_LORE2}));
-    public static ItemHelper cloudBoots = new ItemHelper(Material.LEATHER_BOOTS, 1, PLUGIN_ITEM_CLOUDBOOTS, ItemType.RPG,  ItemRarity.LEGENDARY, Arrays.asList(new String[] {PLUGIN_ITEM_CLOUDBOOTS_LORE1, PLUGIN_ITEM_CLOUDBOOTS_LORE2})){
+    public static ItemHelper cloudSailor = new ItemHelper(Material.FEATHER, 1, PLUGIN_ITEM_CLOUDSAILOR, ItemType.GADGET, ItemRarity.EPIC, Arrays.asList(new String[] {PLUGIN_ITEM_CLOUDSAILOR_LORE1, PLUGIN_ITEM_CLOUDSAILOR_LORE2}));
+    public static ItemHelper cloudBoots = new ItemHelper(Material.LEATHER_BOOTS, 1, PLUGIN_ITEM_CLOUDBOOTS, ItemType.ARMOR,  ItemRarity.LEGENDARY, Arrays.asList(new String[] {PLUGIN_ITEM_CLOUDBOOTS_LORE1, PLUGIN_ITEM_CLOUDBOOTS_LORE2})){
         @Override
         public void init(){
             LeatherArmorMeta cloudBootsMeta = (LeatherArmorMeta) getItemMeta();
@@ -31,7 +31,7 @@ public class CustomItems {
     
     
     
-    public static ItemHelper relluHelmet = new ItemHelper(Material.LEATHER_HELMET, 1, PLUGIN_ITEM_RELLU_HELMET, ItemType.RPG, ItemRarity.LEGENDARY){
+    public static ItemHelper relluHelmet = new ItemHelper(Material.LEATHER_HELMET, 1, PLUGIN_ITEM_RELLU_HELMET, ItemType.ARMOR, ItemRarity.LEGENDARY){
         @Override
         public void init(){
             LeatherArmorMeta lam = (LeatherArmorMeta) getItemMeta();
@@ -45,7 +45,7 @@ public class CustomItems {
         }
     };
     
-    public static ItemHelper relluChestPlate = new ItemHelper(Material.LEATHER_CHESTPLATE, 1, PLUGIN_ITEM_RELLU_CHESTPLATE, ItemType.RPG, ItemRarity.LEGENDARY){
+    public static ItemHelper relluChestPlate = new ItemHelper(Material.LEATHER_CHESTPLATE, 1, PLUGIN_ITEM_RELLU_CHESTPLATE, ItemType.ARMOR, ItemRarity.LEGENDARY){
         @Override
         public void init(){
             LeatherArmorMeta lam = (LeatherArmorMeta) getItemMeta();
@@ -59,7 +59,7 @@ public class CustomItems {
         }
     };
     
-    public static ItemHelper relluLeggings = new ItemHelper(Material.LEATHER_LEGGINGS, 1, PLUGIN_ITEM_RELLU_LEGGINGS, ItemType.RPG, ItemRarity.LEGENDARY){
+    public static ItemHelper relluLeggings = new ItemHelper(Material.LEATHER_LEGGINGS, 1, PLUGIN_ITEM_RELLU_LEGGINGS, ItemType.ARMOR, ItemRarity.LEGENDARY){
         @Override
         public void init(){
             LeatherArmorMeta lam = (LeatherArmorMeta) getItemMeta();
@@ -73,7 +73,7 @@ public class CustomItems {
         }
     };
     
-    public static ItemHelper relluBoots = new ItemHelper(Material.LEATHER_BOOTS, 1, PLUGIN_ITEM_RELLU_BOOTS, ItemType.RPG, ItemRarity.LEGENDARY){
+    public static ItemHelper relluBoots = new ItemHelper(Material.LEATHER_BOOTS, 1, PLUGIN_ITEM_RELLU_BOOTS, ItemType.ARMOR, ItemRarity.LEGENDARY){
         @Override
         public void init(){
             LeatherArmorMeta lam = (LeatherArmorMeta) getItemMeta();
@@ -87,7 +87,7 @@ public class CustomItems {
         }
     };
     
-    public static ItemHelper relluShield = new ItemHelper(Material.SHIELD, 1, PLUGIN_ITEM_RELLU_SHIELD, ItemType.RPG, ItemRarity.LEGENDARY){
+    public static ItemHelper relluShield = new ItemHelper(Material.SHIELD, 1, PLUGIN_ITEM_RELLU_SHIELD, ItemType.ARMOR, ItemRarity.LEGENDARY){
         @Override
         public void init(){
             BlockStateMeta bmeta = (BlockStateMeta) getItemMeta();
