fcb30796a815bce3fc4881f7ca1d917fc70a5e94
Relluem94 <github@relluem94.de>
2021-10-30 00:07:52 +0200

    added String for Smelter Pickaxe

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/Strings.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/Strings.java
@@ -32,6 +32,8 @@ public class Strings {
     public static final String PLUGIN_ITEM_RELLU_SWORD = "§eRellu's Sword";
     public static final String PLUGIN_ITEM_RELLU_SHIELD = "§5Rellu's Shield";
     
+    public static final String PLUGIN_ITEM_AUTOSMELTER = "§cSmelter Pickaxe";
+    
     public static final String PLUGIN_ITEM_CLOUDBOOTS = "§bCloud Boots";
     public static final String PLUGIN_ITEM_CLOUDBOOTS_LORE1 = "§bGrants gliding abillity if worn.";
     public static final String PLUGIN_ITEM_CLOUDBOOTS_LORE2 = "§bAlso reduces Fall Damage by 100%";
