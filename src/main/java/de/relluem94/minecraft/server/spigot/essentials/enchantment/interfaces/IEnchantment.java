b6ed0cc5c40d2dbd884c89f91dacb9047e0de5aa
Relluem94 <github@relluem94.de>
2021-10-29 22:50:43 +0200

    added interface for enchantments

---
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/enchantment/interfaces/IEnchantment.java
@@ -0,0 +1,11 @@
+package de.relluem94.minecraft.server.spigot.essentials.enchantment.interfaces;
+
+import org.bukkit.inventory.ItemStack;
+
+/**
+ *
+ * @author rellu
+ */
+public interface IEnchantment {
+    public void addTo(ItemStack i);
+}
