645576c4b4a382d9a0f694a2e75b2e3c6adfaa07
Relluem94 <github@relluem94.de>
2021-10-30 01:45:19 +0200

    prevent smelting if pickaxe is empty

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/events/skills/Ev_AutoSmelt.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/events/skills/Ev_AutoSmelt.java
@@ -3,6 +3,11 @@ package de.relluem94.minecraft.server.spigot.essentials.events.skills;
 import de.relluem94.minecraft.server.spigot.essentials.CustomItems;
 import static de.relluem94.minecraft.server.spigot.essentials.RelluEssentials.autosmelt;
 import static de.relluem94.minecraft.server.spigot.essentials.RelluEssentials.telekenesis;
+import static de.relluem94.minecraft.server.spigot.essentials.Strings.PLUGIN_ENCHANTMENT_AUTOSMELT_LAVA_TANK;
+import static de.relluem94.minecraft.server.spigot.essentials.Strings.PLUGIN_ENCHANTMENT_AUTOSMELT_LAVA_TANK_FUEL_LEFT;
+import static de.relluem94.minecraft.server.spigot.essentials.Strings.PLUGIN_ENCHANTMENT_AUTOSMELT_LAVA_TANK_FUEL_LEFT_SEPERATOR;
+import static de.relluem94.minecraft.server.spigot.essentials.helpers.ChatHelper.sendMessage;
+import java.util.List;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
@@ -11,40 +16,68 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class Ev_AutoSmelt implements Listener {
-    
+
     @EventHandler
     public void preventPlacementOfFurnace(PlayerInteractEvent e) {
-        if(e.getPlayer().getInventory().getItemInMainHand().equals(CustomItems.autoSmeltFurnace.getCustomItem()) || e.getPlayer().getInventory().getItemInOffHand().equals(CustomItems.autoSmeltFurnace.getCustomItem())){
+        if (e.getPlayer().getInventory().getItemInMainHand().equals(CustomItems.autoSmeltFurnace.getCustomItem()) || e.getPlayer().getInventory().getItemInOffHand().equals(CustomItems.autoSmeltFurnace.getCustomItem())) {
             e.setCancelled(true);
         }
     }
+
+    
     
     @EventHandler
     public void onBreakSmelt(BlockBreakEvent e) {
         Player p = e.getPlayer();
 
         Block b = e.getBlock(); //TODO Check blockfaces for items like torches.
-        
-        if (p.getInventory().getItemInMainHand().getEnchantments().containsKey(autosmelt)) {
-            if (p.getInventory().getItemInMainHand().getEnchantments().containsKey(telekenesis)) {
-                if (p.getInventory().firstEmpty() >= 0) {
-                    for (ItemStack im : b.getDrops()) {
-                        p.getInventory().addItem(smelt(im));
-                    }
 
-                } else {
-                    for (ItemStack im : b.getDrops()) {
-                        p.getWorld().dropItem(p.getLocation(), smelt(im));
+        if (p.getInventory().getItemInMainHand().getEnchantments().containsKey(autosmelt)) {
+
+            boolean hasFuel = true;
+            int fuel = 0;
+            ItemMeta im = p.getInventory().getItemInMainHand().getItemMeta();
+            List<String> lore = im.getLore();
+            for (int i = 0; i < lore.size(); i++) {
+                if (lore.get(i).startsWith(PLUGIN_ENCHANTMENT_AUTOSMELT_LAVA_TANK)) {
+                    fuel = Integer.parseInt(lore.get(i).replace(PLUGIN_ENCHANTMENT_AUTOSMELT_LAVA_TANK, "").split(PLUGIN_ENCHANTMENT_AUTOSMELT_LAVA_TANK_FUEL_LEFT_SEPERATOR)[0]);
+                    if(fuel == 0){
+                        hasFuel = false;
                     }
-                }
-            } else {
-                for (ItemStack im : b.getDrops()) {
-                    b.getWorld().dropItemNaturally(b.getLocation(), smelt(im));
+                    else{
+                        fuel--;
+                        lore.set(i, PLUGIN_ENCHANTMENT_AUTOSMELT_LAVA_TANK + String.format(PLUGIN_ENCHANTMENT_AUTOSMELT_LAVA_TANK_FUEL_LEFT, fuel));
+                    }
                 }
             }
-            e.setDropItems(false);
+            im.setLore(lore);
+            p.getInventory().getItemInMainHand().setItemMeta(im);
+            
+            if (hasFuel) {
+                if (p.getInventory().getItemInMainHand().getEnchantments().containsKey(telekenesis)) {
+                    if (p.getInventory().firstEmpty() >= 0) {
+                        for (ItemStack is : b.getDrops()) {
+                            p.getInventory().addItem(smelt(is));
+                        }
+
+                    } else {
+                        for (ItemStack is : b.getDrops()) {
+                            p.getWorld().dropItem(p.getLocation(), smelt(is));
+                        }
+                    }
+                } else {
+                    for (ItemStack is : b.getDrops()) {
+                        b.getWorld().dropItemNaturally(b.getLocation(), smelt(is));
+                    }
+                }
+                e.setDropItems(false);
+            } else {
+                sendMessage(p, "No Fuel Left in your Smelter Pickaxe thingi, better text in Strings ;)");
+            }
+
         } else {
             e.setDropItems(true);
         }
