4ff554e305648eb8a79df25cf73cf97bb9fae9bb
Relluem94 <github@relluem94.de>
2021-10-30 05:10:44 +0200

    format

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/CustomItems.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/CustomItems.java
@@ -20,10 +20,11 @@ import org.bukkit.inventory.meta.LeatherArmorMeta;
  * @author rellu
  */
 public class CustomItems {
-    public static ItemHelper cloudSailor = new ItemHelper(Material.FEATHER, 1, PLUGIN_ITEM_CLOUDSAILOR, ItemType.GADGET, ItemRarity.EPIC, Arrays.asList(new String[] {PLUGIN_ITEM_CLOUDSAILOR_LORE1, PLUGIN_ITEM_CLOUDSAILOR_LORE2}));
-    public static ItemHelper cloudBoots = new ItemHelper(Material.LEATHER_BOOTS, 1, PLUGIN_ITEM_CLOUDBOOTS, ItemType.ARMOR,  ItemRarity.LEGENDARY, Arrays.asList(new String[] {PLUGIN_ITEM_CLOUDBOOTS_LORE1, PLUGIN_ITEM_CLOUDBOOTS_LORE2})){
+
+    public static ItemHelper cloudSailor = new ItemHelper(Material.FEATHER, 1, PLUGIN_ITEM_CLOUDSAILOR, ItemType.GADGET, ItemRarity.EPIC, Arrays.asList(new String[]{PLUGIN_ITEM_CLOUDSAILOR_LORE1, PLUGIN_ITEM_CLOUDSAILOR_LORE2}));
+    public static ItemHelper cloudBoots = new ItemHelper(Material.LEATHER_BOOTS, 1, PLUGIN_ITEM_CLOUDBOOTS, ItemType.ARMOR, ItemRarity.LEGENDARY, Arrays.asList(new String[]{PLUGIN_ITEM_CLOUDBOOTS_LORE1, PLUGIN_ITEM_CLOUDBOOTS_LORE2})) {
         @Override
-        public void init(){
+        public void init() {
             LeatherArmorMeta cloudBootsMeta = (LeatherArmorMeta) getItemMeta();
             cloudBootsMeta.setColor(Color.SILVER);
             cloudBootsMeta.setUnbreakable(true);
@@ -31,30 +32,29 @@ public class CustomItems {
             setItemMeta(cloudBootsMeta);
         }
     };
-    
-    public static ItemHelper autoSmeltNetheritePickAxe = new ItemHelper(Material.NETHERITE_PICKAXE, 1, PLUGIN_ITEM_AUTOSMELTER, ItemType.TOOL,  ItemRarity.LEGENDARY){
+
+    public static ItemHelper autoSmeltNetheritePickAxe = new ItemHelper(Material.NETHERITE_PICKAXE, 1, PLUGIN_ITEM_AUTOSMELTER, ItemType.TOOL, ItemRarity.LEGENDARY) {
         @Override
-        public void init(){
+        public void init() {
             ItemMeta im = getItemMeta();
-            im.setUnbreakable(true);              
+            im.setUnbreakable(true);
             setItemMeta(im);
             autosmelt.addTo(getItemStack());
         }
     };
-    
-     public static ItemHelper autoSmeltTank = new ItemHelper(Material.GLASS_BOTTLE, 1, PLUGIN_ITEM_AUTOSMELTER_TANK, ItemType.INGREDIENT,  ItemRarity.RARE, Arrays.asList(new String[] {PLUGIN_ITEM_INGREDIENT})); 
-     public static ItemHelper autoSmeltFurnace = new ItemHelper(Material.FURNACE, 1, PLUGIN_ITEM_AUTOSMELTER_FURNACE, ItemType.INGREDIENT,  ItemRarity.RARE, Arrays.asList(new String[] {PLUGIN_ITEM_INGREDIENT})); 
-      
+
+    public static ItemHelper autoSmeltTank = new ItemHelper(Material.GLASS_BOTTLE, 1, PLUGIN_ITEM_AUTOSMELTER_TANK, ItemType.INGREDIENT, ItemRarity.RARE, Arrays.asList(new String[]{PLUGIN_ITEM_INGREDIENT}));
+    public static ItemHelper autoSmeltFurnace = new ItemHelper(Material.FURNACE, 1, PLUGIN_ITEM_AUTOSMELTER_FURNACE, ItemType.INGREDIENT, ItemRarity.RARE, Arrays.asList(new String[]{PLUGIN_ITEM_INGREDIENT}));
 
     /**
-     * 
+     *
      * Internal Testing Stuff
-     * 
-     *     
+     *
+     *
      */
-    public static ItemHelper relluHelmet = new ItemHelper(Material.LEATHER_HELMET, 1, PLUGIN_ITEM_RELLU_HELMET, ItemType.ARMOR, ItemRarity.LEGENDARY){
+    public static ItemHelper relluHelmet = new ItemHelper(Material.LEATHER_HELMET, 1, PLUGIN_ITEM_RELLU_HELMET, ItemType.ARMOR, ItemRarity.LEGENDARY) {
         @Override
-        public void init(){
+        public void init() {
             LeatherArmorMeta lam = (LeatherArmorMeta) getItemMeta();
             lam.setColor(Color.fromRGB(243, 125, 0));
             lam.addEnchant(Enchantment.THORNS, 94, true);
@@ -64,10 +64,10 @@ public class CustomItems {
             setItemMeta(lam);
         }
     };
-    
-    public static ItemHelper relluChestPlate = new ItemHelper(Material.LEATHER_CHESTPLATE, 1, PLUGIN_ITEM_RELLU_CHESTPLATE, ItemType.ARMOR, ItemRarity.LEGENDARY){
+
+    public static ItemHelper relluChestPlate = new ItemHelper(Material.LEATHER_CHESTPLATE, 1, PLUGIN_ITEM_RELLU_CHESTPLATE, ItemType.ARMOR, ItemRarity.LEGENDARY) {
         @Override
-        public void init(){
+        public void init() {
             LeatherArmorMeta lam = (LeatherArmorMeta) getItemMeta();
             lam.setColor(Color.fromRGB(72, 179, 177));
             lam.addEnchant(Enchantment.THORNS, 94, true);
@@ -77,10 +77,10 @@ public class CustomItems {
             setItemMeta(lam);
         }
     };
-    
-    public static ItemHelper relluLeggings = new ItemHelper(Material.LEATHER_LEGGINGS, 1, PLUGIN_ITEM_RELLU_LEGGINGS, ItemType.ARMOR, ItemRarity.LEGENDARY){
+
+    public static ItemHelper relluLeggings = new ItemHelper(Material.LEATHER_LEGGINGS, 1, PLUGIN_ITEM_RELLU_LEGGINGS, ItemType.ARMOR, ItemRarity.LEGENDARY) {
         @Override
-        public void init(){
+        public void init() {
             LeatherArmorMeta lam = (LeatherArmorMeta) getItemMeta();
             lam.setColor(Color.fromRGB(152, 216, 1));
             lam.addEnchant(Enchantment.THORNS, 94, true);
@@ -90,10 +90,10 @@ public class CustomItems {
             setItemMeta(lam);
         }
     };
-    
-    public static ItemHelper relluBoots = new ItemHelper(Material.LEATHER_BOOTS, 1, PLUGIN_ITEM_RELLU_BOOTS, ItemType.ARMOR, ItemRarity.LEGENDARY){
+
+    public static ItemHelper relluBoots = new ItemHelper(Material.LEATHER_BOOTS, 1, PLUGIN_ITEM_RELLU_BOOTS, ItemType.ARMOR, ItemRarity.LEGENDARY) {
         @Override
-        public void init(){
+        public void init() {
             LeatherArmorMeta lam = (LeatherArmorMeta) getItemMeta();
             lam.setColor(Color.fromRGB(227, 59, 46));
             lam.addEnchant(Enchantment.THORNS, 94, true);
@@ -103,10 +103,10 @@ public class CustomItems {
             setItemMeta(lam);
         }
     };
-    
-    public static ItemHelper relluSword = new ItemHelper(Material.DIAMOND_SWORD, 1, PLUGIN_ITEM_RELLU_SWORD, ItemType.WEAPON, ItemRarity.LEGENDARY){
+
+    public static ItemHelper relluSword = new ItemHelper(Material.DIAMOND_SWORD, 1, PLUGIN_ITEM_RELLU_SWORD, ItemType.WEAPON, ItemRarity.LEGENDARY) {
         @Override
-        public void init(){
+        public void init() {
             ItemMeta im = getItemMeta();
             im.addEnchant(Enchantment.FIRE_ASPECT, 94, true);
             im.addEnchant(Enchantment.DAMAGE_ALL, 94, true);
@@ -117,19 +117,19 @@ public class CustomItems {
             setItemMeta(im);
         }
     };
-    
-    public static ItemHelper relluShield = new ItemHelper(Material.SHIELD, 1, PLUGIN_ITEM_RELLU_SHIELD, ItemType.ARMOR, ItemRarity.LEGENDARY){
+
+    public static ItemHelper relluShield = new ItemHelper(Material.SHIELD, 1, PLUGIN_ITEM_RELLU_SHIELD, ItemType.ARMOR, ItemRarity.LEGENDARY) {
         @Override
-        public void init(){
+        public void init() {
             BlockStateMeta bmeta = (BlockStateMeta) getItemMeta();
-            
+
             Banner banner = (Banner) bmeta.getBlockState();
             banner.setBaseColor(DyeColor.PURPLE);
-            
+
             banner.update();
 
             bmeta.setBlockState(banner);
-            
+
             bmeta.addEnchant(Enchantment.DAMAGE_ALL, 94, true);
             bmeta.addEnchant(Enchantment.THORNS, 94, true);
             bmeta.addEnchant(Enchantment.LOOT_BONUS_MOBS, 94, true);
