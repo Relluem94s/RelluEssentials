93f051302769659bf50f7e1629a6ee3f90fbf9b9
Relluem94 <github@relluem94.de>
2021-10-31 19:42:36 +0100

    changed strings to import

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/commands/Rellu.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/commands/Rellu.java
@@ -16,51 +16,53 @@ public class Rellu implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        switch (args.length) {
-            case 1:
-                if (sender instanceof Player) {
-                    Player p = (Player) sender;
-                    if (Permission.isAuthorized(p, Groups.getGroup("admin").getId())) {
-                        if (args[0].equalsIgnoreCase("ping")) {
-                            int ping = ((CraftPlayer) p).getHandle().ping;
-                            p.sendMessage(String.format(PLUGIN_COMMAND_RELLU_PING, ping));
-                            return true;
-
+        if (command.getName().equalsIgnoreCase(PLUGIN_COMMAND_NAME_RELLU)) {
+            switch (args.length) {
+                case 1:
+                    if (sender instanceof Player) {
+                        Player p = (Player) sender;
+                        if (Permission.isAuthorized(p, Groups.getGroup("admin").getId())) {
+                            if (args[0].equalsIgnoreCase(PLUGIN_COMMAND_NAME_RELLU_PING)) {
+                                int ping = ((CraftPlayer) p).getHandle().ping;
+                                p.sendMessage(String.format(PLUGIN_COMMAND_RELLU_PING, ping));
+                                return true;
+                            } else {
+                                p.sendMessage(PLUGIN_COMMAND_RELLU_WRONG_COMMAND);
+                                return true;
+                            }
                         } else {
-                            p.sendMessage(PLUGIN_COMMAND_RELLU_WRONG_COMMAND);
-                            return true;
-                        }
-                    } else {
-                        p.sendMessage(PLUGIN_COMMAND_PERMISSION_MISSING);
-                        return true;
-                    }
-                }
-                break;
-            case 2:
-                if (args[0].equalsIgnoreCase("ping")) {
-                    Player target = Bukkit.getPlayer(args[1]);
-                    if (target != null) {
-                        if (sender instanceof Player) {
-                            Player p = (Player) sender;
-                            int ping = ((CraftPlayer) target).getHandle().ping;
-                            p.sendMessage(String.format(PLUGIN_COMMAND_RELLU_PING_OTHER, target.getCustomName(), ping));
+                            p.sendMessage(PLUGIN_COMMAND_PERMISSION_MISSING);
                             return true;
                         }
                     }
-                }
-                break;
-            default:
-                if (sender instanceof Player) {
-                    Player p = (Player) sender;
-                    if (Permission.isAuthorized(p, Groups.getGroup("admin").getId())) {
-                        p.sendMessage(PLUGIN_COMMAND_RELLU_OPTIONS);
-                        return true;
-                    } else {
-                        p.sendMessage(PLUGIN_COMMAND_PERMISSION_MISSING);
-                        return true;
+                    break;
+                case 2:
+                    if (args[0].equalsIgnoreCase(PLUGIN_COMMAND_NAME_RELLU_PING)) {
+                        Player target = Bukkit.getPlayer(args[1]);
+                        if (target != null) {
+                            if (sender instanceof Player) {
+                                Player p = (Player) sender;
+                                int ping = ((CraftPlayer) target).getHandle().ping;
+                                p.sendMessage(String.format(PLUGIN_COMMAND_RELLU_PING_OTHER, target.getCustomName(), ping));
+                                return true;
+                            }
+                        }
                     }
-                }
-                break;
+                    break;
+                default:
+                    if (sender instanceof Player) {
+                        Player p = (Player) sender;
+                        if (Permission.isAuthorized(p, Groups.getGroup("admin").getId())) {
+                            p.sendMessage(PLUGIN_COMMAND_RELLU_OPTIONS);
+                            return true;
+                        } else {
+                            p.sendMessage(PLUGIN_COMMAND_PERMISSION_MISSING);
+                            return true;
+                        }
+                    }
+                    break;
+            }
+            return false;
         }
         return false;
     }
