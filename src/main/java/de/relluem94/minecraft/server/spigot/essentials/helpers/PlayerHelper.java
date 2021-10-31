302578edc4be94b4f5d93c851db3ad86534e85d4
Relluem94 <github@relluem94.de>
2021-10-31 18:45:36 +0100

    fix cant fly on reload if walking

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/helpers/PlayerHelper.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/helpers/PlayerHelper.java
@@ -9,8 +9,8 @@ import de.relluem94.minecraft.server.spigot.essentials.helpers.pojo.PlayerEntry;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufAllocator;
 import java.io.IOException;
-import de.relluem94.minecraft.server.spigot.essentials.permissions.User;
 import de.relluem94.minecraft.server.spigot.essentials.permissions.Groups;
+import de.relluem94.minecraft.server.spigot.essentials.permissions.Permission;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import net.minecraft.server.v1_16_R3.EntityPlayer;
@@ -19,7 +19,6 @@ import net.minecraft.server.v1_16_R3.IChatBaseComponent.ChatSerializer;
 import net.minecraft.server.v1_16_R3.PacketDataSerializer;
 import net.minecraft.server.v1_16_R3.PacketPlayOutPlayerListHeaderFooter;
 import org.bukkit.Bukkit;
-import org.bukkit.Material;
 import org.bukkit.craftbukkit.v1_16_R3.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
@@ -34,13 +33,11 @@ public class PlayerHelper {
      * @param p Player to set Flying
      */
     public static void setFlying(Player p) {
-        if (User.getGroup(p).getId() >= Groups.getGroup("vip").getId()) {
+        if (Permission.isAuthorized(p, Groups.getGroup("vip").getId())) {
             PlayerEntry pe = playerEntryList.get(p.getUniqueId());
             if (pe.isFlying()) {
                 p.setAllowFlight(true);
-                if (p.getLocation().getBlock().getRelative(0, -1, 0).getType().equals(Material.AIR)) {
-                    p.setFlying(true);
-                }
+                p.setFlying(true);
             }
         }
     }
