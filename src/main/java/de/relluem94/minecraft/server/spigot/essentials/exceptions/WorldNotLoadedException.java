bcf28e8b44accb6027123fe031139d73172511ee
Relluem94 <github@relluem94.de>
2021-11-03 22:35:51 +0100

    added exception from RelluSGA as it belongs to here.

---
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/exceptions/WorldNotLoadedException.java
@@ -0,0 +1,12 @@
+package de.relluem94.minecraft.server.spigot.essentials.exceptions;
+
+/**
+ *
+ * @author rellu
+ */
+public class WorldNotLoadedException extends Exception {
+
+    public WorldNotLoadedException(String message) {
+        super(message);
+    }
+}
