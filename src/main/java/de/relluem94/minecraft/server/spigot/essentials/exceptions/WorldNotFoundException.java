2fea28a80d0fde01de124183d13b0da3ade74819
Relluem94 <github@relluem94.de>
2021-11-04 20:21:19 +0100

    added Exception

---
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/exceptions/WorldNotFoundException.java
@@ -0,0 +1,12 @@
+package de.relluem94.minecraft.server.spigot.essentials.exceptions;
+
+/**
+ *
+ * @author rellu
+ */
+public class WorldNotFoundException extends Exception {
+
+    public WorldNotFoundException(String message) {
+        super(message);
+    }
+}
