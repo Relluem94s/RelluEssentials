a710e5562d5cde10d7bd54a49a0bbce5144ce42d
Relluem94 <github@relluem94.de>
2021-11-05 00:35:16 +0100

    created new class with constants

---
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/constants/DatabaseConstants.java
@@ -0,0 +1,15 @@
+package de.relluem94.minecraft.server.spigot.essentials.constants;
+
+/**
+ *
+ * @author rellu
+ */
+public class DatabaseConstants {
+    
+    /********************************************************************************/
+    /*                           DATABASE   STUFF                                   */
+    /********************************************************************************/
+    
+    public static final String PLUGIN_DATABASE_VERSION = "";
+    public static final String PLUGIN_DATABASE_NAME = "rellu_essentials";
+}
