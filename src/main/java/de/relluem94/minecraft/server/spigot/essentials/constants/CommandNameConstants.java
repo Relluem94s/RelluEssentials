4753c2b401ea8f8088c8ecd7f35464ab6f26bfb3
Relluem94 <github@relluem94.de>
2021-11-04 23:55:25 +0100

    created new class for constants

---
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/constants/CommandNameConstants.java
@@ -0,0 +1,78 @@
+package de.relluem94.minecraft.server.spigot.essentials.constants;
+
+/**
+ *
+ * @author rellu
+ */
+public class CommandNameConstants {
+    
+    /********************************************************************************/
+    /*                            COMMAND   STUFF                                   */
+    /********************************************************************************/
+    
+    public static final String PLUGIN_COMMAND_NAME_AFK = "afk";
+    public static final String PLUGIN_COMMAND_NAME_BROADCAST = "broadcast";
+    public static final String PLUGIN_COMMAND_NAME_BROADCAST_TITLE = "title";
+    public static final String PLUGIN_COMMAND_NAME_CLEARCHAT = "cc";
+    public static final String PLUGIN_COMMAND_NAME_COOCKIE = "cookie";
+    public static final String PLUGIN_COMMAND_NAME_CRAFT = "craft";
+    public static final String PLUGIN_COMMAND_NAME_DAY = "day";
+    public static final String PLUGIN_COMMAND_NAME_ENDERCHEST = "enderchest";
+    public static final String PLUGIN_COMMAND_NAME_FLY = "fly";
+    public static final String PLUGIN_COMMAND_NAME_GAMEMODE_0 = "0";
+    public static final String PLUGIN_COMMAND_NAME_GAMEMODE_0_NAME = "Survival";
+    public static final String PLUGIN_COMMAND_NAME_GAMEMODE_1 = "1";
+    public static final String PLUGIN_COMMAND_NAME_GAMEMODE_1_NAME = "Creative";
+    public static final String PLUGIN_COMMAND_NAME_GAMEMODE_2 = "2";    
+    public static final String PLUGIN_COMMAND_NAME_GAMEMODE_2_NAME = "Adventure";    
+    public static final String PLUGIN_COMMAND_NAME_GAMEMODE_3 = "3";
+    public static final String PLUGIN_COMMAND_NAME_GAMEMODE_3_NAME = "Spectator";
+    public static final String PLUGIN_COMMAND_NAME_GOD = "god";
+    public static final String PLUGIN_COMMAND_NAME_GAMERULES = "gamerules";
+    public static final String PLUGIN_COMMAND_NAME_HEAD = "head";        
+    public static final String PLUGIN_COMMAND_NAME_HEAL = "heal";        
+    public static final String PLUGIN_COMMAND_NAME_HOME = "home"; 
+    public static final String PLUGIN_COMMAND_NAME_HOME_SET = "set"; 
+    public static final String PLUGIN_COMMAND_NAME_HOME_DELETE = "delete";
+    public static final String PLUGIN_COMMAND_NAME_HOME_LIST = "list";
+    public static final String PLUGIN_COMMAND_NAME_INVENTORY = "inv";        
+    public static final String PLUGIN_COMMAND_NAME_MSG = "msg";        
+    public static final String PLUGIN_COMMAND_NAME_REPLY = "r";        
+    public static final String PLUGIN_COMMAND_NAME_MORE = "more";        
+    public static final String PLUGIN_COMMAND_NAME_NICK = "nick";        
+    public static final String PLUGIN_COMMAND_NAME_NIGHT = "night";        
+    public static final String PLUGIN_COMMAND_NAME_SETGROUP = "setGroup";
+    public static final String PLUGIN_COMMAND_NAME_POKE = "poke";
+    public static final String PLUGIN_COMMAND_NAME_PRINT = "print";
+    public static final String PLUGIN_COMMAND_NAME_RAIN = "rain";
+    public static final String PLUGIN_COMMAND_NAME_RELLU = "rellu";
+    public static final String PLUGIN_COMMAND_NAME_RELLU_PING = "ping";
+    public static final String PLUGIN_COMMAND_NAME_RENAME = "rename";
+    public static final String PLUGIN_COMMAND_NAME_REPAIR = "repair";
+    public static final String PLUGIN_COMMAND_NAME_ROLLBACK = "rollback";
+    public static final String PLUGIN_COMMAND_NAME_ROLLBACK_PLAYER = "player";
+    public static final String PLUGIN_COMMAND_NAME_ROLLBACK_UNDO = "undo";
+    public static final String PLUGIN_COMMAND_NAME_ROLLBACK_UNDO_PLAYER = "player";
+    public static final String PLUGIN_COMMAND_NAME_SPAWN = "spawn";
+    public static final String PLUGIN_COMMAND_NAME_WORLD = "world";
+    public static final String PLUGIN_COMMAND_NAME_WORLD_CREATE = "create";
+    public static final String PLUGIN_COMMAND_NAME_WORLD_LOAD = "load";
+    public static final String PLUGIN_COMMAND_NAME_WORLD_LIST = "list";
+    public static final String PLUGIN_COMMAND_NAME_WORLD_UNLOAD = "unload";
+    public static final String PLUGIN_COMMAND_NAME_WORLD_UNLOAD_NO_SAVE = "unloadNoSave";
+    public static final String PLUGIN_COMMAND_NAME_SPEED = "speed";
+    public static final String PLUGIN_COMMAND_NAME_STORM = "storm";
+    public static final String PLUGIN_COMMAND_NAME_SUICIDE = "suicide";
+    public static final String PLUGIN_COMMAND_NAME_SUN = "sun";
+    public static final String PLUGIN_COMMAND_NAME_TEST_COMMAND = "ZAQmNCRXEdwSGU7DvEcXTbBkp2qEaCSSNkQcMhL3m7KSDtmXWaxtbYCaQCFBR96fj";
+    public static final String PLUGIN_COMMAND_NAME_TEST_COMMAND_CUSTOMMOB = "cm";
+    public static final String PLUGIN_COMMAND_NAME_TEST_COMMAND_CLOUDSAILOR = "cs";
+    public static final String PLUGIN_COMMAND_NAME_TEST_COMMAND_RELLU = "rellu";
+    public static final String PLUGIN_COMMAND_NAME_TEST_COMMAND_SMELT = "smelt";
+    public static final String PLUGIN_COMMAND_NAME_TEST_COMMAND_TELE = "tele";
+    public static final String PLUGIN_COMMAND_NAME_TEST_COMMAND_NOENCHANT = "noenchant";
+    public static final String PLUGIN_COMMAND_NAME_TEST_COMMAND_WORLDS = "worlds";
+    public static final String PLUGIN_COMMAND_NAME_TITLE = "title";
+    public static final String PLUGIN_COMMAND_NAME_VANISH = "vanish";
+    public static final String PLUGIN_COMMAND_NAME_WHERE = "where";
+}
