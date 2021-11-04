54ceda5bde89001bf76980309c0fe72f4269e3f8
Relluem94 <github@relluem94.de>
2021-11-05 00:32:31 +0100

    changed instanceof Player to isPlayer()

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/commands/Where.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/commands/Where.java
@@ -12,6 +12,7 @@ import de.relluem94.minecraft.server.spigot.essentials.permissions.Groups;
 import static de.relluem94.minecraft.server.spigot.essentials.Strings.*;
 import static de.relluem94.minecraft.server.spigot.essentials.constants.CommandNameConstants.PLUGIN_COMMAND_NAME_WHERE;
 import static de.relluem94.minecraft.server.spigot.essentials.helpers.StringHelper.locationToString;
+import static de.relluem94.minecraft.server.spigot.essentials.helpers.TypeHelper.isPlayer;
 
 public class Where implements CommandExecutor {
 
@@ -19,7 +20,7 @@ public class Where implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (command.getName().equalsIgnoreCase(PLUGIN_COMMAND_NAME_WHERE)) {
             if (args.length == 0) {
-                if (sender instanceof Player) {
+                if (isPlayer(sender)) {
                     Player p = (Player) sender;
                     if (Permission.isAuthorized(p, Groups.getGroup("user").getId())) {
                         p.sendMessage(String.format(PLUGIN_COMMAND_WHERE, p.getCustomName(), locationToString(p.getLocation())));
@@ -32,7 +33,7 @@ public class Where implements CommandExecutor {
             } else {
                 Player target = Bukkit.getPlayer(args[0]);
                 if (target != null) {
-                    if (sender instanceof Player) {
+                    if (isPlayer(sender)) {
                         Player p = (Player) sender;
                         if (Permission.isAuthorized(p, Groups.getGroup("mod").getId())) {
                             p.sendMessage(String.format(PLUGIN_COMMAND_WHERE, target.getCustomName(), locationToString(target.getLocation())));
