1aa56c0664b8693243b276e3889b7c092c82a749
Relluem94 <github@relluem94.de>
2021-11-04 23:52:14 +0100

    created new ItemConstants Class

---
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/constants/ItemConstants.java
@@ -0,0 +1,38 @@
+package de.relluem94.minecraft.server.spigot.essentials.constants;
+
+/**
+ *
+ * @author rellu
+ */
+public class ItemConstants {
+    
+    /********************************************************************************/
+    /*                              ITEM   STUFF                                    */
+    /********************************************************************************/
+    
+    public static final String PLUGIN_ITEM_NAMESPACE_CLOUD_BOOTS = "cloud_boots";
+    public static final String PLUGIN_ITEM_NAMESPACE_SMELTER_PICKAXE = "smelter_pickaxe";
+    public static final String PLUGIN_ITEM_NAMESPACE_SMELTER_TANK = "smelter_tank";
+    public static final String PLUGIN_ITEM_NAMESPACE_SMELTER_FURNACE = "smelter_furnace";
+    public static final String PLUGIN_ITEM_NAMESPACE_SMELTER_FUEL = "smelter_fuel";
+
+    public static final String PLUGIN_ITEM_RELLU_HELMET = "§6Rellu's Helmet";
+    public static final String PLUGIN_ITEM_RELLU_CHESTPLATE = "§3Rellu's Chestplate";
+    public static final String PLUGIN_ITEM_RELLU_LEGGINGS = "§aRellu's Leggings";
+    public static final String PLUGIN_ITEM_RELLU_BOOTS = "§cRellu's Boots";
+    public static final String PLUGIN_ITEM_RELLU_SWORD = "§eRellu's Sword";
+    public static final String PLUGIN_ITEM_RELLU_SHIELD = "§5Rellu's Shield";
+
+    public static final String PLUGIN_ITEM_INGREDIENT = "§4§l§oThis is a crafting ingredient";
+
+    public static final String PLUGIN_ITEM_AUTOSMELTER = "§cSmelter Pickaxe";
+    public static final String PLUGIN_ITEM_AUTOSMELTER_TANK = "§cSmelter Tank";
+    public static final String PLUGIN_ITEM_AUTOSMELTER_FURNACE = "§cSmelter Furnace";
+
+    public static final String PLUGIN_ITEM_CLOUDBOOTS = "§bCloud Boots";
+    public static final String PLUGIN_ITEM_CLOUDBOOTS_LORE1 = "§bGrants gliding abillity if worn.";
+    public static final String PLUGIN_ITEM_CLOUDBOOTS_LORE2 = "§bAlso reduces Fall Damage by 100%";
+    public static final String PLUGIN_ITEM_CLOUDSAILOR = "§bCloud Sailor";
+    public static final String PLUGIN_ITEM_CLOUDSAILOR_LORE1 = "§bHeld in Off Hand grants gliding abillity.";
+    public static final String PLUGIN_ITEM_CLOUDSAILOR_LORE2 = "§bAlso reduces Fall Damage by 50%";
+}
