dec9eff48c40658591c9cb75017df07668e2833b
Relluem94 <github@relluem94.de>
2021-11-05 00:38:02 +0100

    created new Class for constants

---
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/constants/ExceptionConstants.java
@@ -0,0 +1,15 @@
+package de.relluem94.minecraft.server.spigot.essentials.constants;
+
+/**
+ *
+ * @author rellu
+ */
+public class ExceptionConstants {
+        
+    /********************************************************************************/
+    /*                         EXCEPTION   STUFF                                    */
+    /********************************************************************************/
+    
+    public static final String PLUGIN_EXCEPTION_WORLD_NOT_LOADED = "Can't unload a World (%s) that is not loaded.";
+    public static final String PLUGIN_EXCEPTION_WORLD_NOT_FOUND = "No World found with name: %s";
+}
