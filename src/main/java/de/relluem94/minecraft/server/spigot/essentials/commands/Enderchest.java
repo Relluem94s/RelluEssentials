b3c9558085afd6a80f58abdc9e77dfb9c02e5d05
Relluem94 <github@relluem94.de>
2021-11-05 00:29:15 +0100

    changed imports to new class

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/commands/Enderchest.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/commands/Enderchest.java
@@ -1,7 +1,5 @@
 package de.relluem94.minecraft.server.spigot.essentials.commands;
 
-import static de.relluem94.minecraft.server.spigot.essentials.Strings.*;
-
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -11,13 +9,17 @@ import org.bukkit.entity.Player;
 import de.relluem94.minecraft.server.spigot.essentials.permissions.Permission;
 import de.relluem94.minecraft.server.spigot.essentials.permissions.Groups;
 
+import static de.relluem94.minecraft.server.spigot.essentials.Strings.*;
+import static de.relluem94.minecraft.server.spigot.essentials.constants.CommandNameConstants.PLUGIN_COMMAND_NAME_ENDERCHEST;
+import static de.relluem94.minecraft.server.spigot.essentials.helpers.TypeHelper.isPlayer;
+
 public class Enderchest implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (command.getName().equalsIgnoreCase(PLUGIN_COMMAND_NAME_ENDERCHEST)) {
             if (args.length == 0) {
-                if (sender instanceof Player) {
+                if (isPlayer(sender)) {
                     Player p = (Player) sender;
                     if (Permission.isAuthorized(p, Groups.getGroup("vip").getId())) {
                         p.openInventory(p.getEnderChest());
@@ -31,7 +33,7 @@ public class Enderchest implements CommandExecutor {
             } else {
                 Player target = Bukkit.getOfflinePlayer(args[0]).getPlayer();
                 if (target != null) {
-                    if (sender instanceof Player) {
+                    if (isPlayer(sender)) {
                         Player p = (Player) sender;
                         if (Permission.isAuthorized(p, Groups.getGroup("mod").getId())) {
                             p.openInventory(target.getEnderChest());
