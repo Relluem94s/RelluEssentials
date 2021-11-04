2819832763d62cbca3dc7334c848fe94391ea323
Relluem94 <github@relluem94.de>
2021-11-05 00:31:57 +0100

    changed instanceof Player to isPlayer()

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/commands/Repair.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/commands/Repair.java
@@ -14,6 +14,7 @@ import de.relluem94.minecraft.server.spigot.essentials.permissions.Groups;
 
 import static de.relluem94.minecraft.server.spigot.essentials.Strings.*;
 import static de.relluem94.minecraft.server.spigot.essentials.constants.CommandNameConstants.PLUGIN_COMMAND_NAME_REPAIR;
+import static de.relluem94.minecraft.server.spigot.essentials.helpers.TypeHelper.isPlayer;
 
 public class Repair implements CommandExecutor {
 
@@ -21,7 +22,7 @@ public class Repair implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (command.getName().equalsIgnoreCase(PLUGIN_COMMAND_NAME_REPAIR)) {
             if (args.length == 0) {
-                if (sender instanceof Player) {
+                if (isPlayer(sender)) {
                     Player p = (Player) sender;
                     if (Permission.isAuthorized(p, Groups.getGroup("mod").getId())) {
 
@@ -46,7 +47,7 @@ public class Repair implements CommandExecutor {
             } else {
                 Player target = Bukkit.getPlayer(args[0]);
                 if (target != null) {
-                    if (sender instanceof Player) {
+                    if (isPlayer(sender)) {
                         Player p = (Player) sender;
                         if (Permission.isAuthorized(p, Groups.getGroup("mod").getId())) {
 
