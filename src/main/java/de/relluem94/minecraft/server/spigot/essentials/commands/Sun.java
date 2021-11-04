c856777ac0cf2510f583cae588f0cd85ea0ebdaa
Relluem94 <github@relluem94.de>
2021-11-05 00:32:09 +0100

    changed instanceof Player to isPlayer()

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/commands/Sun.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/commands/Sun.java
@@ -12,6 +12,7 @@ import de.relluem94.minecraft.server.spigot.essentials.permissions.Groups;
 
 import static de.relluem94.minecraft.server.spigot.essentials.Strings.*;
 import static de.relluem94.minecraft.server.spigot.essentials.constants.CommandNameConstants.PLUGIN_COMMAND_NAME_SUN;
+import static de.relluem94.minecraft.server.spigot.essentials.helpers.TypeHelper.isPlayer;
 
 public class Sun implements CommandExecutor {
 
@@ -19,7 +20,7 @@ public class Sun implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (command.getName().equalsIgnoreCase(PLUGIN_COMMAND_NAME_SUN)) {
             if (args.length == 0) {
-                if (sender instanceof Player) {
+                if (isPlayer(sender)) {
                     Player p = (Player) sender;
                     if (Permission.isAuthorized(p, Groups.getGroup("mod").getId())) {
                         p.getWorld().setWeatherDuration(0);
@@ -35,7 +36,7 @@ public class Sun implements CommandExecutor {
             } else {
                 Player target = Bukkit.getPlayer(args[0]);
                 if (target != null) {
-                    if (sender instanceof Player) {
+                    if (isPlayer(sender)) {
                         Player p = (Player) sender;
                         if (Permission.isAuthorized(p, Groups.getGroup("mod").getId())) {
                             target.setPlayerWeather(WeatherType.CLEAR);
