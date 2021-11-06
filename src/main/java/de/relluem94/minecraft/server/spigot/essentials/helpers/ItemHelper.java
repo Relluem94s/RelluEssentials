fbd18721ccd0fda8c4d10b7750da75a7141d6d5b
Relluem94 <github@relluem94.de>
2021-11-06 22:54:30 +0100

    added enum to ItemHelper

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/helpers/ItemHelper.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/helpers/ItemHelper.java
@@ -1,6 +1,5 @@
 package de.relluem94.minecraft.server.spigot.essentials.helpers;
 
-import de.relluem94.minecraft.server.spigot.essentials.helpers.enums.ItemRarity;
 import de.relluem94.minecraft.server.spigot.essentials.helpers.enums.ItemType;
 import de.relluem94.minecraft.server.spigot.essentials.helpers.interfaces.IItemHelper;
 import java.util.ArrayList;
@@ -225,4 +224,59 @@ public class ItemHelper implements IItemHelper {
             is.setItemMeta(im);
         }
     }
+    
+    public enum ItemRarity {
+
+    COMMON("Common", "§f§l", 0),
+    UNCOMMON("Uncommon", "§a§l", 1),
+    RARE("Rare", "§9§l", 2),
+    EPIC("Epic", "§5§l", 3),
+    LEGENDARY("Legendary", "§6§l", 4);
+
+    private final String displayName;
+    private final String prefix;
+    private final int level;
+
+    private ItemRarity(String displayName, String prefix, int level) {
+        this.displayName = displayName;
+        this.prefix = prefix;
+        this.level = level;
+    }
+
+    /**
+     * Returns Displayname of Rarity
+     *
+     * @return String displayName
+     */
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    /**
+     * Returns Prefix of Rarity (Color and Bold)
+     *
+     * @return String prefix
+     */
+    public String getPrefix() {
+        return prefix;
+    }
+
+    /**
+     * Returns Level of Rarity
+     *
+     * @return int level
+     */
+    public int getLevel() {
+        return level;
+    }
+}
+
+    
+    
+    
+    
+    
+    
+    
+    
 }
