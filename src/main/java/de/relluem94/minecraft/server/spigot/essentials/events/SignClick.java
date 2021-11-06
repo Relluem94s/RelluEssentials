9d8bb587a0837d50b2128f90219c27156c809ee0
Relluem94 <github@relluem94.de>
2021-11-06 22:42:21 +0100

    implementing isSign

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/events/SignClick.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/events/SignClick.java
@@ -1,8 +1,5 @@
 package de.relluem94.minecraft.server.spigot.essentials.events;
 
-import de.relluem94.minecraft.server.spigot.essentials.CustomSigns;
-import static de.relluem94.minecraft.server.spigot.essentials.constants.CommandNameConstants.PLUGIN_COMMAND_NAME_HOME;
-import static de.relluem94.minecraft.server.spigot.essentials.constants.CommandNameConstants.PLUGIN_COMMAND_NAME_SPAWN;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Sign;
@@ -12,6 +9,12 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.EquipmentSlot;
 
+import de.relluem94.minecraft.server.spigot.essentials.CustomSigns;
+
+import static de.relluem94.minecraft.server.spigot.essentials.constants.CommandNameConstants.PLUGIN_COMMAND_NAME_HOME;
+import static de.relluem94.minecraft.server.spigot.essentials.constants.CommandNameConstants.PLUGIN_COMMAND_NAME_SPAWN;
+import static de.relluem94.minecraft.server.spigot.essentials.helpers.SignHelper.isSign;
+
 public class SignClick implements Listener {
 
     @EventHandler
@@ -20,24 +23,24 @@ public class SignClick implements Listener {
             if (e.getAction() == Action.RIGHT_CLICK_BLOCK || e.getAction() == Action.LEFT_CLICK_BLOCK) {
                 if (e.getClickedBlock().getType().equals(Material.OAK_WALL_SIGN) || e.getClickedBlock().getType().equals(Material.OAK_SIGN)) {
                     Sign sign = (Sign) e.getClickedBlock().getState();
-                    if(sign.getLine(0).equals(CustomSigns.spawn.getLine1()) && sign.getLine(1).equals(CustomSigns.spawn.getLine2()) && sign.getLine(3).equals(CustomSigns.spawn.getLine4())){
+                    if(isSign(CustomSigns.spawn, sign.getLine(0), sign.getLine(1), sign.getLine(3))){
                         e.getPlayer().performCommand(PLUGIN_COMMAND_NAME_SPAWN);
                     }
-                    else if(sign.getLine(0).equals(CustomSigns.up.getLine1()) && sign.getLine(1).equals(CustomSigns.up.getLine2()) && sign.getLine(3).equals(CustomSigns.up.getLine4())){
+                    else if(isSign(CustomSigns.up, sign.getLine(0), sign.getLine(1), sign.getLine(3))){
                         // GO UP
                     }
-                    else if(sign.getLine(0).equals(CustomSigns.down.getLine1()) && sign.getLine(1).equals(CustomSigns.down.getLine2()) && sign.getLine(3).equals(CustomSigns.down.getLine4())){
+                    else if(isSign(CustomSigns.down, sign.getLine(0), sign.getLine(1), sign.getLine(3))){
                         // DOWN
                     }
-                    else if(sign.getLine(0).equals(CustomSigns.command.getLine1()) && sign.getLine(1).equals(CustomSigns.command.getLine2()) && sign.getLine(3).equals(CustomSigns.command.getLine4())){
+                    else if(isSign(CustomSigns.command, sign.getLine(0), sign.getLine(1), sign.getLine(3))){
                         e.getPlayer().performCommand(sign.getLine(2));
                     }
-                    else if(sign.getLine(0).equals(CustomSigns.teleport.getLine1()) && sign.getLine(1).equals(CustomSigns.teleport.getLine2()) && sign.getLine(3).equals(CustomSigns.teleport.getLine4())){
+                    else if(isSign(CustomSigns.teleport, sign.getLine(0), sign.getLine(1), sign.getLine(3))){
                         String[] locationString = sign.getLine(2).split(",");
                         Location location = new Location(e.getPlayer().getWorld(), Integer.parseInt(locationString[0]), Integer.parseInt(locationString[1]), Integer.parseInt(locationString[2]));
                         e.getPlayer().teleport(location);
                     }
-                    else if(sign.getLine(0).equals(CustomSigns.home.getLine1()) && sign.getLine(1).equals(CustomSigns.home.getLine2()) && sign.getLine(3).equals(CustomSigns.home.getLine4())){
+                    else if(isSign(CustomSigns.home, sign.getLine(0), sign.getLine(1), sign.getLine(3))){
                         e.getPlayer().performCommand(PLUGIN_COMMAND_NAME_HOME + " " + sign.getLine(2));
                     }
                 }
