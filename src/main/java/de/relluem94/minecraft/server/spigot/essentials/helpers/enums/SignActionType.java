4b00330573474aea822fd1a4277ef3d8e309de45
Relluem94 <github@relluem94.de>
2021-11-06 19:49:17 +0100

    added SignActionType Enum

---
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/helpers/enums/SignActionType.java
@@ -0,0 +1,36 @@
+package de.relluem94.minecraft.server.spigot.essentials.helpers.enums;
+
+/**
+ *
+ * @author rellu
+ */
+public enum SignActionType {
+    COMMAND(1, "Command", true),
+    TELEPORT(2, "Teleport", true),
+    SPAWN(3, "Spawn", false),
+    UP(4, "Up", false),
+    DOWN(5, "Down", false);
+    
+    
+    private final int id;
+    private final String displayName;
+    private final boolean customInput;
+
+    private SignActionType(int id, String displayName, boolean test) {
+        this.id = id;
+        this.customInput = test;
+        this.displayName = displayName;
+    }
+
+    public int getId() {
+        return id;
+    }
+    
+    public boolean hasCustomInput() {
+        return customInput;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+}
