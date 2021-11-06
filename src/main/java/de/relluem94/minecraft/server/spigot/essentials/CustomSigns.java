1212716fe5724c69e9e5b7a08e30562cb3c7f901
Relluem94 <github@relluem94.de>
2021-11-06 22:50:15 +0100

    changed to other enum

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/CustomSigns.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/CustomSigns.java
@@ -2,7 +2,7 @@ package de.relluem94.minecraft.server.spigot.essentials;
 
 import de.relluem94.minecraft.server.spigot.essentials.exceptions.SignMissingCustomInputException;
 import de.relluem94.minecraft.server.spigot.essentials.helpers.SignHelper;
-import de.relluem94.minecraft.server.spigot.essentials.helpers.enums.SignActionType;
+import de.relluem94.minecraft.server.spigot.essentials.helpers.SignHelper.ActionType;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -16,7 +16,7 @@ public class CustomSigns {
     static {
         SignHelper temp = null;
         try {
-            temp = new SignHelper(SignActionType.SPAWN);
+            temp = new SignHelper(ActionType.SPAWN);
         } catch (SignMissingCustomInputException ex) {
             Logger.getLogger(CustomSigns.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -28,7 +28,7 @@ public class CustomSigns {
     static {
         SignHelper temp = null;
         try {
-            temp = new SignHelper(SignActionType.UP);
+            temp = new SignHelper(ActionType.UP);
         } catch (SignMissingCustomInputException ex) {
             Logger.getLogger(CustomSigns.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -40,14 +40,14 @@ public class CustomSigns {
     static {
         SignHelper temp = null;
         try {
-            temp = new SignHelper(SignActionType.DOWN);
+            temp = new SignHelper(ActionType.DOWN);
         } catch (SignMissingCustomInputException ex) {
             Logger.getLogger(CustomSigns.class.getName()).log(Level.SEVERE, null, ex);
         }
         down = temp;
     }
     
-    public static SignHelper command = new SignHelper(SignActionType.COMMAND, "");
-    public static SignHelper teleport = new SignHelper(SignActionType.TELEPORT, "");    
-    public static SignHelper home = new SignHelper(SignActionType.HOME, "");
+    public static SignHelper command = new SignHelper(ActionType.COMMAND, "");
+    public static SignHelper teleport = new SignHelper(ActionType.TELEPORT, "");    
+    public static SignHelper home = new SignHelper(ActionType.HOME, "");
 }
