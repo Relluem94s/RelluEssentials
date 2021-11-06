59f31bf7114d809de9aefd00d5bf9ca0a6956fdc
Relluem94 <github@relluem94.de>
2021-11-06 22:42:14 +0100

    implementing isSign

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/events/SignActions.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/events/SignActions.java
@@ -1,11 +1,14 @@
 package de.relluem94.minecraft.server.spigot.essentials.events;
 
-import de.relluem94.minecraft.server.spigot.essentials.CustomSigns;
-import de.relluem94.minecraft.server.spigot.essentials.permissions.Groups;
-import de.relluem94.minecraft.server.spigot.essentials.permissions.Permission;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.SignChangeEvent;
+
+import de.relluem94.minecraft.server.spigot.essentials.CustomSigns;
+import de.relluem94.minecraft.server.spigot.essentials.permissions.Groups;
+import de.relluem94.minecraft.server.spigot.essentials.permissions.Permission;
+
+import static de.relluem94.minecraft.server.spigot.essentials.helpers.SignHelper.isSign;
 
 public class SignActions implements Listener {
 
@@ -13,36 +16,41 @@ public class SignActions implements Listener {
     public void onChangeSignCreateActionSign(SignChangeEvent e) {
         if (Permission.isAuthorized(e.getPlayer(), Groups.getGroup("mod").getId())) {
             if (e.getLine(1) != null) {
-                if (e.getLine(1).equals(CustomSigns.spawn.getSignActionType().getShorthand()) || e.getLine(1).equalsIgnoreCase(CustomSigns.spawn.getSignActionType().getName())) { 
-                    e.setLine(0, CustomSigns.spawn.getLine1());
-                    e.setLine(1, CustomSigns.spawn.getLine2());
-                    e.setLine(2, CustomSigns.spawn.getLine3());
-                    e.setLine(3, CustomSigns.spawn.getLine4());
-                } else if (e.getLine(1).equals(CustomSigns.teleport.getSignActionType().getShorthand()) || e.getLine(1).equalsIgnoreCase(CustomSigns.teleport.getSignActionType().getName())) {
-                    e.setLine(0, CustomSigns.teleport.getLine1());
-                    e.setLine(1, CustomSigns.teleport.getLine2());
+                if (isSign(CustomSigns.spawn, e.getLine(1))) { 
+                    e.setLine(0, CustomSigns.spawn.getLine0());
+                    e.setLine(1, CustomSigns.spawn.getLine1());
+                    e.setLine(2, CustomSigns.spawn.getLine2());
+                    e.setLine(3, CustomSigns.spawn.getLine3());
+                } 
+                else if (isSign(CustomSigns.teleport, e.getLine(1))) {
+                    e.setLine(0, CustomSigns.teleport.getLine0());
+                    e.setLine(1, CustomSigns.teleport.getLine1());
 
-                    e.setLine(3, CustomSigns.teleport.getLine4());
-                } else if (e.getLine(1).equals(CustomSigns.home.getSignActionType().getShorthand()) || e.getLine(1).equalsIgnoreCase(CustomSigns.home.getSignActionType().getName())) {
-                    e.setLine(0, CustomSigns.home.getLine1());
-                    e.setLine(1, CustomSigns.home.getLine2());
+                    e.setLine(3, CustomSigns.teleport.getLine3());
+                } 
+                else if (isSign(CustomSigns.home, e.getLine(1))) {
+                    e.setLine(0, CustomSigns.home.getLine0());
+                    e.setLine(1, CustomSigns.home.getLine1());
 
-                    e.setLine(3, CustomSigns.home.getLine4());
-                } else if (e.getLine(1).equals(CustomSigns.up.getSignActionType().getShorthand()) || e.getLine(1).equalsIgnoreCase(CustomSigns.up.getSignActionType().getName())) {
-                    e.setLine(0, CustomSigns.up.getLine1());
-                    e.setLine(1, CustomSigns.up.getLine2());
-                    e.setLine(2, CustomSigns.up.getLine3());
-                    e.setLine(3, CustomSigns.up.getLine4());
-                } else if (e.getLine(1).equals(CustomSigns.down.getSignActionType().getShorthand()) || e.getLine(1).equalsIgnoreCase(CustomSigns.down.getSignActionType().getName())) {
-                    e.setLine(0, CustomSigns.down.getLine1());
-                    e.setLine(1, CustomSigns.down.getLine2());
-                    e.setLine(2, CustomSigns.down.getLine3());
-                    e.setLine(3, CustomSigns.down.getLine4());
-                } else if (e.getLine(1).equals(CustomSigns.command.getSignActionType().getShorthand()) || e.getLine(1).equalsIgnoreCase(CustomSigns.command.getSignActionType().getName())) {
-                    e.setLine(0, CustomSigns.command.getLine1());
-                    e.setLine(1, CustomSigns.command.getLine2());
+                    e.setLine(3, CustomSigns.home.getLine3());
+                } 
+                else if (isSign(CustomSigns.up, e.getLine(1))) {
+                    e.setLine(0, CustomSigns.up.getLine0());
+                    e.setLine(1, CustomSigns.up.getLine1());
+                    e.setLine(2, CustomSigns.up.getLine2());
+                    e.setLine(3, CustomSigns.up.getLine3());
+                } 
+                else if (isSign(CustomSigns.down, e.getLine(1))) {
+                    e.setLine(0, CustomSigns.down.getLine0());
+                    e.setLine(1, CustomSigns.down.getLine1());
+                    e.setLine(2, CustomSigns.down.getLine2());
+                    e.setLine(3, CustomSigns.down.getLine3());
+                } 
+                else if (isSign(CustomSigns.command, e.getLine(1))) {
+                    e.setLine(0, CustomSigns.command.getLine0());
+                    e.setLine(1, CustomSigns.command.getLine1());
 
-                    e.setLine(3, CustomSigns.command.getLine4());
+                    e.setLine(3, CustomSigns.command.getLine3());
                 }
             }
         }
