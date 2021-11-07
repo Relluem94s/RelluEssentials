c37ec5bf7ee062bbbc296294b621842d405981c6
Relluem94 <github@relluem94.de>
2021-11-07 20:36:36 +0100

    removed old stuff

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/events/features/RotationTool.java
+++
@@ -1,7 +0,0 @@
-package de.relluem94.minecraft.server.spigot.essentials.events.features;
-
-import org.bukkit.event.Listener;
-
-public class RotationTool implements Listener {
-    // To rotate rotatable Blocks like stairs etc.
-}
