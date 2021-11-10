7d2fa4fdf55ff8a7473fdccc2d7925c22d4ada66
Relluem94 <github@relluem94.de>
2021-11-10 22:54:58 +0100

    utf8 fix

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/constants/EventConstants.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/constants/EventConstants.java
@@ -17,8 +17,8 @@ public class EventConstants {
     /*                             EVENT   STUFF                                    */
     /********************************************************************************/
     
-    public static final String PLUGIN_EVENT_JOIN_MESSAGE = "ยง2[\u2726] " + PLUGIN_MESSAGE_COLOR + "%s" + PLUGIN_COMMAND_COLOR + " hat den Server betreten.";
-    public static final String PLUGIN_EVENT_QUIT_MESSAGE = "ยง4[\u274C] " + PLUGIN_MESSAGE_COLOR + "%s" + PLUGIN_COMMAND_COLOR + " hat den Server verlassen.";
+    public static final String PLUGIN_EVENT_JOIN_MESSAGE = "ง2[\u2726] " + PLUGIN_MESSAGE_COLOR + "%s" + PLUGIN_COMMAND_COLOR + " hat den Server betreten.";
+    public static final String PLUGIN_EVENT_QUIT_MESSAGE = "ง4[\u274C] " + PLUGIN_MESSAGE_COLOR + "%s" + PLUGIN_COMMAND_COLOR + " hat den Server verlassen.";
     public static final String PLUGIN_EVENT_DEATH = PLUGIN_PREFIX + PLUGIN_SPACER + PLUGIN_COMMAND_COLOR + "Du starbst bei " + PLUGIN_COMMAND_ARG_COLOR + "%s " + PLUGIN_COMMAND_COLOR + PLUGIN_COMMAND_WHERE_STRING + PLUGIN_COMMAND_COLOR;
     public static final String PLUGIN_EVENT_DEATH_TP = PLUGIN_PREFIX + PLUGIN_SPACER + PLUGIN_COMMAND_COLOR + "Klicke diese Nachricht um dich zum Todespunkt zu teleportieren!";
 
