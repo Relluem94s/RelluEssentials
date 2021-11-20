8fff1acd38e4ee1abaeb3cb2a76921398ac17b53
Relluem94 <github@relluem94.de>
2021-11-20 17:24:21 +0100

    format

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/events/SignActions.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/events/SignActions.java
@@ -16,37 +16,32 @@ public class SignActions implements Listener {
     public void onChangeSignCreateActionSign(SignChangeEvent e) {
         if (Permission.isAuthorized(e.getPlayer(), Groups.getGroup("mod").getId())) {
             if (e.getLine(1) != null) {
-                if (isSign(CustomSigns.spawn, e.getLine(1))) { 
+                if (isSign(CustomSigns.spawn, e.getLine(1))) {
                     e.setLine(0, CustomSigns.spawn.getLine0());
                     e.setLine(1, CustomSigns.spawn.getLine1());
                     e.setLine(2, CustomSigns.spawn.getLine2());
                     e.setLine(3, CustomSigns.spawn.getLine3());
-                } 
-                else if (isSign(CustomSigns.teleport, e.getLine(1))) {
+                } else if (isSign(CustomSigns.teleport, e.getLine(1))) {
                     e.setLine(0, CustomSigns.teleport.getLine0());
                     e.setLine(1, CustomSigns.teleport.getLine1());
 
                     e.setLine(3, CustomSigns.teleport.getLine3());
-                } 
-                else if (isSign(CustomSigns.home, e.getLine(1))) {
+                } else if (isSign(CustomSigns.home, e.getLine(1))) {
                     e.setLine(0, CustomSigns.home.getLine0());
                     e.setLine(1, CustomSigns.home.getLine1());
 
                     e.setLine(3, CustomSigns.home.getLine3());
-                } 
-                else if (isSign(CustomSigns.up, e.getLine(1))) {
+                } else if (isSign(CustomSigns.up, e.getLine(1))) {
                     e.setLine(0, CustomSigns.up.getLine0());
                     e.setLine(1, CustomSigns.up.getLine1());
                     e.setLine(2, CustomSigns.up.getLine2());
                     e.setLine(3, CustomSigns.up.getLine3());
-                } 
-                else if (isSign(CustomSigns.down, e.getLine(1))) {
+                } else if (isSign(CustomSigns.down, e.getLine(1))) {
                     e.setLine(0, CustomSigns.down.getLine0());
                     e.setLine(1, CustomSigns.down.getLine1());
                     e.setLine(2, CustomSigns.down.getLine2());
                     e.setLine(3, CustomSigns.down.getLine3());
-                } 
-                else if (isSign(CustomSigns.command, e.getLine(1))) {
+                } else if (isSign(CustomSigns.command, e.getLine(1))) {
                     e.setLine(0, CustomSigns.command.getLine0());
                     e.setLine(1, CustomSigns.command.getLine1());
 
