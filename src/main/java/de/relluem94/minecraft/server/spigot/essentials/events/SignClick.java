375ae63b01d3d385083e72c263e7a8c68efbdec1
Relluem94 <github@relluem94.de>
2021-11-20 17:24:23 +0100

    format

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/events/SignClick.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/events/SignClick.java
@@ -23,24 +23,19 @@ public class SignClick implements Listener {
             if (e.getAction() == Action.RIGHT_CLICK_BLOCK || e.getAction() == Action.LEFT_CLICK_BLOCK) {
                 if (e.getClickedBlock().getType().equals(Material.OAK_WALL_SIGN) || e.getClickedBlock().getType().equals(Material.OAK_SIGN)) {
                     Sign sign = (Sign) e.getClickedBlock().getState();
-                    if(isSign(CustomSigns.spawn, sign.getLine(0), sign.getLine(1), sign.getLine(3))){
+                    if (isSign(CustomSigns.spawn, sign.getLine(0), sign.getLine(1), sign.getLine(3))) {
                         e.getPlayer().performCommand(PLUGIN_COMMAND_NAME_SPAWN);
-                    }
-                    else if(isSign(CustomSigns.up, sign.getLine(0), sign.getLine(1), sign.getLine(3))){
+                    } else if (isSign(CustomSigns.up, sign.getLine(0), sign.getLine(1), sign.getLine(3))) {
                         // GO UP
-                    }
-                    else if(isSign(CustomSigns.down, sign.getLine(0), sign.getLine(1), sign.getLine(3))){
+                    } else if (isSign(CustomSigns.down, sign.getLine(0), sign.getLine(1), sign.getLine(3))) {
                         // DOWN
-                    }
-                    else if(isSign(CustomSigns.command, sign.getLine(0), sign.getLine(1), sign.getLine(3))){
+                    } else if (isSign(CustomSigns.command, sign.getLine(0), sign.getLine(1), sign.getLine(3))) {
                         e.getPlayer().performCommand(sign.getLine(2));
-                    }
-                    else if(isSign(CustomSigns.teleport, sign.getLine(0), sign.getLine(1), sign.getLine(3))){
+                    } else if (isSign(CustomSigns.teleport, sign.getLine(0), sign.getLine(1), sign.getLine(3))) {
                         String[] locationString = sign.getLine(2).split(",");
                         Location location = new Location(e.getPlayer().getWorld(), Integer.parseInt(locationString[0]), Integer.parseInt(locationString[1]), Integer.parseInt(locationString[2]));
                         e.getPlayer().teleport(location);
-                    }
-                    else if(isSign(CustomSigns.home, sign.getLine(0), sign.getLine(1), sign.getLine(3))){
+                    } else if (isSign(CustomSigns.home, sign.getLine(0), sign.getLine(1), sign.getLine(3))) {
                         e.getPlayer().performCommand(PLUGIN_COMMAND_NAME_HOME + " " + sign.getLine(2));
                     }
                 }
