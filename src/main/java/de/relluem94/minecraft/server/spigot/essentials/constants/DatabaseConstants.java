5d8c90157551489f42edb8ec6c88d549c70602d1
Relluem94 <github@relluem94.de>
2021-11-20 17:23:44 +0100

    format

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/constants/DatabaseConstants.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/constants/DatabaseConstants.java
@@ -5,11 +5,10 @@ package de.relluem94.minecraft.server.spigot.essentials.constants;
  * @author rellu
  */
 public class DatabaseConstants {
-    
-    /********************************************************************************/
-    /*                           DATABASE   STUFF                                   */
-    /********************************************************************************/
-    
+
+    //==============================================================================//
+    //                           DATABASE   STUFF                                   //
+    //==============================================================================//
     public static final String PLUGIN_DATABASE_VERSION = "";
     public static final String PLUGIN_DATABASE_NAME = "rellu_essentials";
 }
