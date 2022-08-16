c346ceee364fd9c69aa4d1c8459c1939f5b5b45c
Relluem94 <github@relluem94.de>
2022-08-17 01:11:23 +0200

    fixed utf8 fuckup

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/constants/ItemConstants.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/constants/ItemConstants.java
@@ -15,23 +15,23 @@ public class ItemConstants {
     public static final String PLUGIN_ITEM_NAMESPACE_SMELTER_FURNACE = "smelter_furnace";
     public static final String PLUGIN_ITEM_NAMESPACE_SMELTER_FUEL = "smelter_fuel";
 
-    public static final String PLUGIN_ITEM_RELLU_HELMET = "§6Rellu's Helmet";
-    public static final String PLUGIN_ITEM_RELLU_CHESTPLATE = "§3Rellu's Chestplate";
-    public static final String PLUGIN_ITEM_RELLU_LEGGINGS = "§aRellu's Leggings";
-    public static final String PLUGIN_ITEM_RELLU_BOOTS = "§cRellu's Boots";
-    public static final String PLUGIN_ITEM_RELLU_SWORD = "§eRellu's Sword";
-    public static final String PLUGIN_ITEM_RELLU_SHIELD = "§5Rellu's Shield";
+    public static final String PLUGIN_ITEM_RELLU_HELMET = "Â§6Rellu's Helmet";
+    public static final String PLUGIN_ITEM_RELLU_CHESTPLATE = "Â§3Rellu's Chestplate";
+    public static final String PLUGIN_ITEM_RELLU_LEGGINGS = "Â§aRellu's Leggings";
+    public static final String PLUGIN_ITEM_RELLU_BOOTS = "Â§cRellu's Boots";
+    public static final String PLUGIN_ITEM_RELLU_SWORD = "Â§eRellu's Sword";
+    public static final String PLUGIN_ITEM_RELLU_SHIELD = "Â§5Rellu's Shield";
 
-    public static final String PLUGIN_ITEM_INGREDIENT = "§4§l§oThis is a crafting ingredient";
+    public static final String PLUGIN_ITEM_INGREDIENT = "Â§4Â§lÂ§oThis is a crafting ingredient";
 
-    public static final String PLUGIN_ITEM_AUTOSMELTER = "§cSmelter Pickaxe";
-    public static final String PLUGIN_ITEM_AUTOSMELTER_TANK = "§cSmelter Tank";
-    public static final String PLUGIN_ITEM_AUTOSMELTER_FURNACE = "§cSmelter Furnace";
+    public static final String PLUGIN_ITEM_AUTOSMELTER = "Â§cSmelter Pickaxe";
+    public static final String PLUGIN_ITEM_AUTOSMELTER_TANK = "Â§cSmelter Tank";
+    public static final String PLUGIN_ITEM_AUTOSMELTER_FURNACE = "Â§cSmelter Furnace";
 
-    public static final String PLUGIN_ITEM_CLOUDBOOTS = "§bCloud Boots";
-    public static final String PLUGIN_ITEM_CLOUDBOOTS_LORE1 = "§bGrants gliding abillity if worn.";
-    public static final String PLUGIN_ITEM_CLOUDBOOTS_LORE2 = "§bAlso reduces Fall Damage by 100%";
-    public static final String PLUGIN_ITEM_CLOUDSAILOR = "§bCloud Sailor";
-    public static final String PLUGIN_ITEM_CLOUDSAILOR_LORE1 = "§bHeld in Off Hand grants gliding abillity.";
-    public static final String PLUGIN_ITEM_CLOUDSAILOR_LORE2 = "§bAlso reduces Fall Damage by 50%";
+    public static final String PLUGIN_ITEM_CLOUDBOOTS = "Â§bCloud Boots";
+    public static final String PLUGIN_ITEM_CLOUDBOOTS_LORE1 = "Â§bGrants gliding abillity if worn.";
+    public static final String PLUGIN_ITEM_CLOUDBOOTS_LORE2 = "Â§bAlso reduces Fall Damage by 100%";
+    public static final String PLUGIN_ITEM_CLOUDSAILOR = "Â§bCloud Sailor";
+    public static final String PLUGIN_ITEM_CLOUDSAILOR_LORE1 = "Â§bHeld in Off Hand grants gliding abillity.";
+    public static final String PLUGIN_ITEM_CLOUDSAILOR_LORE2 = "Â§bAlso reduces Fall Damage by 50%";
 }
