4e4ec9da06c14eec5885f21faf242809c8824508
Relluem94 <github@relluem94.de>
2023-01-07 19:58:02 +0100

    removed tabtitle stuff

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/helpers/PlayerHelper.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/helpers/PlayerHelper.java
@@ -7,19 +7,11 @@ import static de.relluem94.minecraft.server.spigot.essentials.Strings.PLUGIN_COM
 import static de.relluem94.minecraft.server.spigot.essentials.Strings.PLUGIN_PREFIX;
 import static de.relluem94.minecraft.server.spigot.essentials.Strings.PLUGIN_SPACER;
 import de.relluem94.minecraft.server.spigot.essentials.helpers.pojo.PlayerEntry;
-import io.netty.buffer.ByteBuf;
-import io.netty.buffer.ByteBufAllocator;
-import net.minecraft.network.PacketDataSerializer;
-import net.minecraft.network.chat.IChatBaseComponent;
-import net.minecraft.network.chat.IChatBaseComponent.ChatSerializer;
-import net.minecraft.network.protocol.game.PacketPlayOutPlayerListHeaderFooter;
-import net.minecraft.server.level.EntityPlayer;
 
 import de.relluem94.minecraft.server.spigot.essentials.permissions.Groups;
 import de.relluem94.minecraft.server.spigot.essentials.permissions.Permission;
 
 import org.bukkit.Bukkit;
-import org.bukkit.craftbukkit.v1_19_R2.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
 /**
@@ -64,25 +56,5 @@ public class PlayerHelper {
         p.setInvulnerable(isAFK);
         p.setPlayerListName((isAFK ? "§c[AFK] " : "") + p.getCustomName());
         return true;
-    }
-
-    /**
-     *
-     * @param p Player to send TabList
-     * @param header String Header Text
-     * @param footer String Footer Text
-     */
-    public static void sendTablist(Player p, String header, String footer) {
-        IChatBaseComponent tabHeader = ChatSerializer.a("{\"text\":\"" + header + "\"}");
-        IChatBaseComponent tabFooter = ChatSerializer.a("{\"text\":\"" + footer + "\"}");
-        EntityPlayer entityPlayer = ((CraftPlayer) p).getHandle();
-        ByteBuf byteBuf = ByteBufAllocator.DEFAULT.buffer();
-        PacketDataSerializer packetDataSerializer = new PacketDataSerializer(byteBuf);
-        PacketPlayOutPlayerListHeaderFooter packetPlayOutPlayerListHeaderFooter = new PacketPlayOutPlayerListHeaderFooter(packetDataSerializer);
-
-        packetDataSerializer.a(IChatBaseComponent.ChatSerializer.a(tabHeader));
-        packetDataSerializer.a(IChatBaseComponent.ChatSerializer.a(tabFooter));
-        packetPlayOutPlayerListHeaderFooter.a(packetDataSerializer);
-        entityPlayer.b.a(packetPlayOutPlayerListHeaderFooter, null);
     }
 }
