b4ecfeaf902002032af713043c419ca922698f3d
Relluem94 <github@relluem94.de>
2023-01-15 22:18:34 +0100

    fixed sql is not one statment

--- src/main/resources/sqls/patches/v3.0/insertBankTier.sql
+++ src/main/resources/sqls/patches/v3.0/insertBankTier.sql
@@ -3,6 +3,7 @@
  * Created: 15.01.2023
  */
 
-INSERT INTO `rellu_essentials`.`bank_tier` (`id`, `name`, `limit`, `interest`) VALUES (1, 'Standard', 9000000, 0.3);
-INSERT INTO `rellu_essentials`.`bank_tier` (`id`, `name`, `limit`, `interest`) VALUES (2, 'Premium', 9000000000, 0.2);
-INSERT INTO `rellu_essentials`.`bank_tier` (`id`, `name`, `limit`, `interest`) VALUES (3, 'Exclusive', 9000000000000, 0.1);
+INSERT INTO `rellu_essentials`.`bank_tier` (`id`, `name`, `limit`, `interest`) VALUES 
+(1, 'Standard', 9000000, 0.3),
+(2, 'Premium', 9000000000, 0.2),
+(3, 'Exclusive', 9000000000000, 0.1);
