a5d885023d297b6d6f29a5ed86b95bf74a8a3950
Relluem94 <github@relluem94.de>
2023-01-15 21:42:37 +0100

    removed disabled loop and unused imports

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/events/BetterMobs.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/events/BetterMobs.java
@@ -1,6 +1,5 @@
 package de.relluem94.minecraft.server.spigot.essentials.events;
 
-import org.bukkit.Location;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -13,10 +12,6 @@ public class BetterMobs implements Listener {
         EntityType et = e.getEntity().getType();
         if (et == EntityType.PHANTOM) {
             e.setCancelled(true);
-        } else if (et == EntityType.IRON_GOLEM) {
-            //Location l = e.getEntity().getLocation();
-            //l.getWorld().spawnEntity(l, et);
-            // triggers a loop
         }
     }
 }
