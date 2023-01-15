f6002763a7fab8d308238b2263c65ea6842c923e
Relluem94 <github@relluem94.de>
2023-01-15 22:05:55 +0100

    fixed utf8 error

--- src/main/resources/sqls/patches/v1.0/insertPluginInformation.sql
+++ src/main/resources/sqls/patches/v1.0/insertPluginInformation.sql
@@ -3,4 +3,4 @@
  * Created: 06.01.2021
  */
 
-INSERT INTO `rellu_essentials`.`plugin_informations` (`id`, `created`, `createdby`, `tab_header`, `tab_footer`, `motd_message`, `motd_players`, `db_version`) VALUES (1, '2021-01-05 20:57:05', 1, "§7<<------------------>>          §6Relluem94.de           §7<<------------------>>", "§7<<------------------>> §6powered by §8Rellu§cEssentials §7<<------------------>>", "§6powered by §8Rellu§cEssentials", 94, 1);
+INSERT INTO `rellu_essentials`.`plugin_informations` (`id`, `created`, `createdby`, `tab_header`, `tab_footer`, `motd_message`, `motd_players`, `db_version`) VALUES (1, '2021-01-05 20:57:05', 1, "Â§7<<------------------>>          Â§6Relluem94.de           Â§7<<------------------>>", "Â§7<<------------------>> Â§6powered by Â§8RelluÂ§cEssentials Â§7<<------------------>>", "Â§6powered by Â§8RelluÂ§cEssentials", 94, 1);
