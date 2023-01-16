6c0eb4fbb0820c3b43b99cbaf14b55c07462b6f9
Relluem94 <github@relluem94.de>
2023-01-16 20:34:40 +0100

    added Event for Entity Death Event to later get coins on entity death

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/events/BetterMobs.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/events/BetterMobs.java
@@ -1,9 +1,11 @@
 package de.relluem94.minecraft.server.spigot.essentials.events;
 
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
 
 public class BetterMobs implements Listener {
 
@@ -12,6 +14,15 @@ public class BetterMobs implements Listener {
         EntityType et = e.getEntity().getType();
         if (et == EntityType.PHANTOM) {
             e.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onDeath(EntityDeathEvent e) {
+        if(e.getEntity().getKiller() != null && e.getEntity().getKiller() instanceof Player){
+            Player p = e.getEntity().getKiller();
+            p.sendMessage("COINS");
+            // GEN COINS
         }
     }
 }
