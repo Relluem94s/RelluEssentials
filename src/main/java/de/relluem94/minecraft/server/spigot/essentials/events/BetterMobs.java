63b1bb4e5f6555be2a7b0196a8a9b23d071c0e31
Relluem94 <github@relluem94.de>
2023-01-21 20:40:42 +0100

    added a unimplemented method

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/events/BetterMobs.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/events/BetterMobs.java
@@ -10,6 +10,7 @@ import org.bukkit.scoreboard.DisplaySlot;
 import org.bukkit.scoreboard.Score;
 
 import de.relluem94.minecraft.server.spigot.essentials.RelluEssentials;
+import de.relluem94.minecraft.server.spigot.essentials.helpers.pojo.PlayerEntry;
 
 public class BetterMobs implements Listener {
 
@@ -25,9 +26,10 @@ public class BetterMobs implements Listener {
     public void onDeath(EntityDeathEvent e) {
         if(e.getEntity().getKiller() != null && e.getEntity().getKiller() instanceof Player){
             Player p = e.getEntity().getKiller();
+            PlayerEntry pe = RelluEssentials.playerEntryList.get(p.getUniqueId());
+            pe.getPurse();
             p.sendMessage("COINS");
-            Score coins = RelluEssentials.objective.getScore(p.getName());
-            //coins.
+            Score coins = RelluEssentials.objective.getScore("Coins");
             coins.setScore(coins.getScore() + 10);
             RelluEssentials.objective.setDisplaySlot(DisplaySlot.SIDEBAR);
             // GEN COINS
