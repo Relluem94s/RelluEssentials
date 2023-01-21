e234f9b36f1c26dbf4e7a535392bceb1c2612efb
Relluem94 <github@relluem94.de>
2023-01-22 00:43:12 +0100

    added default values

--- src/main/resources/sqls/patches/v3.0/insertBankTier.sql
+++ src/main/resources/sqls/patches/v3.0/insertBankTier.sql
@@ -3,7 +3,7 @@
  * Created: 15.01.2023
  */
 
-INSERT INTO `rellu_essentials`.`bank_tier` (`id`, `name`, `limit`, `interest`) VALUES 
-(1, 'Standard', 9000000, 0.3),
-(2, 'Premium', 9000000000, 0.2),
-(3, 'Exclusive', 9000000000000, 0.1);
+INSERT INTO `rellu_essentials`.`bank_tier` (`id`, `name`, `limit`, `interest`, `cost`) VALUES 
+(1, 'Standard', 9000000, 0.3, 50000),
+(2, 'Premium', 9000000000, 0.2, 5000000),
+(3, 'Exclusive', 9000000000000, 0.1, 50000000);
