ad2358ffa85311b749fd0acfc663806eabce5ebc
Relluem94 <github@relluem94.de>
2023-01-22 00:36:59 +0100

    added Bank insert

---
+++ src/main/resources/sqls/insertBankAccount.sql
@@ -0,0 +1,6 @@
+/**
+ * Author:  rellu
+ * Created: 22.01.2023
+ */
+
+INSERT INTO `rellu_essentials`.`bank_account` (`created`, `createdby`, `player_fk`, `value`, `bank_tier_fk`) VALUES (now(), ?, ?, ?, ?);
