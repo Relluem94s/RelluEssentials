5cf9970ebd37e46f913f141cac9e32a5f4caf49b
Relluem94 <github@relluem94.de>
2023-01-22 00:37:17 +0100

    added Bank Transaction insert

---
+++ src/main/resources/sqls/insertBankTransaction.sql
@@ -0,0 +1,6 @@
+/**
+ * Author:  rellu
+ * Created: 22.01.2023
+ */
+
+INSERT INTO `rellu_essentials`.`bank_transaction` (`created`, `createdby`, `bank_account_fk`, `value`) VALUES (now(), ?, ?, ?);
