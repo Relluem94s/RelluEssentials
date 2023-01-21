162ef18edd427f166498dd174a4c41150d5859eb
Relluem94 <github@relluem94.de>
2023-01-21 23:35:52 +0100

    added purse field to player table

--- src/main/resources/sqls/patches/v3.0/updatePlayer.sql
+++ src/main/resources/sqls/patches/v3.0/updatePlayer.sql
@@ -2,4 +2,4 @@
  * Author:  rellu
  * Created: 21.01.2023
  */
-ALTER TABLE `rellu_essentials`.`player` ADD purse BIGINT(21) NOT NULL DEFAULT '0'; 
+ALTER TABLE `rellu_essentials`.`player` ADD purse FLOAT(21) NOT NULL DEFAULT '0'; 
