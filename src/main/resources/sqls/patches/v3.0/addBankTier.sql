f1dab3495d79d4f0a012ce431d3537a41f4598e9
Relluem94 <github@relluem94.de>
2023-01-22 01:29:23 +0100

    changes

--- src/main/resources/sqls/patches/v3.0/addBankTier.sql
+++ src/main/resources/sqls/patches/v3.0/addBankTier.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS `rellu_essentials`.`bank_tier` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(94) NOT NULL,
   `limit` BIGINT(21) NOT NULL,
-  `interest` DOUBLE NOT NULL,
+  `interest` FLOAT NOT NULL,
   `cost` INT(21) NOT NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
