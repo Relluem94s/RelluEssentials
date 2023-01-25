4c0fef6b129d9ed9ba05ebcfae04175ba10d790d
Relluem94 <github@relluem94.de>
2023-01-25 22:18:49 +0100

    added location type for protection

---
+++ src/main/resources/sqls/patches/v3.0/insertLocationTypes.sql
@@ -0,0 +1,12 @@
+/**
+ * Author:  rellu
+ * Created: 05.01.2021
+ */
+
+-- -----------------------------------------------------
+-- Data for table `rellu_essentials`.`location_type`
+-- -----------------------------------------------------
+
+INSERT INTO `rellu_essentials`.`location_type` (`id`, `location_type`) 
+VALUES 
+(5, 'protection');
