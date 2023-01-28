663345c02c86be6c459021a99fd519240597fec1
Relluem94 <github@relluem94.de>
2023-01-28 20:57:02 +0100

    added order by id to show latest transactions

--- src/main/resources/sqls/getBankAccountTransactionsByPlayer.sql
+++ src/main/resources/sqls/getBankAccountTransactionsByPlayer.sql
@@ -3,4 +3,4 @@
  * Created: 22.01.2023
  */
 
-SELECT * FROM `rellu_essentials`.`bank_transaction` bt where bt.bank_account_fk = ? limit 10; 
+SELECT * FROM `rellu_essentials`.`bank_transaction` bt where bt.bank_account_fk = ? order by id desc limit 10; 
