5673cd97b6c028fb1780cbf3f857b72dd7b5800d
Relluem94 <github@relluem94.de>
2023-01-28 18:44:36 +0100

    added Baker NPC

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/commands/Admin.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/commands/Admin.java
@@ -8,7 +8,10 @@ import org.bukkit.entity.Player;
 import de.relluem94.minecraft.server.spigot.essentials.permissions.Permission;
 import de.relluem94.minecraft.server.spigot.essentials.CustomItems;
 import de.relluem94.minecraft.server.spigot.essentials.Strings;
+import de.relluem94.minecraft.server.spigot.essentials.NPC.Baker;
 import de.relluem94.minecraft.server.spigot.essentials.NPC.Banker;
+import de.relluem94.minecraft.server.spigot.essentials.NPC.Farmer;
+import de.relluem94.minecraft.server.spigot.essentials.NPC.Miner;
 import de.relluem94.minecraft.server.spigot.essentials.helpers.InventoryHelper;
 import de.relluem94.minecraft.server.spigot.essentials.permissions.Groups;
 
@@ -33,11 +36,12 @@ public class Admin implements CommandExecutor {
                             org.bukkit.inventory.Inventory inv = InventoryHelper.fillInventory(InventoryHelper.createInventory(9, Strings.PLUGIN_PREFIX + Strings.PLUGIN_SPACER +"§dNPCs"), CustomItems.npc_gui_disabled.getCustomItem());
                             
                             inv.setItem(0, Banker.npc.getCustomItem());
-                            inv.setItem(1, CustomItems.npcFarmer.getCustomItem());
-                            inv.setItem(2, CustomItems.npcFisher.getCustomItem());
-                            inv.setItem(3, CustomItems.npcSmith.getCustomItem());
-                            inv.setItem(4, CustomItems.npcAdventurer.getCustomItem());
-                            inv.setItem(5, CustomItems.npcMiner.getCustomItem());
+                            inv.setItem(1, Farmer.npc.getCustomItem());
+                            inv.setItem(2, Miner.npc.getCustomItem());
+                            inv.setItem(3, Baker.npc.getCustomItem());
+                            inv.setItem(4, CustomItems.npcFisher.getCustomItem());
+                            inv.setItem(5, CustomItems.npcSmith.getCustomItem());
+                            inv.setItem(6, CustomItems.npcAdventurer.getCustomItem());
         
                             InventoryHelper.openInventory(sender, inv);
                             p.sendMessage("TEST 66");
