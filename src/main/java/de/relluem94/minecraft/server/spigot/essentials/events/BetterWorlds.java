d7bde9deef2c242d1d9333188577cfb5b36d9924
Relluem94 <github@relluem94.de>
2023-03-06 21:51:33 +0100

    added WorldInitEvent to boost startup Time

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/events/BetterWorlds.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/events/BetterWorlds.java
@@ -2,8 +2,11 @@ package de.relluem94.minecraft.server.spigot.essentials.events;
 
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
+import org.bukkit.event.world.WorldInitEvent;
+
 import de.relluem94.minecraft.server.spigot.essentials.helpers.WorldHelper;
 
 public class BetterWorlds implements Listener {
@@ -14,5 +17,10 @@ public class BetterWorlds implements Listener {
         
         WorldHelper.saveWorldGroupInventory(p, e.getFrom());
         WorldHelper.loadWorldGroupInventory(p);
+    }
+
+    @EventHandler(priority=EventPriority.HIGHEST)
+    public void worldInit(WorldInitEvent e){
+        e.getWorld().setKeepSpawnInMemory(false);
     }
 }
