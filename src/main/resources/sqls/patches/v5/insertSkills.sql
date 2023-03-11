1a03d5ba78316006f2728d3bbc786fe1146b0969
Relluem94 <github@relluem94.de>
2023-03-11 23:03:21 +0100

    added inserts for skills

---
+++ src/main/resources/sqls/patches/v5/insertSkills.sql
@@ -0,0 +1,9 @@
+/**
+ * Author:  rellu
+ * Created: 03.02.2023
+ */
+
+INSERT INTO `rellu_essentials`.`skills` (`id`, `name`, `displayname`, `max_level`) VALUES 
+(4, 'MONSTER_HUNTER', 'Monster Hunter', 1000),
+(5, 'FISHER', 'Fisher', 1000),
+(6, 'MINER', 'Miner', 1000);
