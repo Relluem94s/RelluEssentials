6062b474295cf4197989ca08f5c175eeeaabaa15
Relluem94 <github@relluem94.de>
2023-03-13 22:22:24 +0100

    added DB stuff to marry command

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/commands/Marry.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/commands/Marry.java
@@ -8,48 +8,128 @@ import org.bukkit.entity.Player;
 
 import de.relluem94.minecraft.server.spigot.essentials.permissions.Permission;
 import de.relluem94.minecraft.server.spigot.essentials.RelluEssentials;
+import de.relluem94.minecraft.server.spigot.essentials.api.PlayerAPI;
+import de.relluem94.minecraft.server.spigot.essentials.api.ProtectionAPI;
+import de.relluem94.minecraft.server.spigot.essentials.events.BetterLock;
+import de.relluem94.minecraft.server.spigot.essentials.helpers.pojo.PlayerEntry;
+import de.relluem94.minecraft.server.spigot.essentials.helpers.pojo.PlayerPartnerEntry;
+import de.relluem94.minecraft.server.spigot.essentials.helpers.pojo.ProtectionEntry;
 import de.relluem94.minecraft.server.spigot.essentials.permissions.Groups;
 import static de.relluem94.minecraft.server.spigot.essentials.Strings.*;
 import static de.relluem94.minecraft.server.spigot.essentials.constants.CommandNameConstants.PLUGIN_COMMAND_NAME_MARRY;
 import static de.relluem94.minecraft.server.spigot.essentials.constants.CommandNameConstants.PLUGIN_COMMAND_NAME_MARRY_ACCEPT;
+import static de.relluem94.minecraft.server.spigot.essentials.constants.CommandNameConstants.PLUGIN_COMMAND_NAME_MARRY_DIVORCE;
 import static de.relluem94.minecraft.server.spigot.essentials.helpers.TypeHelper.isPlayer;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.UUID;
 
 public class Marry implements CommandExecutor {
 
     HashMap<Player, Player> marryAcceptList = new HashMap<>();
 
-    private void addMarryEntry(Player p, Player t){
-        marryAcceptList.put(t, p);
+    private void addMarryEntry(Player player, Player target){
+        if(PlayerAPI.getPlayerEntry(player).getPartner() != null || PlayerAPI.getPlayerEntry(target).getPartner() != null){
+            player.sendMessage(PLUGIN_COMMAND_MARRY_REQUEST_IS_MAARIED);
+            return;
+        }
+
+        player.sendMessage(String.format(PLUGIN_COMMAND_MARRY_SEND_REQUEST, target.getCustomName()));
+
+        marryAcceptList.put(target, player);
         Bukkit.getScheduler().runTaskLater(RelluEssentials.getInstance(), new Runnable() {
     
             @Override
             public void run() {
-                if(hasMarryEntry(t)){
-                    p.sendMessage(PLUGIN_COMMAND_TP_REQUEST_EXPIRED);
-                    t.sendMessage(PLUGIN_COMMAND_TP_REQUEST_EXPIRED);
-                    removeMarryEntry(t);
+                if(hasMarryEntry(target)){
+                    player.sendMessage(PLUGIN_COMMAND_MARRY_REQUEST_EXPIRED);
+                    target.sendMessage(PLUGIN_COMMAND_MARRY_REQUEST_EXPIRED);
+                    removeMarryEntry(target);
                 }
             }
         }, 20*60*2);
     }
 
-    private boolean hasMarryEntry(Player t) {
-        return marryAcceptList.containsKey(t);
+    private boolean hasMarryEntry(Player target) {
+        return marryAcceptList.containsKey(target);
     }
 
-    private void removeMarryEntry(Player t) {
-        marryAcceptList.remove(t);
+    private void removeMarryEntry(Player target) {
+        marryAcceptList.remove(target);
     }
 
 
-    public void marry(Player p, Player t){
-        t.sendMessage(String.format(PLUGIN_COMMAND_MARRY_MARRIED, p.getCustomName()));
-        p.sendMessage(String.format(PLUGIN_COMMAND_MARRY_MARRIED, t.getCustomName()));
-        // TODO insert in db
+    public void marry(Player player, Player target){
+        target.sendMessage(String.format(PLUGIN_COMMAND_MARRY_MARRIED, player.getCustomName()));
+        player.sendMessage(String.format(PLUGIN_COMMAND_MARRY_MARRIED, target.getCustomName()));
+
+        PlayerEntry firstPlayer = PlayerAPI.getPlayerEntry(target);
+        PlayerEntry secondPlayer = PlayerAPI.getPlayerEntry(player);
+
+        PlayerPartnerEntry playerPartnerEntry = new PlayerPartnerEntry();
+        playerPartnerEntry.setCreatedby(firstPlayer.getID());
+        playerPartnerEntry.setFirstPlayerID(firstPlayer.getID());
+        playerPartnerEntry.setSecondPlayerID(secondPlayer.getID());
+
+        RelluEssentials.dBH.insertPlayerPartner(playerPartnerEntry);
+        playerPartnerEntry = RelluEssentials.dBH.getPlayerPartner(firstPlayer.getID());
+
+        firstPlayer.setPartner(playerPartnerEntry);
+        secondPlayer.setPartner(playerPartnerEntry);
+
+        Collection<ProtectionEntry> protectionEntryList = new ArrayList<ProtectionEntry> ();
+        protectionEntryList.addAll(ProtectionAPI.getProtectionEntryList().values());
+
+        for(ProtectionEntry pre : protectionEntryList){ 
+            if(pre.getCreatedBy() == firstPlayer.getID()){
+                BetterLock.addRight(target, pre, secondPlayer.getID(), true);
+            }
+
+            if(pre.getCreatedBy() == secondPlayer.getID()){
+                BetterLock.addRight(player, pre, firstPlayer.getID(), true);
+            }
+            
+        }
+        
     }
 
+
+
+    private void divorce(PlayerEntry pe) {
+        PlayerPartnerEntry ppe = pe.getPartner();
+
+        PlayerEntry secondPlayerEntry = PlayerAPI.getPlayerEntry((ppe.getSecondPlayerID() == pe.getID() ? ppe.getSecondPlayerID() : ppe.getFirstPlayerID()));
+
+        Player firstPlayer = Bukkit.getPlayer(UUID.fromString(pe.getUUID()));
+        Player secondPlayer = Bukkit.getPlayer(UUID.fromString(secondPlayerEntry.getUUID()));
+
+        if(firstPlayer != null && secondPlayer != null){
+            firstPlayer.sendMessage(String.format(PLUGIN_COMMAND_MARRY_DIVORCED, secondPlayer.getCustomName()));
+            secondPlayer.sendMessage(String.format(PLUGIN_COMMAND_MARRY_DIVORCED, firstPlayer.getCustomName()));
+
+            ppe.setDeletedBy(pe.getID());
+            pe.setPartner(null);
+            secondPlayerEntry.setPartner(null);
+        
+            RelluEssentials.dBH.deletePlayerPartner(ppe);
+
+            Collection<ProtectionEntry> protectionEntryList = new ArrayList<ProtectionEntry> ();
+            protectionEntryList.addAll(ProtectionAPI.getProtectionEntryList().values());
+
+            for(ProtectionEntry pre : protectionEntryList){
+                if(pre.getCreatedBy() == pe.getID()){
+                    BetterLock.removeRight(firstPlayer, pre, secondPlayerEntry.getID(), true);
+                }
+    
+                if(pre.getCreatedBy() == secondPlayerEntry.getID()){
+                    BetterLock.removeRight(secondPlayer, pre, pe.getID(), true);
+                }
+                
+            }
+        }
+    }
 
 
     @Override
@@ -73,7 +153,7 @@ public class Marry implements CommandExecutor {
         }
 
         if (args.length == 0) {
-            p.sendMessage(PLUGIN_COMMAND_TP_INFO);
+            p.sendMessage(PLUGIN_COMMAND_MARRY_INFO);
             return true;
         }
         
@@ -91,13 +171,26 @@ public class Marry implements CommandExecutor {
 
             Player target = Bukkit.getPlayer(args[0]);
             if (target == null) {
-                p.sendMessage(PLUGIN_COMMAND_TARGET_NOT_A_PLAYER);
+                p.sendMessage(String.format(PLUGIN_COMMAND_TARGET_NOT_A_PLAYER, args[0]));
                 return false;
             }
 
             addMarryEntry(p, target);
-            p.sendMessage(String.format(PLUGIN_COMMAND_MARRY_SEND_REQUEST, target.getCustomName()));
+            
             return true;
+        }
+
+        if (args.length == 2) {
+            if(args[0].equals(PLUGIN_COMMAND_NAME_MARRY_DIVORCE)){
+                PlayerEntry pe = PlayerAPI.getPlayerEntry(p);
+                if(PlayerAPI.getPlayerEntry(p).getPartner() != null){
+                    divorce(pe);
+                    return true;
+                }
+
+                p.sendMessage(PLUGIN_COMMAND_MARRY_DIVORCE_NOT_MARRIED);
+                return true;
+            }
         }
         return false;
     }
