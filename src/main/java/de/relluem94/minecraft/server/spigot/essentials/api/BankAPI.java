802b03cf1d13902aa61efc7baee17ed9a4102701
Relluem94 <github@relluem94.de>
2023-03-17 22:37:11 +0100

    added JavaDocs

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/api/BankAPI.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/api/BankAPI.java
@@ -9,17 +9,20 @@ public class BankAPI {
     
     private static List<BankTierEntry> bankTiersList = new ArrayList<>();
 
+    /**
+     * 
+     * @param bankTierEntries List of BankTierEntry
+     */
     public BankAPI(List<BankTierEntry> bankTierEntries){
         bankTiersList.addAll(bankTierEntries);
     }
 
+    /**
+     * 
+     * Gives back a List of all BankTiers
+     * @return List of BankTierEntry
+     */
     public static List<BankTierEntry> getBankTiers(){
         return bankTiersList;
     }
-
-
-
-
-
-
 }
