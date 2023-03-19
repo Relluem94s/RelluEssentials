80125001dd006b1bd13f5004aa6c396d4ecb81e4
Relluem94 <github@relluem94.de>
2023-03-19 19:04:55 +0100

    RE-23 removed imports

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/events/BetterBags.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/events/BetterBags.java
@@ -15,8 +15,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockDropItemEvent;
-import org.bukkit.event.block.BlockFromToEvent;
-import org.bukkit.event.block.BlockPhysicsEvent;
 import org.bukkit.event.entity.EntityPickupItemEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryType;
