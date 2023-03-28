531d5ea9625faf6c82a9e22cac924267e7a43915
Relluem94 <github@relluem94.de>
2023-03-28 13:55:37 +0200

    RE-41 fixed test hang (version of MockBukkit) implemented tests and temp build deny for lobby (with test)

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/events/BlockPlace.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/events/BlockPlace.java
@@ -2,7 +2,12 @@ package de.relluem94.minecraft.server.spigot.essentials.events;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+
+import de.relluem94.minecraft.server.spigot.essentials.Strings;
+import de.relluem94.minecraft.server.spigot.essentials.permissions.Groups;
+import de.relluem94.minecraft.server.spigot.essentials.permissions.Permission;
 
 /**
  *
@@ -12,6 +17,17 @@ public class BlockPlace implements Listener {
 
     @EventHandler
     public void placeBlocks(BlockPlaceEvent e) {
+        if(e.getBlock().getWorld().getName().equals(Strings.PLUGIN_WORLD_LOBBY)){
+          e.setCancelled(!Permission.isAuthorized(e.getPlayer(), Groups.getGroup("mod").getId()));
+        }
+      // has to be reimplemented
+    }
+
+    @EventHandler
+    public void breakBlocks(BlockBreakEvent e) {
+        if(e.getBlock().getWorld().getName().equals(Strings.PLUGIN_WORLD_LOBBY)){
+          e.setCancelled(!Permission.isAuthorized(e.getPlayer(), Groups.getGroup("mod").getId()));
+        }
       // has to be reimplemented
     }
 }
--- src/test/java/de/relluem94/minecraft/server/spigot/essentials/RelluEssentialsTest.java
+++ src/test/java/de/relluem94/minecraft/server/spigot/essentials/RelluEssentialsTest.java
@@ -7,7 +7,11 @@ import java.util.UUID;
 import java.util.logging.Logger;
 
 import org.bukkit.GameMode;
+import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.OfflinePlayer;
+import org.bukkit.block.Block;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Assert;
@@ -23,17 +27,23 @@ import be.seeseemelk.mockbukkit.entity.PlayerMock;
 import ch.vorburger.exec.ManagedProcessException;
 import ch.vorburger.mariadb4j.DB;
 import de.relluem94.minecraft.server.spigot.essentials.constants.DatabaseConstants;
+import de.relluem94.minecraft.server.spigot.essentials.events.BetterPlayerJoin;
 import de.relluem94.minecraft.server.spigot.essentials.helpers.DatabaseHelper;
+import de.relluem94.minecraft.server.spigot.essentials.helpers.pojo.PlayerEntry;
+import de.relluem94.minecraft.server.spigot.essentials.permissions.Groups;
 
 public class RelluEssentialsTest {
 
     private static ServerMock server;
     private static RelluEssentials plugin;
     private static DB db;
+    private static PlayerMock player1;
+    private static PlayerMock player2;
+    private static PlayerMock player3;
+
 
     @BeforeClass
-    public static void setUpDB() throws ManagedProcessException {
-        Logger.getLogger(RelluEssentialsTest.class.getName()).severe("StartUp Database");
+    public static void setUp() throws ManagedProcessException {
         PrintStream sysOut = System.out;
 
         PrintStream noOut = new PrintStream(new OutputStream(){
@@ -54,85 +64,165 @@ public class RelluEssentialsTest {
 
 
         
-        Logger.getLogger(RelluEssentialsTest.class.getName()).severe("StartUp Plugin");
         server = MockBukkit.mock();
         plugin = MockBukkit.load(RelluEssentials.class);
-    }
 
-    @AfterClass
-    public static void tearDownDB() throws ManagedProcessException{
-        Logger.getLogger(RelluEssentialsTest.class.getName()).severe("Tear Down Plugin");
-        MockBukkit.unmock();
-
-        Logger.getLogger(RelluEssentialsTest.class.getName()).severe("Tear Down Database");
-        db.stop();
-    }
-
-
-    @Test
-    @DisplayName("Test Plugin 1")
-    public void test(){
-        System.out.println("Test1 >> 1");
-        Assert.assertTrue(plugin.isEnabled());
-        System.out.println("Test1 >> 2");
-        Assert.assertTrue(plugin.isUnitTest());
-        System.out.println("Test1 >> 3");
-
-    }
-
-
-    
-    @Test
-    @DisplayName("Test Plugin 2")
-    public void test2(){
-        System.out.println("Test2 >> 1");
         server.addSimpleWorld(Strings.PLUGIN_WORLD_LOBBY);
         server.addSimpleWorld(Strings.PLUGIN_WORLD_WORLD);
         server.addSimpleWorld(Strings.PLUGIN_WORLD_WORLD_NETHER);
         server.addSimpleWorld(Strings.PLUGIN_WORLD_WORLD_THE_END);
 
-        System.out.println("Test2 >> 2");
+        server.setOfflinePlayers(20);
 
-        PlayerMock player = server.addPlayer();
-        PlayerMock player2 = server.addPlayer();
+        player1 = server.addPlayer("PlayerOne");
+        player2 = server.addPlayer("PlayerTwo");
+        player3 = server.addPlayer("PlayerThree");
 
-        System.out.println("Test2 >> 3");
+        addPlayerToDB(player1, "admin");
+        addPlayerToDB(player2, "vip");
+        addPlayerToDB(player3, "user");
 
-        player.assertGameMode(GameMode.SURVIVAL); // TODO Hangs here!
-        player2.assertGameMode(GameMode.CREATIVE);
+        server.getScheduler().performTicks(100L);
 
-/*
- * 
- * 
-Running de.relluem94.minecraft.server.spigot.essentials.RelluEssentialsTest
-Mar 28, 2023 11:25:42 AM de.relluem94.minecraft.server.spigot.essentials.RelluEssentialsTest setUpDB
-SEVERE: StartUp Database
-Mar 28, 2023 11:25:45 AM de.relluem94.minecraft.server.spigot.essentials.RelluEssentialsTest setUpDB
-SEVERE: StartUp Plugin
-Test1 >> 1
-Test1 >> 2
-Test1 >> 3
-Test2 >> 1
-Test2 >> 2
-Test2 >> 3
-[11:25:46 SEVERE]: Tear Down Plugin
- * 
- */
+        
 
-        System.out.println("Test2 >> 4");
+        //server.execute("setgroup", server.getConsoleSender(), new String[]{player1.getName(), "mod"});
+        //server.execute("setgroup", server.getConsoleSender(), new String[]{player2.getName(), "vip"});
+        //server.execute("setgroup", server.getConsoleSender(), new String[]{player3.getName(), "user"});
 
-        player.simulateBlockBreak(server.getWorld(Strings.PLUGIN_WORLD_LOBBY).getBlockAt(1, 1, 1));
-        player2.simulateBlockBreak(server.getWorld(Strings.PLUGIN_WORLD_LOBBY).getBlockAt(1, 1, 2));
+        //player3.disconnect();
+    }
 
-        System.out.println("Test2 >> 5");
+    /**
+     * Only needed due to Could not pass event AsyncPlayerPreLoginEvent to RelluEssentials v3.0
+     */
+    private static void addPlayerToDB(PlayerMock p, String groupName){
+        PlayerEntry pe = new PlayerEntry();
+        pe.setUUID(p.getUniqueId().toString());
+        pe.setName(p.getName());
+        pe.setCustomName(p.getName());
+        pe.setGroup(Groups.getGroup(groupName));
 
-        server.execute("poke", player, player2.getName());
+        plugin.getDatabaseHelper().insertPlayer(pe);
 
-        System.out.println("Test2 >> 6");
+        pe = plugin.getDatabaseHelper().getPlayer(p.getUniqueId().toString());
 
-        Assertions.assertTrue(server.getOnlinePlayers().isEmpty());
-        Assert.assertTrue(server.getOnlinePlayers().isEmpty());
+        plugin.getPlayerAPI().putPlayerEntry(p.getUniqueId(), pe);
+    }
 
-        System.out.println("Test2 >> 8");
+    @AfterClass
+    public static void tearDown() throws ManagedProcessException{
+        MockBukkit.getMock().getScheduler().shutdown();
+        MockBukkit.unmock();
+        db.stop();
+    }
+
+
+    @Test
+    @DisplayName("Plugin Enabled")
+    public void testPluginEnabled(){
+        Assert.assertTrue(plugin.isEnabled());
+
+    }
+
+    @Test
+    @DisplayName("Plugin Unit Test")
+    public void testUnitTest(){
+        Assert.assertTrue(plugin.isUnitTest());
+    }
+
+    @Test
+    @DisplayName("Plugin World loaded Lobby")
+    public void testWorldLobbyLoaded(){
+        Assert.assertNotNull(server.getWorld(Strings.PLUGIN_WORLD_LOBBY));
+    }
+
+    @Test
+    @DisplayName("Plugin World Loading World")
+    public void testWorldWorldLoaded(){
+        Assert.assertNotNull(server.getWorld(Strings.PLUGIN_WORLD_WORLD));
+    }
+
+    @Test
+    @DisplayName("Plugin World Loading World Nether")
+    public void testWorldWorldNetherLoaded(){
+        Assert.assertNotNull(server.getWorld(Strings.PLUGIN_WORLD_WORLD_THE_END));
+    }
+
+    @Test
+    @DisplayName("Plugin World Loading World End")
+    public void testWorldWorldEndLoaded(){
+        Assert.assertNotNull(server.getWorld(Strings.PLUGIN_WORLD_WORLD_THE_END));
+    }
+
+    @Test
+    @DisplayName("Plugin Offline Player #1")
+    public void testPlayer1Online(){
+        Assert.assertTrue(player1.isOnline());
+    }
+
+    @Test
+    @DisplayName("Plugin Offline Player #2")
+    public void testPlayer2Online(){
+        Assert.assertTrue(player2.isOnline());
+    }
+
+    @Test
+    @DisplayName("Plugin Offline Player #3")
+    public void testPlayer3Offline(){
+        Assert.assertTrue(player3.isOnline());
+    }
+    
+    @Test
+    @DisplayName("Test Gamemmode Player #1")
+    public void testGameModePlayer1(){
+        Assert.assertEquals(GameMode.SURVIVAL, player1.getGameMode()); 
+    }
+
+    @Test
+    @DisplayName("Test Gamemmode Player #2")
+    public void testGameModePlayer2(){
+        Assert.assertEquals(GameMode.SURVIVAL, player2.getGameMode());
+    }
+
+    @Test
+    @DisplayName("Test Gamemmode Player #3")
+    public void testGameModePlayer3(){
+        Assert.assertEquals(GameMode.SURVIVAL, player3.getGameMode());
+    }
+
+    @Test
+    @DisplayName("Test BlockBreak Player #1")
+    public void testSimulateBlockBreakPlayer1(){
+        Block b = server.getWorld(Strings.PLUGIN_WORLD_LOBBY).getBlockAt(1, 1, 1);
+        Assertions.assertNotEquals(Material.AIR, b.getType());
+        player1.simulateBlockBreak(b);
+        Assert.assertEquals(Material.AIR, b.getType());
+    }
+
+    @Test
+    @DisplayName("Test BlockBreak Player #2")
+    public void testSimulateBlockBreakPlayer2(){
+        Block b = server.getWorld(Strings.PLUGIN_WORLD_LOBBY).getBlockAt(1, 1, 2);
+        Assertions.assertNotEquals(Material.AIR, b.getType());
+        player2.simulateBlockBreak(b);
+        Assert.assertEquals(Material.GRASS, b.getType());
+    }
+
+    @Test
+    @DisplayName("Test BlockBreak Player #3")
+    public void testSimulateBlockBreakPlayer3(){
+        Block b = server.getWorld(Strings.PLUGIN_WORLD_LOBBY).getBlockAt(1, 1, 3);
+        Assertions.assertNotEquals(Material.AIR, b.getType());
+        player2.simulateBlockBreak(b);
+        Assert.assertEquals(Material.GRASS, b.getType());
+    }
+
+    @Test
+    @DisplayName("Test Plugin 2")
+    public void test2(){
+
+        server.execute("poke", player1, player2.getName());
+        Assert.assertFalse(server.getOnlinePlayers().isEmpty());
     }
 }
