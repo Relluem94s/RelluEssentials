91c7ec5d7ef0987ed951d89181af5856b5af8a17
Relluem94 <github@relluem94.de>
2023-03-29 19:43:42 +0200

    RE-30 Added Console and CMD Block Options

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/commands/Title.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/commands/Title.java
@@ -1,29 +1,29 @@
 package de.relluem94.minecraft.server.spigot.essentials.commands;
 
+import static de.relluem94.minecraft.server.spigot.essentials.Strings.PLUGIN_COMMAND_PERMISSION_MISSING;
+import static de.relluem94.minecraft.server.spigot.essentials.Strings.PLUGIN_COMMAND_TARGET_NOT_A_PLAYER;
+import static de.relluem94.minecraft.server.spigot.essentials.Strings.PLUGIN_COMMAND_TO_LESS_ARGUMENTS;
+import static de.relluem94.minecraft.server.spigot.essentials.constants.CommandNameConstants.PLUGIN_COMMAND_NAME_TITLE;
+import static de.relluem94.minecraft.server.spigot.essentials.helpers.StringHelper.replaceColor;
+import static de.relluem94.minecraft.server.spigot.essentials.helpers.TypeHelper.isCMDBlock;
+import static de.relluem94.minecraft.server.spigot.essentials.helpers.TypeHelper.isConsole;
+import static de.relluem94.minecraft.server.spigot.essentials.helpers.TypeHelper.isPlayer;
+import static de.relluem94.rellulib.utils.StringUtils.implode;
+
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import de.relluem94.minecraft.server.spigot.essentials.permissions.Permission;
 import de.relluem94.minecraft.server.spigot.essentials.permissions.Groups;
-
-import static de.relluem94.rellulib.utils.StringUtils.*;
-
-import static de.relluem94.minecraft.server.spigot.essentials.Strings.*;
-import static de.relluem94.minecraft.server.spigot.essentials.constants.CommandNameConstants.PLUGIN_COMMAND_NAME_TITLE;
-import static de.relluem94.minecraft.server.spigot.essentials.helpers.StringHelper.replaceColor;
-import static de.relluem94.minecraft.server.spigot.essentials.helpers.TypeHelper.isPlayer;
+import de.relluem94.minecraft.server.spigot.essentials.permissions.Permission;
 
 public class Title implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         Player p = null;
-        Player target = null;
-
-        
 
         if (!command.getName().equalsIgnoreCase(PLUGIN_COMMAND_NAME_TITLE)) {
             return false;
@@ -34,6 +34,10 @@ public class Title implements CommandExecutor {
             return true;
         } 
 
+        if (isCMDBlock(sender) || isConsole(sender)) {
+            title(args, sender);
+        }
+        
         if (isPlayer(sender)) {
             p = (Player) sender;
         }
@@ -47,13 +51,17 @@ public class Title implements CommandExecutor {
             return true;
         } 
 
-        target = Bukkit.getPlayer(args[0]);
+        title(args, sender);
+        return true;
+    }
+
+    private void title(String[] args, CommandSender commandSender){
+        Player target = Bukkit.getPlayer(args[0]);
         if (target == null) {
-            p.sendMessage(String.format(PLUGIN_COMMAND_TARGET_NOT_A_PLAYER, args[0]));
-            return true;
+            commandSender.sendMessage(String.format(PLUGIN_COMMAND_TARGET_NOT_A_PLAYER, args[0]));
+            return;
         }   
 
         target.sendTitle(replaceColor(args[1]), replaceColor(implode(2, args)), 5, 80, 5);
-        return true;
     }
 }
