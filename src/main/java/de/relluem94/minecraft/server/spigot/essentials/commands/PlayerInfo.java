67235e4ac29522ee158add278fd2395d9a02a05c
Relluem94 <github@relluem94.de>
2023-03-29 21:57:12 +0200

    RE-30 Added Console and CMD Block Options & RE-45 Linted & excluded strings into Strings.java

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/commands/PlayerInfo.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/commands/PlayerInfo.java
@@ -1,24 +1,37 @@
 package de.relluem94.minecraft.server.spigot.essentials.commands;
 
+import static de.relluem94.minecraft.server.spigot.essentials.Strings.PLUGIN_COMMAND_PERMISSION_MISSING;
+import static de.relluem94.minecraft.server.spigot.essentials.Strings.PLUGIN_COMMAND_PLAYERINFO;
+import static de.relluem94.minecraft.server.spigot.essentials.Strings.PLUGIN_COMMAND_PLAYERINFO_DEATHPOINTS;
+import static de.relluem94.minecraft.server.spigot.essentials.Strings.PLUGIN_COMMAND_PLAYERINFO_DEATHS;
+import static de.relluem94.minecraft.server.spigot.essentials.Strings.PLUGIN_COMMAND_PLAYERINFO_FIRST_ONLINE;
+import static de.relluem94.minecraft.server.spigot.essentials.Strings.PLUGIN_COMMAND_PLAYERINFO_GROUP;
+import static de.relluem94.minecraft.server.spigot.essentials.Strings.PLUGIN_COMMAND_PLAYERINFO_HOMES;
+import static de.relluem94.minecraft.server.spigot.essentials.Strings.PLUGIN_COMMAND_PLAYERINFO_JUMPED;
+import static de.relluem94.minecraft.server.spigot.essentials.Strings.PLUGIN_COMMAND_PLAYERINFO_LAST_ONLINE;
+import static de.relluem94.minecraft.server.spigot.essentials.Strings.PLUGIN_COMMAND_PLAYERINFO_LEFT_GAME;
+import static de.relluem94.minecraft.server.spigot.essentials.Strings.PLUGIN_COMMAND_PLAYERINFO_MARRIED_SINCE;
+import static de.relluem94.minecraft.server.spigot.essentials.Strings.PLUGIN_COMMAND_PLAYERINFO_MARRIED_TO;
+import static de.relluem94.minecraft.server.spigot.essentials.Strings.PLUGIN_COMMAND_PLAYERINFO_MINED;
+import static de.relluem94.minecraft.server.spigot.essentials.Strings.PLUGIN_COMMAND_TARGET_NOT_A_PLAYER;
+import static de.relluem94.minecraft.server.spigot.essentials.Strings.PLUGIN_COMMAND_TO_LESS_ARGUMENTS;
+import static de.relluem94.minecraft.server.spigot.essentials.Strings.PLUGIN_COMMAND_TO_MANY_ARGUMENTS;
+import static de.relluem94.minecraft.server.spigot.essentials.constants.CommandNameConstants.PLUGIN_COMMAND_NAME_PLAYERINFO;
+
+import java.util.Date;
+
 import org.bukkit.Material;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.Statistic;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 
-import de.relluem94.minecraft.server.spigot.essentials.permissions.Permission;
 import de.relluem94.minecraft.server.spigot.essentials.RelluEssentials;
 import de.relluem94.minecraft.server.spigot.essentials.helpers.PlayerHelper;
 import de.relluem94.minecraft.server.spigot.essentials.helpers.pojo.PlayerEntry;
 import de.relluem94.minecraft.server.spigot.essentials.permissions.Groups;
-
-import static de.relluem94.minecraft.server.spigot.essentials.Strings.*;
-import static de.relluem94.minecraft.server.spigot.essentials.constants.CommandNameConstants.PLUGIN_COMMAND_NAME_PLAYERINFO;
-import static de.relluem94.minecraft.server.spigot.essentials.helpers.TypeHelper.isPlayer;
-
-import java.util.Date;
+import de.relluem94.minecraft.server.spigot.essentials.permissions.Permission;
 
 
 public class PlayerInfo implements CommandExecutor {
@@ -30,77 +43,68 @@ public class PlayerInfo implements CommandExecutor {
             return false;
         }
 
-        Player p = null;
-
-        if (isPlayer(sender)) {
-            p = (Player) sender;
-        }
-
-        if(p == null){
-            return true;
-        }
-
-        if (!Permission.isAuthorized(p, Groups.getGroup("user").getId())) {
-            p.sendMessage(PLUGIN_COMMAND_PERMISSION_MISSING);
-            return true;
-        }
-      
-        
-
         if (args.length < 1) {
-            p.sendMessage(PLUGIN_COMMAND_TO_LESS_ARGUMENTS);
+            sender.sendMessage(PLUGIN_COMMAND_TO_LESS_ARGUMENTS);
             return true;
         }
 
         if (args.length > 1) {
-            p.sendMessage(PLUGIN_COMMAND_TO_MANY_ARGUMENTS);
+            sender.sendMessage(PLUGIN_COMMAND_TO_MANY_ARGUMENTS);
             return true;
         }
 
-
-
-
+        if (!Permission.isAuthorized(sender, Groups.getGroup("user").getId())) {
+            sender.sendMessage(PLUGIN_COMMAND_PERMISSION_MISSING);
+            return true;
+        }
+      
         OfflinePlayer target = PlayerHelper.getOfflinePlayer(args[0]);
         
         if (target == null) {
-            p.sendMessage(String.format(PLUGIN_COMMAND_TARGET_NOT_A_PLAYER, args[0]));
+            sender.sendMessage(String.format(PLUGIN_COMMAND_TARGET_NOT_A_PLAYER, args[0]));
             return true;
         }
 
-        PlayerEntry pet = RelluEssentials.getInstance().getPlayerAPI().getPlayerEntry(p);
+        showPlayerInfo(sender, args[0]);
+        return true;
+    }
+
+    private void showPlayerInfo(CommandSender sender, String targetName){
+        OfflinePlayer target = PlayerHelper.getOfflinePlayer(targetName);
+        
+        if (target == null) {
+            sender.sendMessage(String.format(PLUGIN_COMMAND_TARGET_NOT_A_PLAYER, targetName));
+            return;
+        }
+
+        PlayerEntry pet = RelluEssentials.getInstance().getPlayerAPI().getPlayerEntry(target.getUniqueId());
 
         if(pet == null){
-            p.sendMessage(String.format(PLUGIN_COMMAND_TARGET_NOT_A_PLAYER, args[0]));
-            return true;
+            sender.sendMessage(String.format(PLUGIN_COMMAND_TARGET_NOT_A_PLAYER, targetName));
+            return;
         }
 
-        p.sendMessage(String.format(PLUGIN_COMMAND_PLAYERINFO, target.getName()));
-        p.sendMessage(String.format("Homes: %s", pet.getHomes().size()));
-        p.sendMessage(String.format("DeathPoints: %s", pet.getDeaths().size()));
-        p.sendMessage(String.format("Group: %s", pet.getGroup().getPrefix() + pet.getGroup().getName()));
+        sender.sendMessage(String.format(PLUGIN_COMMAND_PLAYERINFO, target.getName()));
+        sender.sendMessage(String.format(PLUGIN_COMMAND_PLAYERINFO_HOMES, pet.getHomes().size()));
+        sender.sendMessage(String.format(PLUGIN_COMMAND_PLAYERINFO_DEATHPOINTS, pet.getDeaths().size()));
+        sender.sendMessage(String.format(PLUGIN_COMMAND_PLAYERINFO_GROUP, pet.getGroup().getPrefix() + pet.getGroup().getName()));
 
         if(pet.getPartner() != null){
-            p.sendMessage(String.format("Married: %s $4\u2665§f %s",  RelluEssentials.getInstance().getPlayerAPI().getPlayerEntry(pet.getPartner().getFirstPlayerID()).getName(), RelluEssentials.getInstance().getPlayerAPI().getPlayerEntry(pet.getPartner().getSecondPlayerID())));
-            p.sendMessage(String.format("Married since: %s", pet.getPartner().getCreated()));
+            sender.sendMessage(String.format(PLUGIN_COMMAND_PLAYERINFO_MARRIED_TO,  RelluEssentials.getInstance().getPlayerAPI().getPlayerEntry(pet.getPartner().getFirstPlayerID()).getName(), RelluEssentials.getInstance().getPlayerAPI().getPlayerEntry(pet.getPartner().getSecondPlayerID())));
+            sender.sendMessage(String.format(PLUGIN_COMMAND_PLAYERINFO_MARRIED_SINCE, pet.getPartner().getCreated()));
         }
 
+        sender.sendMessage(String.format(PLUGIN_COMMAND_PLAYERINFO_LAST_ONLINE, new Date(target.getLastPlayed()).toString()));
+        sender.sendMessage(String.format(PLUGIN_COMMAND_PLAYERINFO_FIRST_ONLINE, new Date(target.getFirstPlayed()).toString()));
 
-        
-        p.sendMessage(String.format("Last Online: %s", new Date(target.getLastPlayed()).toString()));
-        p.sendMessage(String.format("First Online: %s", new Date(target.getFirstPlayed()).toString()));
-
-        
-
-        p.sendMessage(String.format("%s mined: %s", Material.STONE.name(), target.getStatistic(Statistic.MINE_BLOCK, Material.STONE)));
-        p.sendMessage(String.format("%s mined: %s", Material.DIRT.name(), target.getStatistic(Statistic.MINE_BLOCK, Material.DIRT)));
-        p.sendMessage(String.format("%s mined: %s", Material.SAND.name(), target.getStatistic(Statistic.MINE_BLOCK, Material.SAND)));
-        p.sendMessage(String.format("%s mined: %s", Material.COBBLESTONE.name(), target.getStatistic(Statistic.MINE_BLOCK, Material.COBBLESTONE)));
-        p.sendMessage(String.format("%s mined: %s", Material.DEEPSLATE.name(), target.getStatistic(Statistic.MINE_BLOCK, Material.DEEPSLATE)));
-        p.sendMessage(String.format("%s mined: %s", Material.DIAMOND.name(), target.getStatistic(Statistic.MINE_BLOCK, Material.DIAMOND)));
-        p.sendMessage(String.format("Deaths: %s", target.getStatistic(Statistic.DEATHS)));
-        p.sendMessage(String.format("Jumped: %s", target.getStatistic(Statistic.JUMP)));
-        p.sendMessage(String.format("Leaved Game: %s", target.getStatistic(Statistic.LEAVE_GAME)));
-
-        return true;
+        sender.sendMessage(String.format(PLUGIN_COMMAND_PLAYERINFO_MINED, Material.STONE.name(), target.getStatistic(Statistic.MINE_BLOCK, Material.STONE)));
+        sender.sendMessage(String.format(PLUGIN_COMMAND_PLAYERINFO_MINED, Material.DIRT.name(), target.getStatistic(Statistic.MINE_BLOCK, Material.DIRT)));
+        sender.sendMessage(String.format(PLUGIN_COMMAND_PLAYERINFO_MINED, Material.SAND.name(), target.getStatistic(Statistic.MINE_BLOCK, Material.SAND)));
+        sender.sendMessage(String.format(PLUGIN_COMMAND_PLAYERINFO_MINED, Material.COBBLESTONE.name(), target.getStatistic(Statistic.MINE_BLOCK, Material.COBBLESTONE)));
+        sender.sendMessage(String.format(PLUGIN_COMMAND_PLAYERINFO_MINED, Material.DEEPSLATE.name(), target.getStatistic(Statistic.MINE_BLOCK, Material.DEEPSLATE)));
+        sender.sendMessage(String.format(PLUGIN_COMMAND_PLAYERINFO_MINED, Material.DIAMOND.name(), target.getStatistic(Statistic.MINE_BLOCK, Material.DIAMOND)));
+        sender.sendMessage(String.format(PLUGIN_COMMAND_PLAYERINFO_DEATHS, target.getStatistic(Statistic.DEATHS)));
+        sender.sendMessage(String.format(PLUGIN_COMMAND_PLAYERINFO_JUMPED, target.getStatistic(Statistic.JUMP)));
+        sender.sendMessage(String.format(PLUGIN_COMMAND_PLAYERINFO_LEFT_GAME, target.getStatistic(Statistic.LEAVE_GAME)));
     }
 }
