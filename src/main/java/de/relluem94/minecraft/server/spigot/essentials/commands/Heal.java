77ae090d190072e34f2e2f167d801e150dcf6bc5
Relluem94 <github@relluem94.de>
2023-03-29 22:07:46 +0200

    RE-30 Added Console and CMD Block Options

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/commands/Heal.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/commands/Heal.java
@@ -1,17 +1,25 @@
 package de.relluem94.minecraft.server.spigot.essentials.commands;
 
+import static de.relluem94.minecraft.server.spigot.essentials.Strings.PLUGIN_COMMAND_HEAL;
+import static de.relluem94.minecraft.server.spigot.essentials.Strings.PLUGIN_COMMAND_NOT_A_PLAYER;
+import static de.relluem94.minecraft.server.spigot.essentials.Strings.PLUGIN_COMMAND_PERMISSION_MISSING;
+import static de.relluem94.minecraft.server.spigot.essentials.Strings.PLUGIN_COMMAND_TARGET_NOT_A_PLAYER;
+import static de.relluem94.minecraft.server.spigot.essentials.Strings.PLUGIN_COMMAND_TO_LESS_ARGUMENTS;
+import static de.relluem94.minecraft.server.spigot.essentials.Strings.PLUGIN_COMMAND_TO_MANY_ARGUMENTS;
+import static de.relluem94.minecraft.server.spigot.essentials.constants.CommandNameConstants.PLUGIN_COMMAND_NAME_HEAL;
+import static de.relluem94.minecraft.server.spigot.essentials.helpers.TypeHelper.isCMDBlock;
+import static de.relluem94.minecraft.server.spigot.essentials.helpers.TypeHelper.isConsole;
+import static de.relluem94.minecraft.server.spigot.essentials.helpers.TypeHelper.isPlayer;
+
+import org.bukkit.Bukkit;
+import org.bukkit.attribute.Attribute;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.attribute.Attribute;
 
-import de.relluem94.minecraft.server.spigot.essentials.permissions.Permission;
 import de.relluem94.minecraft.server.spigot.essentials.permissions.Groups;
-
-import static de.relluem94.minecraft.server.spigot.essentials.Strings.*;
-import static de.relluem94.minecraft.server.spigot.essentials.constants.CommandNameConstants.PLUGIN_COMMAND_NAME_HEAL;
-import static de.relluem94.minecraft.server.spigot.essentials.helpers.TypeHelper.isPlayer;
+import de.relluem94.minecraft.server.spigot.essentials.permissions.Permission;
 
 public class Heal implements CommandExecutor {
 
@@ -20,6 +28,16 @@ public class Heal implements CommandExecutor {
         if (!command.getName().equalsIgnoreCase(PLUGIN_COMMAND_NAME_HEAL)) {
             return false;
         }
+
+        if (isCMDBlock(sender) || isConsole(sender)) {
+            if (args.length < 1) {
+                sender.sendMessage(PLUGIN_COMMAND_TO_LESS_ARGUMENTS);
+                return true;   
+            }
+            
+            heal(sender, args[0]);
+        }
+
 
         if (!isPlayer(sender)) {
             sender.sendMessage(PLUGIN_COMMAND_NOT_A_PLAYER);
@@ -38,9 +56,24 @@ public class Heal implements CommandExecutor {
             return true;   
         }
 
+        heal(p);
+        return true;
+    }
+
+    private void heal(Player p) {
         p.setHealth(p.getAttribute(Attribute.GENERIC_MAX_HEALTH).getDefaultValue());
         p.setFoodLevel(20);
         p.sendMessage(PLUGIN_COMMAND_HEAL);
-        return true;
+    }
+
+    private void heal(CommandSender sender, String targetName) {
+        Player target = Bukkit.getPlayer(targetName);
+        
+        if (target == null) {
+            sender.sendMessage(String.format(PLUGIN_COMMAND_TARGET_NOT_A_PLAYER, targetName));
+            return;
+        }
+
+        heal(target);
     }
 }
