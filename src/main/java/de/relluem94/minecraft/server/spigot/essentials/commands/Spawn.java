257d80ab84b68f0db3eb415be2f9d35cfde5185b
Relluem94 <github@relluem94.de>
2023-03-29 23:02:39 +0200

    RE-30 Added Console and CMD Block Options & RE-42 Never nested

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/commands/Spawn.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/commands/Spawn.java
@@ -1,54 +1,64 @@
 package de.relluem94.minecraft.server.spigot.essentials.commands;
 
+import static de.relluem94.minecraft.server.spigot.essentials.Strings.PLUGIN_COMMAND_NOT_A_PLAYER;
+import static de.relluem94.minecraft.server.spigot.essentials.Strings.PLUGIN_COMMAND_PERMISSION_MISSING;
+import static de.relluem94.minecraft.server.spigot.essentials.Strings.PLUGIN_COMMAND_SPAWN;
+import static de.relluem94.minecraft.server.spigot.essentials.Strings.PLUGIN_COMMAND_TARGET_NOT_A_PLAYER;
+import static de.relluem94.minecraft.server.spigot.essentials.constants.CommandNameConstants.PLUGIN_COMMAND_NAME_SPAWN;
+import static de.relluem94.minecraft.server.spigot.essentials.helpers.TypeHelper.isPlayer;
+
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import de.relluem94.minecraft.server.spigot.essentials.permissions.Permission;
 import de.relluem94.minecraft.server.spigot.essentials.permissions.Groups;
-
-import static de.relluem94.minecraft.server.spigot.essentials.Strings.*;
-import static de.relluem94.minecraft.server.spigot.essentials.constants.CommandNameConstants.PLUGIN_COMMAND_NAME_SPAWN;
-import static de.relluem94.minecraft.server.spigot.essentials.helpers.TypeHelper.isPlayer;
+import de.relluem94.minecraft.server.spigot.essentials.permissions.Permission;
 
 public class Spawn implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (command.getName().equalsIgnoreCase(PLUGIN_COMMAND_NAME_SPAWN)) {
-            if (args.length == 0) {
-                if (isPlayer(sender)) {
-                    Player p = (Player) sender;
-                    if (Permission.isAuthorized(p, Groups.getGroup("user").getId())) {
-                        Back.addBackPoint(p);
-                        p.teleport(p.getWorld().getSpawnLocation());
-                        p.sendMessage(String.format(PLUGIN_COMMAND_SPAWN, p.getWorld().getName()));
-                        return true;
-                    } else {
-                        p.sendMessage(PLUGIN_COMMAND_PERMISSION_MISSING);
-                        return true;
-                    }
-                }
-            } else {
-                Player target = Bukkit.getPlayer(args[0]);
-                if (target != null) {
-                    if (isPlayer(sender)) {
-                        Player p = (Player) sender;
-                        if (Permission.isAuthorized(p, Groups.getGroup("mod").getId())) {
-                            Back.addBackPoint(target);
-                            target.teleport(target.getWorld().getSpawnLocation());
-                            target.sendMessage(String.format(PLUGIN_COMMAND_SPAWN, target.getWorld().getName()));
-                            return true;
-                        } else {
-                            p.sendMessage(PLUGIN_COMMAND_PERMISSION_MISSING);
-                            return true;
-                        }
-                    }
-                }
-            }
+        if (!command.getName().equalsIgnoreCase(PLUGIN_COMMAND_NAME_SPAWN)) {
+            return false;
         }
-        return false;
+
+        if (args.length != 0) {
+            Player target = Bukkit.getPlayer(args[0]);
+            if (target == null) {
+                sender.sendMessage(String.format(PLUGIN_COMMAND_TARGET_NOT_A_PLAYER, args[0]));
+                return true;
+            }
+
+            if (!Permission.isAuthorized(sender, Groups.getGroup("mod").getId())) {
+                sender.sendMessage(PLUGIN_COMMAND_PERMISSION_MISSING);
+                return true;
+            }
+
+            spawn(target);
+            return true;
+        } 
+
+        if (!isPlayer(sender)) {
+            sender.sendMessage(PLUGIN_COMMAND_NOT_A_PLAYER);
+            return true;
+        }
+
+        Player p = (Player) sender;
+            
+        if (!Permission.isAuthorized(p, Groups.getGroup("user").getId())) {
+            p.sendMessage(PLUGIN_COMMAND_PERMISSION_MISSING);
+            return true;
+        }
+
+        spawn(p);
+        return true;
+    }
+
+    public void spawn(Player p){
+        Back.addBackPoint(p);
+        p.teleport(p.getWorld().getSpawnLocation());
+        p.sendMessage(String.format(PLUGIN_COMMAND_SPAWN, p.getWorld().getName()));
     }
 }
