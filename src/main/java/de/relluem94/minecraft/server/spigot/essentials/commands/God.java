f0c7c2fb46ffd804ebfd8ec57e6cb844dce953d6
Relluem94 <github@relluem94.de>
2023-03-29 22:14:44 +0200

    RE-30 Added Console and CMD Block Options

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/commands/God.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/commands/God.java
@@ -1,16 +1,25 @@
 package de.relluem94.minecraft.server.spigot.essentials.commands;
 
+import static de.relluem94.minecraft.server.spigot.essentials.Strings.PLUGIN_COMMAND_GOD_OFF;
+import static de.relluem94.minecraft.server.spigot.essentials.Strings.PLUGIN_COMMAND_GOD_ON;
+import static de.relluem94.minecraft.server.spigot.essentials.Strings.PLUGIN_COMMAND_NOT_A_PLAYER;
+import static de.relluem94.minecraft.server.spigot.essentials.Strings.PLUGIN_COMMAND_PERMISSION_MISSING;
+import static de.relluem94.minecraft.server.spigot.essentials.Strings.PLUGIN_COMMAND_TARGET_NOT_A_PLAYER;
+import static de.relluem94.minecraft.server.spigot.essentials.Strings.PLUGIN_COMMAND_TO_LESS_ARGUMENTS;
+import static de.relluem94.minecraft.server.spigot.essentials.Strings.PLUGIN_COMMAND_TO_MANY_ARGUMENTS;
+import static de.relluem94.minecraft.server.spigot.essentials.constants.CommandNameConstants.PLUGIN_COMMAND_NAME_GOD;
+import static de.relluem94.minecraft.server.spigot.essentials.helpers.TypeHelper.isCMDBlock;
+import static de.relluem94.minecraft.server.spigot.essentials.helpers.TypeHelper.isConsole;
+import static de.relluem94.minecraft.server.spigot.essentials.helpers.TypeHelper.isPlayer;
+
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import de.relluem94.minecraft.server.spigot.essentials.permissions.Permission;
 import de.relluem94.minecraft.server.spigot.essentials.permissions.Groups;
-
-import static de.relluem94.minecraft.server.spigot.essentials.Strings.*;
-import static de.relluem94.minecraft.server.spigot.essentials.constants.CommandNameConstants.PLUGIN_COMMAND_NAME_GOD;
-import static de.relluem94.minecraft.server.spigot.essentials.helpers.TypeHelper.isPlayer;
+import de.relluem94.minecraft.server.spigot.essentials.permissions.Permission;
 
 public class God implements CommandExecutor {
 
@@ -18,6 +27,16 @@ public class God implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (!command.getName().equalsIgnoreCase(PLUGIN_COMMAND_NAME_GOD)) {
             return false;
+        }
+
+        if (isCMDBlock(sender) || isConsole(sender)) {
+            if (args.length < 1) {
+                sender.sendMessage(PLUGIN_COMMAND_TO_LESS_ARGUMENTS);
+                return true;   
+            }
+            
+            toggleGodMode(sender, args[0]);
+            return true; 
         }
 
         if (!isPlayer(sender)) {
@@ -30,7 +49,6 @@ public class God implements CommandExecutor {
         if (!Permission.isAuthorized(p, Groups.getGroup("mod").getId())) {
             p.sendMessage(PLUGIN_COMMAND_PERMISSION_MISSING);
             return true;
-            
         }
 
         if (args.length > 0) {
@@ -38,8 +56,23 @@ public class God implements CommandExecutor {
             return true;   
         }
 
+        toggleGodMode(p);
+        return true;
+    }
+
+    private void toggleGodMode(Player p){
         p.sendMessage(!p.isInvulnerable() ? PLUGIN_COMMAND_GOD_ON : PLUGIN_COMMAND_GOD_OFF);
         p.setInvulnerable(!p.isInvulnerable());
-        return true;
+    }
+
+    private void toggleGodMode(CommandSender sender, String targetName) {
+        Player target = Bukkit.getPlayer(targetName);
+        
+        if (target == null) {
+            sender.sendMessage(String.format(PLUGIN_COMMAND_TARGET_NOT_A_PLAYER, targetName));
+            return;
+        }
+
+        toggleGodMode(target);
     }
 }
