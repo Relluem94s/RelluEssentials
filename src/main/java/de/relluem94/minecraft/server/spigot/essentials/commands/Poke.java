7f92add74981bfaa47e269bcdfe893fe66bcdfe0
Relluem94 <github@relluem94.de>
2023-03-29 22:53:53 +0200

    RE-30 Added Console and CMD Block Options & RE-42 Never nested

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/commands/Poke.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/commands/Poke.java
@@ -1,59 +1,68 @@
 package de.relluem94.minecraft.server.spigot.essentials.commands;
 
+import static de.relluem94.minecraft.server.spigot.essentials.Strings.PLUGIN_COMMAND_PERMISSION_MISSING;
+import static de.relluem94.minecraft.server.spigot.essentials.Strings.PLUGIN_COMMAND_POKE;
+import static de.relluem94.minecraft.server.spigot.essentials.Strings.PLUGIN_COMMAND_POKE_MESSAGE_SENDER;
+import static de.relluem94.minecraft.server.spigot.essentials.Strings.PLUGIN_COMMAND_POKE_MESSAGE_TARGET;
+import static de.relluem94.minecraft.server.spigot.essentials.Strings.PLUGIN_COMMAND_POKE_SUBTITLE;
+import static de.relluem94.minecraft.server.spigot.essentials.Strings.PLUGIN_COMMAND_POKE_TITLE;
+import static de.relluem94.minecraft.server.spigot.essentials.Strings.PLUGIN_COMMAND_TARGET_NOT_A_PLAYER;
+import static de.relluem94.minecraft.server.spigot.essentials.Strings.PLUGIN_NAME_CHAT_CONSOLE;
+import static de.relluem94.minecraft.server.spigot.essentials.constants.CommandNameConstants.PLUGIN_COMMAND_NAME_POKE;
+import static de.relluem94.minecraft.server.spigot.essentials.helpers.ChatHelper.sendMessage;
+import static de.relluem94.minecraft.server.spigot.essentials.helpers.TypeHelper.isPlayer;
+
 import org.bukkit.Bukkit;
+import org.bukkit.Effect;
+import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.Effect;
-import org.bukkit.Sound;
 
-import de.relluem94.minecraft.server.spigot.essentials.permissions.Permission;
 import de.relluem94.minecraft.server.spigot.essentials.permissions.Groups;
-
-import static de.relluem94.minecraft.server.spigot.essentials.Strings.*;
-import static de.relluem94.minecraft.server.spigot.essentials.constants.CommandNameConstants.PLUGIN_COMMAND_NAME_POKE;
-import static de.relluem94.minecraft.server.spigot.essentials.helpers.ChatHelper.sendMessage;
-import static de.relluem94.minecraft.server.spigot.essentials.helpers.TypeHelper.isPlayer;
+import de.relluem94.minecraft.server.spigot.essentials.permissions.Permission;
 
 public class Poke implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (command.getName().equalsIgnoreCase(PLUGIN_COMMAND_NAME_POKE)) {
-            if (args.length == 0) {
-                if (isPlayer(sender)) {
-                    Player p = (Player) sender;
-                    if (Permission.isAuthorized(p, Groups.getGroup("vip").getId())) {
-                        sendMessage(p, PLUGIN_COMMAND_POKE);
-                        return true;
-                    } else {
-                        sendMessage(p, PLUGIN_COMMAND_PERMISSION_MISSING);
-                        return true;
-                    }
-                }
-            } else {
-                Player target = Bukkit.getPlayer(args[0]);
-                if (target != null) {
-                    if (isPlayer(sender)) {
-                        Player p = (Player) sender;
-                        if (Permission.isAuthorized(p, Groups.getGroup("vip").getId())) {
-                            target.getWorld().playSound(target.getLocation(), Sound.ENTITY_GENERIC_EXPLODE, 10F, 0F);
-                            target.getWorld().playEffect(target.getLocation(), Effect.MOBSPAWNER_FLAMES, 5);
-                            target.getWorld().playEffect(target.getLocation(), Effect.EXTINGUISH, 5);
-                            target.getWorld().playEffect(target.getLocation(), Effect.ENDERDRAGON_GROWL, 5);
-                            target.sendTitle(PLUGIN_COMMAND_POKE_TITLE, PLUGIN_COMMAND_POKE_SUBTITLE, 5, 80, 5);
-                            sendMessage(target, String.format(PLUGIN_COMMAND_POKE_MESSAGE_TARGET, p.getDisplayName()));
-                            sendMessage(p, String.format(PLUGIN_COMMAND_POKE_MESSAGE_SENDER, target.getDisplayName()));
-                            return true;
-                        } else {
-                            sendMessage(p, PLUGIN_COMMAND_PERMISSION_MISSING);
-                            return true;
-                        }
-                    }
-                }
-            }
+        if (!command.getName().equalsIgnoreCase(PLUGIN_COMMAND_NAME_POKE)) {
+            return false;
         }
-        return false;
+
+        if (args.length == 0) {
+            if (!Permission.isAuthorized(sender, Groups.getGroup("vip").getId())) {
+                sendMessage(sender, PLUGIN_COMMAND_PERMISSION_MISSING);
+                return true;
+            } 
+
+            sendMessage(sender, PLUGIN_COMMAND_POKE);
+            return true;
+        } 
+
+        Player target = Bukkit.getPlayer(args[0]);
+        if (target == null) {
+            sender.sendMessage(String.format(PLUGIN_COMMAND_TARGET_NOT_A_PLAYER, args[0]));
+            return true;
+        }
+
+        if (!Permission.isAuthorized(sender, Groups.getGroup("vip").getId())) {
+            sendMessage(sender, PLUGIN_COMMAND_PERMISSION_MISSING);
+            return true;
+        }
+
+        poke(target);
+        sendMessage(target, String.format(PLUGIN_COMMAND_POKE_MESSAGE_TARGET, isPlayer(sender) ? ((Player)sender).getDisplayName() : PLUGIN_NAME_CHAT_CONSOLE + sender.getName()));
+        sendMessage(sender, String.format(PLUGIN_COMMAND_POKE_MESSAGE_SENDER, target.getDisplayName()));
+        return true;
+    }
+
+    private void poke(Player target){
+        target.getWorld().playSound(target.getLocation(), Sound.ENTITY_GENERIC_EXPLODE, 10F, 0F);
+        target.getWorld().playEffect(target.getLocation(), Effect.MOBSPAWNER_FLAMES, 5);
+        target.getWorld().playEffect(target.getLocation(), Effect.EXTINGUISH, 5);
+        target.getWorld().playEffect(target.getLocation(), Effect.ENDERDRAGON_GROWL, 5);
+        target.sendTitle(PLUGIN_COMMAND_POKE_TITLE, PLUGIN_COMMAND_POKE_SUBTITLE, 5, 80, 5);
     }
 }
