7be314c67f5741e6370186223d818257c52bf268
Relluem94 <github@relluem94.de>
2023-06-18 21:58:23 +0200

    RE-83 added command sender to sudo

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/commands/Sudo.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/commands/Sudo.java
@@ -1,12 +1,15 @@
 package de.relluem94.minecraft.server.spigot.essentials.commands;
 
 import static de.relluem94.minecraft.server.spigot.essentials.Strings.PLUGIN_COMMAND_PERMISSION_MISSING;
+import static de.relluem94.minecraft.server.spigot.essentials.Strings.PLUGIN_COMMAND_WRONG_SUB_COMMAND;
 import static de.relluem94.minecraft.server.spigot.essentials.constants.CommandNameConstants.PLUGIN_COMMAND_NAME_SUDO;
 import static de.relluem94.minecraft.server.spigot.essentials.helpers.TypeHelper.isPlayer;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 
 import de.relluem94.minecraft.server.spigot.essentials.RelluEssentials;
@@ -18,12 +21,18 @@ import de.relluem94.minecraft.server.spigot.essentials.helpers.pojo.PlayerEntry;
 import de.relluem94.minecraft.server.spigot.essentials.managers.SudoManager;
 import de.relluem94.minecraft.server.spigot.essentials.permissions.Groups;
 import de.relluem94.minecraft.server.spigot.essentials.permissions.Permission;
+import de.relluem94.rellulib.utils.StringUtils;
 
 public class Sudo implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (command.getName().equalsIgnoreCase(PLUGIN_COMMAND_NAME_SUDO)) {
+            if(Bukkit.getCommandAliases().containsKey(args[0])){
+                dispatchCommand(args);
+                return true;
+            }
+
             if (args.length == 1) {
                 if (isPlayer(sender)) {
                     Player p = (Player) sender;
@@ -51,6 +60,7 @@ public class Sudo implements CommandExecutor {
                             p.sendMessage(String.format(Strings.PLUGIN_COMMAND_SUDO_ACTIVATED, tpe.getGroup().getPrefix() + target.getName()));
                         }
                         else{
+                            
                             p.sendMessage(String.format(Strings.PLUGIN_COMMAND_SUDO_PLAYER_NOT_FOUND, args[0]));
                         }
                         return true;
@@ -61,8 +71,17 @@ public class Sudo implements CommandExecutor {
                     }
                 }
             }
+            else{
+                sender.sendMessage(PLUGIN_COMMAND_WRONG_SUB_COMMAND);
+                return true;
+            }
         }
         return false;
+    }
+
+    private void dispatchCommand(String[] args){
+        ConsoleCommandSender console = Bukkit.getServer().getConsoleSender();
+        Bukkit.getServer().dispatchCommand(console, StringUtils.toString(args));
     }
 
     public static void exitSudo(Player p){
