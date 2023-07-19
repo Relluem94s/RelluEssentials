299cc33a803d1f67f401565136d00e847203ee5c
Relluem94 <github@relluem94.de>
2023-07-19 19:17:28 +0200

    RE-110 added script

--- src/main/resources/sqls/patches/v6/insertNetherBagType.sql
+++ src/main/resources/sqls/patches/v6/insertNetherBagType.sql
@@ -5,9 +5,3 @@
 
 INSERT INTO `rellu_essentials`.`bag_type` (`id`, `displayname`, `name`, `cost`, `slot_1_name`, `slot_2_name`, `slot_3_name`, `slot_4_name`, `slot_5_name`, `slot_6_name`, `slot_7_name`, `slot_8_name`, `slot_9_name`, `slot_10_name`, `slot_11_name`, `slot_12_name`, `slot_13_name`, `slot_14_name`) VALUES 
 (3, 'Nether Bag', 'NETHER', 500000, '', '', '', '', '', '', '', '', '', '', '', '', '', '');
-
-
-/**
-GHAST_TEAR
-BLAZE_ROD
-*/
