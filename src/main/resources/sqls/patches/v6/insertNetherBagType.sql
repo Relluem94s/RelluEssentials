d24aac8af3321e018901c7f9f5c41a08079b7d84
Relluem94 <github@relluem94.de>
2023-07-20 14:28:56 +0200

    RE-110 added items to bag

--- src/main/resources/sqls/patches/v6/insertNetherBagType.sql
+++ src/main/resources/sqls/patches/v6/insertNetherBagType.sql
@@ -1,11 +1,74 @@
 /**
  * Author:  rellu
- * Created: 07.02.2023
+ * Created: 20.07.2023
  */
 
-INSERT INTO `rellu_essentials`.`bag_type` (`id`, `displayname`, `name`, `cost`, 
-`slot_1_name`, `slot_2_name`, `slot_3_name`, `slot_4_name`, `slot_5_name`, `slot_6_name`, `slot_7_name`, `slot_8_name`, `slot_9_name`, `slot_10_name`, `slot_11_name`, `slot_12_name`, `slot_13_name`, `slot_14_name`
-`slot_15_name`, `slot_16_name`, `slot_17_name`, `slot_18_name`, `slot_19_name`, `slot_20_name`, `slot_21_name`, `slot_22_name`, `slot_23_name`, `slot_24_name`, `slot_25_name`, `slot_26_name`, `slot_27_name`, `slot_28_name`) VALUES 
-(6, 'Nether Bag', 'NETHER', 500000
-, '', '', '', '', '', '', '', '', '', '', '', '', '', ''
-, '', '', '', '', '', '', '', '', '', '', '', '', '', '');
+INSERT INTO 
+    `rellu_essentials`.`bag_type` (
+        `id`, 
+        `displayname`, 
+        `name`, 
+        `cost`,
+        `slot_1_name`, 
+        `slot_2_name`, 
+        `slot_3_name`, 
+        `slot_4_name`, 
+        `slot_5_name`, 
+        `slot_6_name`, 
+        `slot_7_name`,
+        `slot_8_name`, 
+        `slot_9_name`, 
+        `slot_10_name`, 
+        `slot_11_name`,
+        `slot_12_name`,
+        `slot_13_name`,
+        `slot_14_name`,
+        `slot_15_name`,
+        `slot_16_name`,
+        `slot_17_name`,
+        `slot_18_name`,
+        `slot_19_name`,
+        `slot_20_name`,
+        `slot_21_name`,
+        `slot_22_name`,
+        `slot_23_name`,
+        `slot_24_name`,
+        `slot_25_name`,
+        `slot_26_name`,
+        `slot_27_name`,
+        `slot_28_name`
+    ) 
+VALUES (
+    6, 
+    'Nether Bag',
+    'NETHER',
+    500000,
+    'NETHERRACK',
+    'SOUL_SOIL',
+    'SOUL_SAND',
+    'BASALT',
+    'BLACKSTONE',
+    'MAGMA_BLOCK',
+    'SHROOMLIGHT', 
+    'NETHER_BRICKS',
+    'RED_NETHER_BRICKS',
+    DEFAULT, 
+    DEFAULT, 
+    DEFAULT, 
+    DEFAULT, 
+    DEFAULT, 
+    'GLOWSTONE', 
+    DEFAULT,
+    DEFAULT,
+    DEFAULT,
+    'TWISTING_VINES',
+    'WARPED_ROOTS',
+    'WARPED_WART_BLOCK', 
+    'GLOWSTONE_DUST', 
+    DEFAULT, 
+    DEFAULT,
+    DEFAULT, 
+    'WEEPING_VINES',
+    'CRIMSON_ROOTS',
+    'NETHER_WART_BLOCK'
+);
