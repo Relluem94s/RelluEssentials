f848adfeaa694dfb2a6c58b4a9b083a57eb992d0
Relluem94 <github@relluem94.de>
2024-02-28 22:21:32 +0100

    RE-164 Test Commit (should work in Build)

--- src/test/java/de/relluem94/minecraft/server/spigot/essentials/RelluEssentialsTest.java
+++ src/test/java/de/relluem94/minecraft/server/spigot/essentials/RelluEssentialsTest.java
@@ -1,30 +1,4 @@
 package de.relluem94.minecraft.server.spigot.essentials;
-
-import java.io.OutputStream;
-import java.io.PrintStream;
-
-import org.bukkit.GameMode;
-import org.bukkit.Material;
-import org.bukkit.block.Block;
-import org.junit.AfterClass;
-import org.junit.Assert;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.DisplayName;
-
-import be.seeseemelk.mockbukkit.MockBukkit;
-import be.seeseemelk.mockbukkit.ServerMock;
-import be.seeseemelk.mockbukkit.entity.PlayerMock;
-import ch.vorburger.exec.ManagedProcessException;
-import ch.vorburger.mariadb4j.DB;
-import de.relluem94.minecraft.server.spigot.essentials.constants.DatabaseConstants;
-import de.relluem94.minecraft.server.spigot.essentials.helpers.DatabaseHelper;
-import de.relluem94.minecraft.server.spigot.essentials.helpers.pojo.PlayerEntry;
-import de.relluem94.minecraft.server.spigot.essentials.permissions.Groups;
-
-
 public class RelluEssentialsTest {
 
 }
--- src/test/java/de/relluem94/minecraft/server/spigot/essentials/helpers/PlayerHelperTest.java
+++ src/test/java/de/relluem94/minecraft/server/spigot/essentials/helpers/PlayerHelperTest.java
@@ -1,58 +1,5 @@
 package de.relluem94.minecraft.server.spigot.essentials.helpers;
 
-import java.io.OutputStream;
-import java.io.PrintStream;
-import java.util.UUID;
-
-import org.junit.Assert;
-import org.junit.Test;
-
-import de.relluem94.minecraft.server.spigot.essentials.helpers.pojo.OfflinePlayerEntry;
-
 public class PlayerHelperTest {
-    @Test
-    public void testGetOfflinePlayerByName1() {
-        OfflinePlayerEntry ope = PlayerHelper.getOfflinePlayerByName("Relluem94");
-        Assert.assertNotNull(ope);
-    }
 
-    @Test
-    public void testGetOfflinePlayerByName2() {
-        OfflinePlayerEntry ope = PlayerHelper.getOfflinePlayerByName("Relluem94");
-        Assert.assertEquals(UUID.fromString("ec0149f9-8b21-44ee-9731-8bff508087e7"), ope.getId());
-    }
-
-    @Test
-    public void testGetOfflinePlayerByName3() {
-        PrintStream sysOut = System.out;
-
-        PrintStream noOut = new PrintStream(new OutputStream(){
-            public void write(int b) {}
-        });
-
-        System.setOut(noOut); // To disable Error Message from NetworkUtils
-
-        OfflinePlayerEntry ope = PlayerHelper.getOfflinePlayerByName("Relluem9494");
-        System.setOut(sysOut);
-
-        Assert.assertNull(ope);
-    }
-
-    @Test
-    public void testGetOfflinePlayerByUUID1() {
-        OfflinePlayerEntry ope = PlayerHelper.getOfflinePlayerByUUID(UUID.fromString("ec0149f9-8b21-44ee-9731-8bff508087e7"));
-        Assert.assertNotNull(ope);
-    }
-
-    @Test
-    public void testGetOfflinePlayerByUUID2() {
-        OfflinePlayerEntry ope = PlayerHelper.getOfflinePlayerByUUID(UUID.fromString("ec0149f9-8b21-44ee-9731-8bff508087e7"));
-        Assert.assertEquals("Relluem94", ope.getName());
-    }
-
-    @Test
-    public void testGetOfflinePlayerByUUID3() {
-        OfflinePlayerEntry ope = PlayerHelper.getOfflinePlayerByUUID(UUID.fromString("00000000-AAAA-AAAA-0000-AAFFAAAAAAAA"));
-        Assert.assertNull(ope);
-    }
 }
