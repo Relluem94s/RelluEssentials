9a57b32102ab73f2f6962e3540d4b59dec966955
Relluem94 <github@relluem94.de>
2024-03-10 21:26:31 +0100

    RE-170 add cherry items

---
+++ src/main/resources/sqls/patches/v8/insertProtectionLocks.sql
@@ -0,0 +1,14 @@
+/**
+ * Author:  rellu
+ * Created: 12.02.2023
+ */
+
+
+INSERT INTO `rellu_essentials`.`protection_locks` (`created`, `createdby`, `value`) VALUES
+(now(), 1, 'CHERRY_SIGN'),
+(now(), 1, 'CHERRY_WALL_SIGN'),
+(now(), 1, 'CHERRY_WALL_HANGING_SIGN'),
+(now(), 1, 'CHERRY_HANGING_SIGN'),
+(now(), 1, 'CHERRY_TRAPDOOR'),
+(now(), 1, 'CHERRY_DOOR'),
+(now(), 1, 'CHERRY_FENCE_GATE');
