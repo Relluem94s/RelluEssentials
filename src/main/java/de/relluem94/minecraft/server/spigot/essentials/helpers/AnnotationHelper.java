38ec838e6eafe7f8a5fb2f31df2a7862792b28ea
Relluem94 <github@relluem94.de>
2025-10-02 21:52:07 +0200

    RE-196 Add CommandConstruct and CommandsEnum

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/helpers/AnnotationHelper.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/helpers/AnnotationHelper.java
@@ -2,9 +2,11 @@ package de.relluem94.minecraft.server.spigot.essentials.helpers;
 
 import de.relluem94.minecraft.server.spigot.essentials.interfaces.CommandConstruct;
 import de.relluem94.minecraft.server.spigot.essentials.interfaces.CommandName;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class AnnotationHelper {
-    public static <T extends CommandConstruct> String getCommandName(Class<T> clazz) {
+    public static <T extends CommandConstruct> @Nullable String getCommandName(@NotNull Class<T> clazz) {
         CommandName annotation = clazz.getAnnotation(CommandName.class);
         return annotation != null ? annotation.value() : null;
     }
