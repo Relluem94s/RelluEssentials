e2619455fcaf0bd728ff81a7e1f2ffa8fe8f9394
Relluem94 <github@relluem94.de>
2025-10-09 23:07:59 +0200

    RE-224 Create Interface for CustomItem / CustomInventory

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/helpers/objects/CustomInventory.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/helpers/objects/CustomInventory.java
@@ -2,6 +2,7 @@ package de.relluem94.minecraft.server.spigot.essentials.helpers.objects;
 
 
 import de.relluem94.minecraft.server.spigot.essentials.helpers.ItemHelper;
+import de.relluem94.minecraft.server.spigot.essentials.helpers.interfaces.ICustomItems;
 import lombok.Getter;
 import lombok.Setter;
 import org.jetbrains.annotations.ApiStatus;
@@ -13,14 +14,23 @@ import org.jetbrains.annotations.ApiStatus;
 @Setter
 @Getter
 public class CustomInventory {
+    private Class<? extends ICustomItems> customItems;
     private ItemHelper.Type type;
     private int size;
     private String titleGUI;
 
-    @ApiStatus.Experimental
+    @ApiStatus.Internal
     public CustomInventory(ItemHelper.Type type, int size, String titleGUI) {
         this.type = type;
         this.size = size;
         this.titleGUI = titleGUI;
+    }
+
+    @ApiStatus.AvailableSince("v4.3.3")
+    public CustomInventory(Class<? extends ICustomItems> customItems, ItemHelper.Type type, int size, String titleGUI) {
+        this.type = type;
+        this.size = size;
+        this.titleGUI = titleGUI;
+        this.customItems = customItems;
     }
 }
