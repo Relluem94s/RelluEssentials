d1017746d8d9a20a3620449186b4ec7caf1d9c35
Relluem94 <github@relluem94.de>
2026-07-10 23:21:12 +0200

    RE-122 Updated MonsterBag and added Phantom_Membrane, Breeze_Rod

---
+++ src/main/resources/sqls/patches/v10/alterMonsterBag.sql
@@ -0,0 +1,12 @@
+/**
+ * Author:  rellu
+ * Created: 10.07.2026
+ */
+
+UPDATE 
+    `bag_type` 
+SET
+    `slot_15_name` = 'PHANTOM_MEMBRANE',
+    `slot_16_name` = 'BREEZE_ROD'
+WHERE
+    `bag_type`.`id` = 3;
