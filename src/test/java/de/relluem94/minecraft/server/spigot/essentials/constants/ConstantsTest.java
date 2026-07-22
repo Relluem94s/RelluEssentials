8ee087b94322405073bb4af4ac8409e7283094e1
Relluem94 <github@relluem94.de>
2026-07-22 23:19:12 +0200

    RE-310 Add Constants Tests

---
+++ src/test/java/de/relluem94/minecraft/server/spigot/essentials/constants/ConstantsTest.java
@@ -0,0 +1,13 @@
+package de.relluem94.minecraft.server.spigot.essentials.constants;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class ConstantsTest {
+
+    @Test
+    void pluginEolIsNotNull() {
+        assertNotNull(Constants.PLUGIN_EOL);
+    }
+}
