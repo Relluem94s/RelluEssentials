e8118d7614cc6c0c1dd84790fb1db6b847fb587b
Relluem94 <github@relluem94.de>
2026-07-26 22:06:56 +0200

    RE-347 Create EventWrapper + Lazy Load CommandWrapper

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/managers/CommandManager.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/managers/CommandManager.java
@@ -1,13 +1,12 @@
 package de.relluem94.minecraft.server.spigot.essentials.managers;
 
-import static de.relluem94.minecraft.server.spigot.essentials.RelluEssentials.languageHelper;
-import static de.relluem94.minecraft.server.spigot.essentials.constants.Constants.PLUGIN_NAME_CONSOLE;
-import static de.relluem94.minecraft.server.spigot.essentials.helpers.ChatHelper.consoleSendMessage;
-
+import de.relluem94.minecraft.server.spigot.essentials.RelluEssentials;
 import de.relluem94.minecraft.server.spigot.essentials.constants.MessageKey;
 import org.bukkit.command.PluginCommandYamlParser;
 
-import de.relluem94.minecraft.server.spigot.essentials.RelluEssentials;
+import static de.relluem94.minecraft.server.spigot.essentials.RelluEssentials.languageHelper;
+import static de.relluem94.minecraft.server.spigot.essentials.constants.Constants.PLUGIN_NAME_CONSOLE;
+import static de.relluem94.minecraft.server.spigot.essentials.helpers.ChatHelper.consoleSendMessage;
 
 public class CommandManager implements IEnable {
 
@@ -15,7 +14,7 @@ public class CommandManager implements IEnable {
     public void enable() {
         consoleSendMessage(PLUGIN_NAME_CONSOLE, languageHelper.get(MessageKey.PLUGIN_MANAGER_REGISTER_COMMANDS));
 
-        RelluEssentials.commandWrapperList.forEach(wrapper -> wrapper.init(RelluEssentials.getInstance()));
+        RelluEssentials.getCommandWrapperList().forEach(wrapper -> wrapper.init(RelluEssentials.getInstance()));
 
         int commands = PluginCommandYamlParser.parse(RelluEssentials.getInstance()).size();
         consoleSendMessage(PLUGIN_NAME_CONSOLE, languageHelper.get(MessageKey.PLUGIN_MANAGER_COMMANDS_REGISTERED, commands));
