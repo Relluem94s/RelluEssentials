f84f22bc976c551c57792216f65cb13b0ef4783b
Relluem94 <github@relluem94.de>
2026-07-30 22:20:19 +0200

    RE-221 Implement Custom Item / Inventory Registry

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/managers/RecipeManager.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/managers/RecipeManager.java
@@ -1,13 +1,21 @@
 package de.relluem94.minecraft.server.spigot.essentials.managers;
 
+import de.relluem94.minecraft.server.spigot.essentials.constants.MessageKey;
+import de.relluem94.minecraft.server.spigot.essentials.helpers.ItemHelper;
+import de.relluem94.minecraft.server.spigot.essentials.helpers.RecipeHelper;
+import de.relluem94.minecraft.server.spigot.essentials.helpers.recipe.Shaped;
+import de.relluem94.minecraft.server.spigot.essentials.registry.ItemRegistry;
+import de.relluem94.minecraft.server.spigot.essentials.registry.RegistryKey;
+import org.bukkit.Bukkit;
+import org.jspecify.annotations.NonNull;
+
+import java.util.Map;
+
 import static de.relluem94.minecraft.server.spigot.essentials.RelluEssentials.languageHelper;
 import static de.relluem94.minecraft.server.spigot.essentials.constants.Constants.PLUGIN_NAME_CONSOLE;
+import static de.relluem94.minecraft.server.spigot.essentials.constants.ItemConstants.PLUGIN_ITEM_NAMESPACE_CLOUD_BOOTS;
+import static de.relluem94.minecraft.server.spigot.essentials.constants.ItemConstants.PLUGIN_ITEM_NAMESPACE_CLOUD_SAILOR;
 import static de.relluem94.minecraft.server.spigot.essentials.helpers.ChatHelper.consoleSendMessage;
-
-import de.relluem94.minecraft.server.spigot.essentials.constants.MessageKey;
-import org.bukkit.Bukkit;
-
-import de.relluem94.minecraft.server.spigot.essentials.CustomRecipes;
 
 public class RecipeManager implements IEnable {
 
@@ -15,8 +23,25 @@ public class RecipeManager implements IEnable {
     public void enable() {
         consoleSendMessage(PLUGIN_NAME_CONSOLE, languageHelper.get(MessageKey.PLUGIN_MANAGER_REGISTER_RECIPE));
         int recipeCount = 0;
-        Bukkit.addRecipe(CustomRecipes.cloudBoots.getRecipe());                 recipeCount++;
+        Bukkit.addRecipe(buildCloudBootsRecipe().getRecipe());
+        recipeCount++;
         consoleSendMessage(PLUGIN_NAME_CONSOLE, languageHelper.get(MessageKey.PLUGIN_MANAGER_RECIPE_REGISTERED, recipeCount));
     }
-    
+
+    private @NonNull RecipeHelper buildCloudBootsRecipe() {
+        ItemHelper cloudSailorItem = ItemRegistry.find(RegistryKey.of(PLUGIN_ITEM_NAMESPACE_CLOUD_SAILOR)).orElseThrow();
+        ItemHelper cloudBootsItem = ItemRegistry.find(RegistryKey.of(PLUGIN_ITEM_NAMESPACE_CLOUD_BOOTS)).orElseThrow();
+
+        return new RecipeHelper(
+                PLUGIN_ITEM_NAMESPACE_CLOUD_BOOTS,
+                new Shaped(
+                        new String[]{
+                                "F F",
+                                "F F",
+                        },
+                        Map.of('F', cloudSailorItem.getMaterial())
+                ),
+                cloudBootsItem.getCustomItem()
+        );
+    }
 }
