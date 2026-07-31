7a4bc613994248aba82a6e25fe2fb3c848c61238
Relluem94 <github@relluem94.de>
2026-07-31 20:07:27 +0200

    RE-364 Migrate RelluPickaxe to new ItemRegistry

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/commands/dev/GivePickaxeCommand.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/commands/dev/GivePickaxeCommand.java
@@ -2,15 +2,18 @@ package de.relluem94.minecraft.server.spigot.essentials.commands.dev;
 
 import de.relluem94.minecraft.server.spigot.essentials.commands.DevCommand;
 import de.relluem94.minecraft.server.spigot.essentials.interfaces.SubCommand;
-import de.relluem94.minecraft.server.spigot.essentials.items.RelluPickaxe;
+import de.relluem94.minecraft.server.spigot.essentials.model.RegistryKey;
+import de.relluem94.minecraft.server.spigot.essentials.registry.ItemRegistry;
 import org.bukkit.entity.Player;
 import org.jspecify.annotations.NonNull;
+
+import static de.relluem94.minecraft.server.spigot.essentials.constants.ItemConstants.PLUGIN_ITEM_NAMESPACE_RELLU_PICKAXE;
 
 public class GivePickaxeCommand implements SubCommand {
 
     @Override
     public void execute(Player player, String[] args) {
-        player.getInventory().addItem(new RelluPickaxe().getCustomItem());
+        player.getInventory().addItem(ItemRegistry.find(RegistryKey.of(PLUGIN_ITEM_NAMESPACE_RELLU_PICKAXE)).orElseThrow().getCustomItem());
     }
 
     @Override
