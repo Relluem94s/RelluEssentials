7ea914ed7b7e132388168b8931b6bd41d942e3c4
Relluem94 <github@relluem94.de>
2026-08-03 22:38:18 +0200

    RE-410 Added SelectionService

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/services/SelectionService.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/services/SelectionService.java
@@ -1,6 +1,4 @@
-package de.relluem94.minecraft.server.spigot.essentials.commands.modify.shared;
-
-import static de.relluem94.minecraft.server.spigot.essentials.RelluEssentials.languageHelper;
+package de.relluem94.minecraft.server.spigot.essentials.services;
 
 import de.relluem94.minecraft.server.spigot.essentials.RelluEssentials;
 import de.relluem94.minecraft.server.spigot.essentials.enums.MessageKey;
@@ -9,11 +7,17 @@ import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.Nullable;
 
-public class SelectionResolver {
+public class SelectionService {
+
+  private final TranslationService translationService;
+
+  public SelectionService(TranslationService translationService) {
+    this.translationService = translationService;
+  }
 
   public @Nullable Selection resolve(Player player) {
     if (!RelluEssentials.getInstance().position.containsKey(player)) {
-      player.sendMessage(languageHelper.getWithPrefix(MessageKey.COMMAND_MODIFY_NO_POSITIONS));
+      player.sendMessage(translationService.getWithPrefix(MessageKey.COMMAND_MODIFY_NO_POSITIONS));
       return null;
     }
 
@@ -21,17 +25,18 @@ public class SelectionResolver {
     Location pos2 = RelluEssentials.getInstance().position.get(player).getSecondValue();
 
     if (pos1 == null) {
-      player.sendMessage(languageHelper.getWithPrefix(MessageKey.COMMAND_MODIFY_POS_1_EMPTY));
+      player.sendMessage(translationService.getWithPrefix(MessageKey.COMMAND_MODIFY_POS_1_EMPTY));
       return null;
     }
 
     if (pos2 == null) {
-      player.sendMessage(languageHelper.getWithPrefix(MessageKey.COMMAND_MODIFY_POS_2_EMPTY));
+      player.sendMessage(translationService.getWithPrefix(MessageKey.COMMAND_MODIFY_POS_2_EMPTY));
       return null;
     }
 
     if (pos1.getWorld() != pos2.getWorld()) {
-      player.sendMessage(languageHelper.getWithPrefix(MessageKey.COMMAND_MODIFY_DIFFERENT_WORLDS));
+      player.sendMessage(
+          translationService.getWithPrefix(MessageKey.COMMAND_MODIFY_DIFFERENT_WORLDS));
       return null;
     }
 
