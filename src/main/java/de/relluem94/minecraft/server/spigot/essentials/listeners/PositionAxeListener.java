50c693b49a37a694c254d504989cc88f60f9e88b
Relluem94 <github@relluem94.de>
2026-08-05 00:10:24 +0200

    RE-407 RE-409 RE-401 refactoring of langaueHelper to TranslationService

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/listeners/PositionAxeListener.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/listeners/PositionAxeListener.java
@@ -1,6 +1,5 @@
 package de.relluem94.minecraft.server.spigot.essentials.listeners;
 
-import static de.relluem94.minecraft.server.spigot.essentials.RelluEssentials.translationService;
 import static de.relluem94.minecraft.server.spigot.essentials.constants.ItemConstants.PLUGIN_ITEM_NAMESPACE_POSITION_AXE;
 
 import de.relluem94.minecraft.server.spigot.essentials.RelluEssentials;
@@ -10,6 +9,7 @@ import de.relluem94.minecraft.server.spigot.essentials.helpers.ItemHelper;
 import de.relluem94.minecraft.server.spigot.essentials.interfaces.ListenerConstruct;
 import de.relluem94.minecraft.server.spigot.essentials.model.RegistryKey;
 import de.relluem94.minecraft.server.spigot.essentials.registry.ItemRegistry;
+import de.relluem94.minecraft.server.spigot.essentials.services.TranslationService;
 import de.relluem94.rellulib.stores.DoubleStore;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
@@ -26,9 +26,11 @@ public class PositionAxeListener implements ListenerConstruct {
   private final ItemHelper positionAxeItem = ItemRegistry.find(
       RegistryKey.of(PLUGIN_ITEM_NAMESPACE_POSITION_AXE)).orElseThrow();
 
+  TranslationService translationService;
+
   @Override
   public void injectContext(ServiceContext context) {
-
+    translationService = context.getTranslationService();
   }
 
   @EventHandler
