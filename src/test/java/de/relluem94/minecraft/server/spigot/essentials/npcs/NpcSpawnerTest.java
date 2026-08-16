411d8217a92869c4d662d2078c69effae73b1b7d
Relluem94 <github@relluem94.de>
2026-08-16 18:02:19 +0200

    RE-329 refactor and test create

--- src/test/java/de/relluem94/minecraft/server/spigot/essentials/npcs/NpcSpawnerTest.java
+++ src/test/java/de/relluem94/minecraft/server/spigot/essentials/npcs/NpcSpawnerTest.java
@@ -251,4 +251,25 @@ class NpcSpawnerTest {
 
     verify(server).getEntity(MANNEQUIN_UUID);
   }
+
+  @Test
+  void spawnMannequin_whenExistingEntityIsNotMannequin_returnsEmptyOptional() {
+    Entity nonMannequinEntity = mock(Entity.class);
+    when(npc.getId()).thenReturn(NPC_UUID);
+    when(npc.getWorldName()).thenReturn(WORLD_NAME);
+    when(mannequin.getType()).thenReturn(EntityType.MANNEQUIN);
+    when(mannequin.getUniqueId()).thenReturn(MANNEQUIN_UUID);
+    when(mannequin.getPersistentDataContainer()).thenReturn(persistentDataContainer);
+    when(server.getWorld(WORLD_NAME)).thenReturn(world);
+    when(world.getEntities()).thenReturn(List.of(mannequin));
+    when(persistentDataContainer.get(any(NamespacedKey.class), eq(PersistentDataType.STRING)))
+        .thenReturn(NPC_UUID.toString());
+    when(server.getEntity(MANNEQUIN_UUID)).thenReturn(nonMannequinEntity);
+
+    Optional<UUID> result = npcSpawner.spawnMannequin(npc);
+
+    assertTrue(result.isPresent());
+    assertEquals(MANNEQUIN_UUID, result.get());
+  }
+
 }
