b7cf07aa4c78817e020c5a7aab07431561104268
Relluem94 <github@relluem94.de>
2026-08-19 23:37:24 +0200

    RE-505 Javadocs

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/registries/CommandRegistry.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/registries/CommandRegistry.java
@@ -5,14 +5,27 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * Registry responsible for managing and storing registered command wrappers.
+ */
 public class CommandRegistry {
 
   private final List<CommandWrapper> registeredCommands = new ArrayList<>();
 
+  /**
+   * Registers a new command wrapper into the registry.
+   *
+   * @param commandWrapper the command wrapper to be registered
+   */
   public void register(CommandWrapper commandWrapper) {
     registeredCommands.add(commandWrapper);
   }
 
+  /**
+   * Retrieves an unmodifiable list of all registered command wrappers.
+   *
+   * @return an unmodifiable list containing all registered commands
+   */
   public List<CommandWrapper> getAll() {
     return Collections.unmodifiableList(registeredCommands);
   }
