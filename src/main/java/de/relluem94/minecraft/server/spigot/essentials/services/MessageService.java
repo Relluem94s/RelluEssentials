0ee72a88de22440f5b0d33c1d841ff00e77180d8
Relluem94 <github@relluem94.de>
2026-08-23 21:50:47 +0200

    RE-505 Javadocs

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/services/MessageService.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/services/MessageService.java
@@ -5,29 +5,38 @@ import org.bukkit.Location;
 import org.bukkit.World;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Service responsible for handling message formatting and translations.
+ */
 public class MessageService {
 
   private final TranslationService translationService;
 
+  /**
+   * Creates a new instance of MessageService.
+   *
+   * @param translationService the service used for fetching translated strings
+   */
   public MessageService(TranslationService translationService){
     this.translationService = translationService;
   }
 
   /**
+   * Converts a location to a human-readable string without rounding coordinates.
    *
-   * @param l Location
-   * @return String with Location
+   * @param l the location to convert
+   * @return a string representation of the location
    */
   public @NotNull String locationToString(@NotNull Location l) {
     return locationToString(l, false);
   }
 
-
   /**
+   * Converts a location to a human-readable string.
    *
-   * @param l     Location
-   * @param round boolean should round the number
-   * @return String with Location
+   * @param l     the location to convert
+   * @param round whether the coordinates should be rounded
+   * @return a string representation of the location
    */
   public @NotNull String locationToString(@NotNull Location l, boolean round) {
     if (round) {
@@ -42,9 +51,4 @@ public class MessageService {
     return translationService.get(MessageKey.COMMAND_WHERE_STRING, l.getX(), l.getY(), l.getZ(),
         world.getName());
   }
-
-  public static @NotNull String firstCharToUpper(@NotNull String s) {
-    return Character.toUpperCase(s.charAt(0)) + s.substring(1);
-  }
-
 }
