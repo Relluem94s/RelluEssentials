43dac8c63db2459e491b168b05f1c947f2013c8a
Relluem94 <github@relluem94.de>
2026-09-06 18:14:16 +0200

    RE-521 Refactor to remove Static Bukkit Registry

--- src/test/java/de/relluem94/minecraft/server/spigot/essentials/models/items/CustomItemTest.java
+++ src/test/java/de/relluem94/minecraft/server/spigot/essentials/models/items/CustomItemTest.java
@@ -17,17 +17,11 @@ import java.util.Map;
 import java.util.function.Consumer;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
-import org.bukkit.Registry;
 import org.bukkit.Server;
-import org.bukkit.enchantments.Enchantment;
-import org.bukkit.enchantments.EnchantmentTarget;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
-import org.jetbrains.annotations.NotNull;
-import org.jspecify.annotations.NonNull;
-import org.jspecify.annotations.Nullable;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -41,6 +35,13 @@ class CustomItemTest {
 
   @Mock
   Server server;
+
+  @SuppressWarnings("unchecked")
+  private static void setBukkitServer(Server server) throws Exception {
+    java.lang.reflect.Field serverField = org.bukkit.Bukkit.class.getDeclaredField("server");
+    serverField.setAccessible(true);
+    serverField.set(null, server);
+  }
 
   private CustomItem buildMinimalItem() {
     return new CustomItem(Material.STONE, 1, "Name", List.of(), Type.NONE, Rarity.NONE, null,
@@ -330,115 +331,6 @@ class CustomItemTest {
       assertNotNull(dataKey);
       verify(capturedMeta[0].getPersistentDataContainer()).set(dataKey, PersistentDataType.DOUBLE,
           3.14);
-    }
-  }
-
-  @Test
-  void toItemStackAppliesOnlyValidEnchantments() {
-    ItemMeta[] capturedMeta = new ItemMeta[1];
-
-    NamespacedKey validKey = new NamespacedKey("minecraft", "sharpness");
-    NamespacedKey unknownKey = new NamespacedKey("minecraft", "unknown_enchant");
-
-    Enchantment sharpness = new Enchantment() {
-      @Override
-      public @NonNull NamespacedKey getKeyOrThrow() {
-        return validKey;
-      }
-
-      @Override
-      public @Nullable NamespacedKey getKeyOrNull() {
-        return validKey;
-      }
-
-      @Override
-      public boolean isRegistered() {
-        return true;
-      }
-
-      @Override
-      public @NonNull String getTranslationKey() {
-        return validKey + ".translate";
-      }
-
-      @Override
-      public @NotNull String getName() {
-        return "SHARPNESS";
-      }
-
-      @Override
-      public int getMaxLevel() {
-        return 5;
-      }
-
-      @Override
-      public int getStartLevel() {
-        return 1;
-      }
-
-      @Override
-      public @NotNull EnchantmentTarget getItemTarget() {
-        return EnchantmentTarget.WEAPON;
-      }
-
-      @Override
-      public boolean isTreasure() {
-        return false;
-      }
-
-      @Override
-      public boolean isCursed() {
-        return false;
-      }
-
-      @Override
-      public boolean conflictsWith(@NotNull Enchantment other) {
-        return false;
-      }
-
-      @Override
-      public boolean canEnchantItem(@NotNull ItemStack item) {
-        return true;
-      }
-
-      @Override
-      public @NonNull NamespacedKey getKey() {
-        return validKey;
-      }
-    };
-
-    @SuppressWarnings("unchecked") Registry<Enchantment> enchantmentRegistry = mock(Registry.class);
-    when(server.getRegistry(Enchantment.class)).thenReturn(enchantmentRegistry);
-    when(enchantmentRegistry.get(validKey)).thenReturn(sharpness);
-    when(enchantmentRegistry.get(unknownKey)).thenReturn(null);
-
-    List<CustomItem.EnchantmentData> enchantments = List.of(
-        new CustomItem.EnchantmentData("%%invalid key%%", 1),
-        new CustomItem.EnchantmentData(unknownKey.toString(), 2),
-        new CustomItem.EnchantmentData(validKey.toString(), 3));
-
-    try (MockedConstruction<ItemStack> ignored = Mockito.mockConstruction(ItemStack.class,
-        (mock, _) -> capturedMeta[0] = mockItemMetaOnItemStack(
-            mock)); MockedStatic<NamespacedKey> mockedNamespacedKey = Mockito.mockStatic(
-        NamespacedKey.class)) {
-
-      mockedNamespacedKey.when(() -> NamespacedKey.fromString("%%invalid key%%")).thenReturn(null);
-      mockedNamespacedKey.when(() -> NamespacedKey.fromString(unknownKey.toString()))
-          .thenReturn(unknownKey);
-      mockedNamespacedKey.when(() -> NamespacedKey.fromString(validKey.toString()))
-          .thenReturn(validKey);
-
-      CustomItem customItem = new CustomItem(Material.STONE, 1, null, List.of(), Type.NONE,
-          Rarity.NONE, null, enchantments, Map.of(), List.of(),
-          new RelluEssentialsNamespacedKey("test", "stone"), server);
-
-      customItem.toItemStack();
-
-      verify(capturedMeta[0], never()).addEnchant(Mockito.any(), Mockito.eq(1),
-          Mockito.anyBoolean());
-      verify(capturedMeta[0], never()).addEnchant(Mockito.any(), Mockito.eq(2),
-          Mockito.anyBoolean());
-      verify(capturedMeta[0]).addEnchant(sharpness, 3, true);
     }
   }
 }
