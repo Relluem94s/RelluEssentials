76e927a1b1ff5186c0632880331658d26a46ae9e
Relluem94 <github@relluem94.de>
2026-09-07 23:25:35 +0200

    RE-530 Implemented ServerService in GameModeSurvival

--- src/main/java/de/relluem94/minecraft/server/spigot/essentials/commands/GameModeSurvival.java
+++ src/main/java/de/relluem94/minecraft/server/spigot/essentials/commands/GameModeSurvival.java
@@ -5,18 +5,18 @@ import static de.relluem94.minecraft.server.spigot.essentials.helpers.TypeHelper
 import de.relluem94.minecraft.server.spigot.essentials.annotations.CommandName;
 import de.relluem94.minecraft.server.spigot.essentials.contexts.ServiceContext;
 import de.relluem94.minecraft.server.spigot.essentials.enums.MessageKey;
-import de.relluem94.minecraft.server.spigot.essentials.helpers.TabCompleterHelper;
 import de.relluem94.minecraft.server.spigot.essentials.interfaces.CommandConstruct;
 import de.relluem94.minecraft.server.spigot.essentials.interfaces.CommandsEnum;
 import java.util.ArrayList;
 import java.util.List;
-import lombok.NonNull;
+import java.util.stream.Collectors;
 import org.bukkit.GameMode;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.jspecify.annotations.NonNull;
 
 /**
  * Command implementation that sets the game mode of a player to {@link GameMode#SURVIVAL}.
@@ -75,8 +75,7 @@ public class GameModeSurvival implements CommandConstruct {
     }
 
     if (args.length == 1) {
-      Player target = serviceContext.getPluginMetadataService().getPlugin().getServer()
-          .getPlayer(args[0]);
+      Player target = serviceContext.getServerService().getPlayer(args[0]);
 
       if (target == null) {
         sender.sendMessage(serviceContext.getTranslationService()
@@ -131,6 +130,7 @@ public class GameModeSurvival implements CommandConstruct {
       return new ArrayList<>();
     }
 
-    return TabCompleterHelper.getOnlinePlayers();
+    return serviceContext.getServerService().getOnlinePlayers().stream().map(Player::getName)
+        .collect(Collectors.toList());
   }
 }
